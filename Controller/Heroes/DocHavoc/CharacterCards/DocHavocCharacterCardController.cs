b95ef965316ae1d363babd079e5c1f5abc5fda0b
byoung <breen.young@gmail.com>
2020-11-05 00:20:03 -0400

    Start DocHavoc development - Add stub character card - Add Doc's Flask card & tests

---
+++ Controller/Heroes/DocHavoc/CharacterCards/DocHavocCharacterCardController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+
+namespace Cauldron.DocHavoc
+{
+    public class DocHavocCharacterCardController : HeroCharacterCardController
+    {
+        public DocHavocCharacterCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+        }
+
+        public override IEnumerator UsePower(int index = 0)
+        {
+            return base.UsePower(index);
+        }
+
+        public override IEnumerator UseIncapacitatedAbility(int index)
+        {
+            return base.UseIncapacitatedAbility(index);
+        }
+    }
+}
