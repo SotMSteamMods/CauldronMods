38fb74c888d03210aced21e5f86c6de9d2ce4d33
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2020-11-04 21:09:33 -0600

    Lady of The Wood Test + Fixes
    Fixed stranger decklist name
    changed namespace for setup.cs

--- Controller/Heroes/LadyOfTheWood/ThundergreyShawlCardController.cs
+++ Controller/Heroes/LadyOfTheWood/ThundergreyShawlCardController.cs
@@ -1,6 +1,8 @@
 ﻿using Handelabra.Sentinels.Engine.Controller;
 using Handelabra.Sentinels.Engine.Model;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Cauldron.LadyOfTheWood
 {
