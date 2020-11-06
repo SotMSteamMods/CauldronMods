82d724f3ac999b369ed5812927dc71be30cf374d
Breen Young <breen.young@gmail.com>
2020-11-06 08:31:35 -0400

    Add missing power option

--- Controller/Heroes/DocHavoc/Cards/DocsFlaskCardController.cs
+++ Controller/Heroes/DocHavoc/Cards/DocsFlaskCardController.cs
@@ -9,6 +9,7 @@ namespace Cauldron.DocHavoc
     public class DocsFlaskCardController : CardController
     {
         public static string Identifier = "DocsFlask";
+        private const int HpGain = 1;
 
         public DocsFlaskCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
@@ -16,7 +17,10 @@ namespace Cauldron.DocHavoc
 
         public override void AddTriggers()
         {
+            //==============================================================
             // At the start of your turn, 1 hero target regains 1 HP
+            //==============================================================
+
             this.AddStartOfTurnTrigger((Func<TurnTaker, bool>)(tt => tt == this.TurnTaker),
                 (Func<PhaseChangeAction, IEnumerator>)(p =>
                     this.GameController.SelectAndGainHP(this.DecisionMaker, 1,
@@ -25,6 +29,25 @@ namespace Cauldron.DocHavoc
                 TriggerType.GainHP);
 
                 base.AddTriggers();
+        }
+
+        public override IEnumerator UsePower(int index = 0)
+        {
+            //==============================================================
+            // Each 1 hero target regains 1 HP.
+            //==============================================================
+
+            int powerNumeral = this.GetPowerNumeral(0, HpGain);
+            IEnumerator routine = this.GameController.GainHP(this.HeroTurnTakerController, (Func<Card, bool>)(c => c.IsHero), powerNumeral, cardSource: this.GetCardSource());
+            if (this.UseUnityCoroutines)
+            {
+
+                yield return this.GameController.StartCoroutine(routine);
+            }
+            else
+            {
+                this.GameController.ExhaustCoroutine(routine);
+            }
         }
     }
 }
