582a6801048fac719d170d3bba648889eb068c79
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2020-11-05 18:38:23 -0600

    InitializedTheStranger +LotW Reorganization
    Setup CardControllers and Test File
    Also re-organized LotW cards

---
+++ Testing/Heroes/TheStrangerTests.cs
@@ -0,0 +1,42 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using Handelabra.Sentinels.UnitTest;
+using NUnit.Framework;
+using Cauldron.TheStranger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CauldronTests
+{
+    [TestFixture()]
+    public class TheStranger : BaseTest
+    {
+        #region TheStrangerHelperFunctions
+        protected HeroTurnTakerController stranger { get { return FindHero("TheStranger"); } }
+        private void SetupIncap(TurnTakerController villain)
+        {
+            SetHitPoints(stranger.CharacterCard, 1);
+            DealDamage(villain, stranger, 2, DamageType.Melee);
+        }
+       
+
+        #endregion
+
+        [Test()]
+        public void TestStrangerLoads()
+        {
+            SetupGameController("BaronBlade", "Cauldron.TheStranger", "Megalopolis");
+
+            Assert.AreEqual(3, this.GameController.TurnTakerControllers.Count());
+
+            Assert.IsNotNull(stranger);
+            Assert.IsInstanceOf(typeof(TheStrangerCharacterCardController), stranger.CharacterCardController);
+
+            Assert.AreEqual(26, stranger.CharacterCard.HitPoints);
+        }
+
+
+    }
+}
