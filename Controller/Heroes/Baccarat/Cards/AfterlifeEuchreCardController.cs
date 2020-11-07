2363873d19a725e14c177f99982fb64a78bcd081
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2020-11-07 17:17:16 -0600

    Clean up default parameters in AfterlifeEuchreCardController
    When i try to get rid of the default parameters in line 29, I get null object exceptions, so i left those in

--- Controller/Heroes/Baccarat/Cards/AfterlifeEuchreCardController.cs
+++ Controller/Heroes/Baccarat/Cards/AfterlifeEuchreCardController.cs
@@ -28,7 +28,7 @@ namespace Cauldron.Baccarat
 				//...or {Baccarat} deals 1 target 2 toxic damage.
 				new Function(base.HeroTurnTakerController, "Baccarat deals 1 target 2 toxic damage", SelectionType.TurnTaker, () => base.GameController.SelectTargetsAndDealDamage(this.DecisionMaker, new DamageSource(base.GameController, base.CharacterCard), 2, DamageType.Toxic, new int?(1), false, new int?(1), false, false, false, null, null, null, null, null, false, null, null, false, null, base.GetCardSource(null)))
 			};
-			SelectFunctionDecision selectFunction = new SelectFunctionDecision(base.GameController, base.HeroTurnTakerController, functionChoices, false, null, null, null, base.GetCardSource(null));
+			SelectFunctionDecision selectFunction = new SelectFunctionDecision(base.GameController, base.HeroTurnTakerController, functionChoices, false);
 			IEnumerator coroutine = base.GameController.SelectAndPerformFunction(selectFunction, null, null);
 			if (base.UseUnityCoroutines)
 			{
