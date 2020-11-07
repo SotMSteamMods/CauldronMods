1d4460c1a094c218e01eb277f7caf71bb5f6dd76
Vilnor <onlytelford@yahoo.com>
2020-11-07 12:24:16 -0500

    Started Refactoring common NecroCode down to abstract base classes NecroCardController and UndeadCardController

---
+++ Controller/Heroes/Necro/CharacterCards/NecroCardController.cs
@@ -0,0 +1,33 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System.Collections;
+using System.Linq;
+using System;
+
+namespace Cauldron.Necro
+{
+    public abstract class NecroCardController : CardController
+    {
+        public static readonly string RitualKeyword = "ritual";
+        public static readonly string UndeadKeyword = "undead";
+
+        protected NecroCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+        }
+
+        protected bool IsRitual(Card card)
+        {
+            return card.DoKeywordsContain(RitualKeyword);
+        }
+
+        protected int GetNumberOfRitualsInPlay()
+        {
+            return base.FindCardsWhere(c => c.IsInPlayAndHasGameText && this.IsRitual(c)).Count();
+        }
+
+        protected bool IsUndead(Card card)
+        {
+            return card.DoKeywordsContain(UndeadKeyword);
+        }
+    }
+}
