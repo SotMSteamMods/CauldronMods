77ac870b86cd81c52e44deb275d29afd6987e7d2
byoung <breen.young@gmail.com>
2020-11-07 23:15:08 -0400

    Optimize usings

--- Controller/Heroes/DocHavoc/Cards/RapidRegenCardController.cs
+++ Controller/Heroes/DocHavoc/Cards/RapidRegenCardController.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using Handelabra.Sentinels.Engine.Controller;
 using Handelabra.Sentinels.Engine.Model;
 
