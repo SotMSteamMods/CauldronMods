5c8dd24c3f2daec48944214ccc0292ed2f57829c
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2020-11-07 21:19:05 -0600

    Emergency Release Protocols Controller + Tests

--- Controller/Environments/HalberdExperimentalResearchCenter/EmergencyReleaseProtocolCardController.cs
+++ Controller/Environments/HalberdExperimentalResearchCenter/EmergencyReleaseProtocolCardController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using Handelabra.Sentinels.Engine.Controller;
 using Handelabra.Sentinels.Engine.Model;
 
@@ -16,7 +17,18 @@ namespace Cauldron.HalberdExperimentalResearchCenter
         #endregion Constructors
 
         #region Methods
+        public override void AddTriggers()
+        {
 
+            //At the end of the environment turn, play the top card of the environment deck
+            base.AddEndOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, (PhaseChangeAction action) => base.PlayTheTopCardOfTheEnvironmentDeckWithMessageResponse(action), TriggerType.PlayCard);
+            // At the start of their turn, a player may skip the rest of their turn to destroy this card.
+            base.AddStartOfTurnTrigger((TurnTaker tt) => tt.IsHero, new Func<PhaseChangeAction, IEnumerator>(base.SkipTheirTurnToDestroyThisCardResponse), new TriggerType[]
+            {
+                TriggerType.SkipTurn,
+                TriggerType.DestroySelf
+            });
+        }
         #endregion Methods
     }
 }
--- Testing/Environments/HalberdExperimentalResearchCenterTests.cs
+++ Testing/Environments/HalberdExperimentalResearchCenterTests.cs
@@ -27,8 +27,45 @@ namespace CauldronTests
             Assert.AreEqual(5, this.GameController.TurnTakerControllers.Count());
             
         }
+        [Test()]
+        public void TestEmergencyProtocolsPlay()
+        {
+            SetupGameController("BaronBlade", "Ra", "Legacy", "Haka", "Cauldron.HalberdExperimentalResearchCenter");
+            StartGame();
+            //stack the deck so we know what the played card will be
+            Card prophet = GetCard("HalberdProphet");
+            PutOnDeck(halberd, prophet);
+            GoToPlayCardPhase(halberd);
+            AssertInDeck(prophet);
+            //At the end of the environment turn, play the top card of the environment deck.
 
-       
+            PlayCard("EmergencyReleaseProtocol");
+            GoToEndOfTurn(halberd);
+            //since the top card of the deck was played, prophet should be in play
+            AssertIsInPlay(prophet);
+
+        }
+
+        [Test()]
+        public void TestEmergencyProtocolsDestroy()
+        {
+            SetupGameController("BaronBlade", "Ra", "Legacy", "Haka", "Cauldron.HalberdExperimentalResearchCenter");
+            StartGame();
+
+            Card emergency = GetCard("EmergencyReleaseProtocol");
+            PlayCard(emergency);
+            AssertIsInPlay(emergency);
+
+            //At the start of their turn, a player may skip the rest of their turn to destroy this card.
+            //yes we want the player to skip their turn
+            DecisionYesNo = true;
+            GoToStartOfTurn(ra);
+            AssertInTrash(emergency);
+
+        }
+
+
+
 
     }
 }
