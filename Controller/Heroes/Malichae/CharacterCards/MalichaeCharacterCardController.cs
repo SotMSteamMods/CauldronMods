1d8e524cefcff4796180501965707f44796b708d
Vilnor <onlytelford@yahoo.com>
2020-11-08 21:21:42 -0500

    Malichae Incap Powers and Tests

--- Controller/Heroes/Malichae/CharacterCards/MalichaeCharacterCardController.cs
+++ Controller/Heroes/Malichae/CharacterCards/MalichaeCharacterCardController.cs
@@ -6,54 +6,117 @@ using System.Linq;
 
 namespace Cauldron.Malichae
 {
-	public class MalichaeCharacterCardController : HeroCharacterCardController
-	{
-		public MalichaeCharacterCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
-		{
-		}
-		public override IEnumerator UsePower(int index = 0)
-		{
-			var coroutine = base.DrawCards(this.DecisionMaker, 2);
-			if (base.UseUnityCoroutines)
-			{
-				yield return base.GameController.StartCoroutine(coroutine);
-			}
-			else
-			{
-				base.GameController.ExhaustCoroutine(coroutine);
-			}
-			coroutine = base.SelectAndDiscardCards(this.DecisionMaker, 1);
-			if (base.UseUnityCoroutines)
-			{
-				yield return base.GameController.StartCoroutine(coroutine);
-			}
-			else
-			{
-				base.GameController.ExhaustCoroutine(coroutine);
-			}
-			yield break;
-		}
-		public override IEnumerator UseIncapacitatedAbility(int index)
-		{
-			switch (index)
-			{
-				case 0:
-					{
-						
-						break;
-					}
-				case 1:
-					{
-						
-						yield break;
-					}
-				case 2:
-					{
-						
-						break;
-					}
-			}
-			yield break;
-		}
-	}
+    public class MalichaeCharacterCardController : HeroCharacterCardController
+    {
+        public MalichaeCharacterCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+        }
+        public override IEnumerator UsePower(int index = 0)
+        {
+            var coroutine = base.DrawCards(this.DecisionMaker, 2);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            coroutine = base.SelectAndDiscardCards(this.DecisionMaker, 1);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+        public override IEnumerator UseIncapacitatedAbility(int index)
+        {
+            switch (index)
+            {
+                case 0:
+                    {
+                        List<DiscardCardAction> storedResultsDiscard = new List<DiscardCardAction>();
+                        List<SelectTurnTakerDecision> storedResultsTurnTaker = new List<SelectTurnTakerDecision>();
+                        var coroutine = base.GameController.SelectHeroToDiscardCard(this.DecisionMaker,
+                            storedResultsTurnTaker: storedResultsTurnTaker,
+                            storedResultsDiscard: storedResultsDiscard,
+                            cardSource: base.GetCardSource());
+                        if (base.UseUnityCoroutines)
+                        {
+                            yield return base.GameController.StartCoroutine(coroutine);
+                        }
+                        else
+                        {
+                            base.GameController.ExhaustCoroutine(coroutine);
+                        }
+                        if (DidDiscardCards(storedResultsDiscard, 1))
+                        {
+                            var httc = base.FindHeroTurnTakerController(GetSelectedTurnTaker(storedResultsTurnTaker).ToHero());
+                            coroutine = base.DrawCards(httc, 3);
+                            if (base.UseUnityCoroutines)
+                            {
+                                yield return base.GameController.StartCoroutine(coroutine);
+                            }
+                            else
+                            {
+                                base.GameController.ExhaustCoroutine(coroutine);
+                            }
+                        }
+
+                        break;
+                    }
+                case 1:
+                    {
+                        var coroutine = base.GameController.SelectAndDestroyCard(this.DecisionMaker, new LinqCardCriteria(c => c.IsEnvironment, "enviroment"), false, cardSource: GetCardSource());
+                        if (base.UseUnityCoroutines)
+                        {
+                            yield return base.GameController.StartCoroutine(coroutine);
+                        }
+                        else
+                        {
+                            base.GameController.ExhaustCoroutine(coroutine);
+                        }
+                        yield break;
+                    }
+                case 2:
+                    {
+                        List<SelectDamageTypeDecision> storedResults = new List<SelectDamageTypeDecision>();
+                        var coroutine = base.GameController.SelectDamageType(this.DecisionMaker, storedResults, cardSource: GetCardSource());
+                        if (base.UseUnityCoroutines)
+                        {
+                            yield return base.GameController.StartCoroutine(coroutine);
+                        }
+                        else
+                        {
+                            base.GameController.ExhaustCoroutine(coroutine);
+                        }
+
+                        var selectedType = GetSelectedDamageType(storedResults);
+                        if (selectedType.HasValue)
+                        {
+                            ChangeDamageTypeStatusEffect status = new ChangeDamageTypeStatusEffect(selectedType.Value);
+                            status.SourceCriteria.IsHero = true;
+                            status.SourceCriteria.IsTarget = true;
+                            status.UntilStartOfNextTurn(this.TurnTaker);
+
+                            coroutine = base.AddStatusEffect(status);
+                            if (base.UseUnityCoroutines)
+                            {
+                                yield return base.GameController.StartCoroutine(coroutine);
+                            }
+                            else
+                            {
+                                base.GameController.ExhaustCoroutine(coroutine);
+                            }
+                        }
+                        break;
+                    }
+            }
+            yield break;
+        }
+    }
 }
--- Testing/Heroes/MalichaeTests.cs
+++ Testing/Heroes/MalichaeTests.cs
@@ -19,6 +19,7 @@ namespace CauldronTests
         {
             SetHitPoints(Malichae.CharacterCard, 1);
             DealDamage(villain, Malichae, 2, DamageType.Melee);
+            AssertIncapacitated(Malichae);
         }
 
         #endregion
@@ -53,6 +54,131 @@ namespace CauldronTests
             AssertInTrash(Malichae, discard);
         }
 
+        [Test]
+        public void Incap1_DiscardDraw()
+        {
+            SetupGameController("BaronBlade", "Cauldron.Malichae", "Ra", "Fanatic", "Megalopolis");
+            StartGame();
+
+            SetupIncap(baron);
+            GoToUseIncapacitatedAbilityPhase(Malichae);
+
+            QuickHandStorage(Malichae, ra, fanatic);
+
+            var discard = GetCardFromHand(ra);
+
+            DecisionSelectTurnTaker = ra.TurnTaker;
+            DecisionDiscardCard = discard;
+
+            UseIncapacitatedAbility(Malichae, 0);
+            QuickHandCheck(0, 2, 0);
+            AssertInTrash(ra, discard);
+        }
+
+        [Test]
+        public void Incap1_NoDiscardNoDraw()
+        {
+            SetupGameController("BaronBlade", "Cauldron.Malichae", "Ra", "Fanatic", "Megalopolis");
+            StartGame();
+
+            SetupIncap(baron);
+            GoToUseIncapacitatedAbilityPhase(Malichae);
+
+            QuickHandStorage(Malichae, ra, fanatic);
+
+            DecisionSelectTurnTaker = ra.TurnTaker;
+            DecisionSelectCards = new Card[1];
+
+            UseIncapacitatedAbility(Malichae, 0);
+            QuickHandCheck(0, 0, 0);
+            AssertNumberOfCardsInTrash(Malichae, 0);
+        }
+
+        [Test]
+        public void Incap2()
+        {
+            SetupGameController("BaronBlade", "Cauldron.Malichae", "Ra", "Fanatic", "Megalopolis");
+            StartGame();
+
+            SetupIncap(baron);
+            GoToUseIncapacitatedAbilityPhase(Malichae);
+            var card = PlayCard(env, "TrafficPileup");
+            AssertInPlayArea(env, card);
+
+            DecisionSelectCard = card;
+
+            UseIncapacitatedAbility(Malichae, 1);
+
+            AssertNumberOfCardsInTrash(env, 1);
+        }
+
+        [Test]
+        public void Incap3_DamageTypeChanged()
+        {
+            SetupGameController("BaronBlade", "Cauldron.Malichae", "Ra", "Fanatic", "Megalopolis");
+            StartGame();
+
+            SetupIncap(baron);
+            GoToUseIncapacitatedAbilityPhase(Malichae);
+            var card = PlayCard(ra, "FleshOfTheSunGod");
+            AssertInPlayArea(ra, card);
+
+            DecisionSelectDamageType = DamageType.Fire;
+
+            UseIncapacitatedAbility(Malichae, 2);
+
+            QuickHPStorage(baron, ra, fanatic);
+            //If the damage type is changed, the damage will be prevented
+            DealDamage(fanatic, ra, 2, DamageType.Melee);
+            QuickHPCheck(0, 0, 0);
+
+            QuickHPStorage(baron, ra, fanatic);
+            //If the damage type is changed, the damage will be prevented
+            DealDamage(ra, ra, 2, DamageType.Melee);
+            QuickHPCheck(0, 0, 0);
+
+            QuickHPStorage(baron, ra, fanatic);
+            //villian damage is uneffected
+            DealDamage(baron, ra, 2, DamageType.Melee);
+            QuickHPCheck(0, -2, 0);
+        }
+
+        [Test]
+        public void Incap3_EffectExpires()
+        {
+            SetupGameController("BaronBlade", "Cauldron.Malichae", "Ra", "Fanatic", "Megalopolis");
+            StartGame();
+
+            SetupIncap(baron);
+            GoToUseIncapacitatedAbilityPhase(Malichae);
+            var card = PlayCard(ra, "FleshOfTheSunGod");
+            AssertInPlayArea(ra, card);
+
+            DecisionSelectDamageType = DamageType.Fire;
+
+            UseIncapacitatedAbility(Malichae, 2);
+
+            GoToEndOfTurn(Malichae);
+
+            GoToStartOfTurn(Malichae);
+
+            //effect is now expired
+
+            QuickHPStorage(baron, ra, fanatic);
+            //If the damage type is changed, the damage will be prevented
+            DealDamage(fanatic, ra, 2, DamageType.Melee);
+            QuickHPCheck(0, -2, 0);
+
+            QuickHPStorage(baron, ra, fanatic);
+            //If the damage type is changed, the damage will be prevented
+            DealDamage(ra, ra, 2, DamageType.Melee);
+            QuickHPCheck(0, -2, 0);
+
+            QuickHPStorage(baron, ra, fanatic);
+            //villian damage is uneffected
+            DealDamage(baron, ra, 2, DamageType.Melee);
+            QuickHPCheck(0, -2, 0);
+        }
 
     }
 }
