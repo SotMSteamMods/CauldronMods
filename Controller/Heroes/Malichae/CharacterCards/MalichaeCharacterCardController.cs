958531e45eee850daa1dc262ad04e394dc60e962
Vilnor <onlytelford@yahoo.com>
2020-11-08 20:22:02 -0500

    Malichae initial setup Innate Power

---
+++ Controller/Heroes/Malichae/CharacterCards/MalichaeCharacterCardController.cs
@@ -0,0 +1,59 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cauldron.Malichae
+{
+	public class MalichaeCharacterCardController : HeroCharacterCardController
+	{
+		public MalichaeCharacterCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+		{
+		}
+		public override IEnumerator UsePower(int index = 0)
+		{
+			var coroutine = base.DrawCards(this.DecisionMaker, 2);
+			if (base.UseUnityCoroutines)
+			{
+				yield return base.GameController.StartCoroutine(coroutine);
+			}
+			else
+			{
+				base.GameController.ExhaustCoroutine(coroutine);
+			}
+			coroutine = base.SelectAndDiscardCards(this.DecisionMaker, 1);
+			if (base.UseUnityCoroutines)
+			{
+				yield return base.GameController.StartCoroutine(coroutine);
+			}
+			else
+			{
+				base.GameController.ExhaustCoroutine(coroutine);
+			}
+			yield break;
+		}
+		public override IEnumerator UseIncapacitatedAbility(int index)
+		{
+			switch (index)
+			{
+				case 0:
+					{
+						
+						break;
+					}
+				case 1:
+					{
+						
+						yield break;
+					}
+				case 2:
+					{
+						
+						break;
+					}
+			}
+			yield break;
+		}
+	}
+}
---
+++ Testing/Heroes/MalichaeTests.cs
@@ -0,0 +1,58 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using Handelabra.Sentinels.UnitTest;
+using NUnit.Framework;
+using Cauldron.Malichae;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CauldronTests
+{
+    [TestFixture()]
+    public class MalichaeTests : BaseTest
+    {
+        #region MalichaeTestsHelperFunctions
+        protected HeroTurnTakerController Malichae { get { return FindHero("Malichae"); } }
+        private void SetupIncap(TurnTakerController villain)
+        {
+            SetHitPoints(Malichae.CharacterCard, 1);
+            DealDamage(villain, Malichae, 2, DamageType.Melee);
+        }
+
+        #endregion
+
+        [Test()]
+        public void TestMalichaeLoads()
+        {
+            SetupGameController("BaronBlade", "Cauldron.Malichae", "Megalopolis");
+
+            Assert.AreEqual(3, this.GameController.TurnTakerControllers.Count());
+
+            Assert.IsNotNull(Malichae);
+            Assert.IsInstanceOf(typeof(MalichaeCharacterCardController), Malichae.CharacterCardController);
+
+            Assert.AreEqual(27, Malichae.CharacterCard.HitPoints);
+        }
+
+        [Test]
+        public void InnatePower()
+        {
+            SetupGameController("BaronBlade", "Cauldron.Malichae", "Ra", "Fanatic", "Megalopolis");
+            StartGame();
+
+            GoToUsePowerPhase(Malichae);
+
+            QuickHandStorage(Malichae, ra, fanatic);
+            var discard = GetCardFromHand(Malichae);
+            DecisionDiscardCard = discard;
+            UsePower(Malichae);
+
+            QuickHandCheck(1, 0, 0);
+            AssertInTrash(Malichae, discard);
+        }
+
+
+    }
+}
