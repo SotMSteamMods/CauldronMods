a668dc8bf0031f17832209be04c98990f28d716a
Jake <mgirman520@gmail.com>
2020-11-08 22:37:44 -0500

    Create TemporalReversalCardController.cs

---
+++ Controller/Environments/FSCContinuanceWanderer/Cards/TemporalReversalCardController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+
+namespace Cauldron
+{
+    public class TemporalReversalCardController : CardController
+    {
+        #region Constructors
+
+        public TemporalReversalCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        public override IEnumerator Play()
+        {
+            //When this card enters play, place 1 card in play from each other deck back on top of that deck.
+            IEnumerator coroutine = base.DoActionToEachTurnTakerInTurnOrder((TurnTakerController turnTakerController) => turnTakerController.IsHero || turnTakerController.IsVillain, MoveCardToDeckResponse);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+        }
+
+        private IEnumerator MoveCardToDeckResponse(TurnTakerController turnTakerController)
+        {
+            IEnumerator coroutine = base.GameController.SelectAndReturnCards(this.DecisionMaker, new int?(1), new LinqCardCriteria((Card c) => !c.IsCharacter && c.Owner == turnTakerController.TurnTaker, "card in play"), false, true, false, null, cardSource: base.GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+        }
+
+        public override void AddTriggers()
+        {
+            //At the end of the environment turn, destroy this card.
+            base.AddEndOfTurnTrigger((TurnTaker turnTaker) => turnTaker == base.TurnTaker, new Func<PhaseChangeAction, IEnumerator>(base.DestroyThisCardResponse), TriggerType.DestroySelf);
+        }
+
+        #endregion Methods
+    }
+}
