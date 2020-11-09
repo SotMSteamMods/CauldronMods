b157098b8d2bc43face237f7966aa3f57bf2ca58
Jake <mgirman520@gmail.com>
2020-11-09 10:03:39 -0500

    Create VortexGlitchCardController.cs

---
+++ Controller/Environments/FSCContinuanceWanderer/Cards/VortexGlitchCardController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+
+namespace Cauldron
+{
+    public class VortexGlitchCardController : CardController
+    {
+        #region Constructors
+
+        public VortexGlitchCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        public override void AddTriggers()
+        {
+            //Players may not play one-shots.
+            base.CannotPlayCards((TurnTakerController turnTakerController) => turnTakerController.TurnTaker != null && turnTakerController.IsHero, (Card c) => c.IsHero && c.IsOneShot);
+            //When another environment card enters play, destroy this card.
+            base.AddTrigger<CardEntersPlayAction>((CardEntersPlayAction p) => p.CardEnteringPlay.IsEnvironment, (base.DestroyThisCardResponse), TriggerType.DestroySelf, TriggerTiming.After);
+        }
+
+        #endregion Methods
+    }
+}
