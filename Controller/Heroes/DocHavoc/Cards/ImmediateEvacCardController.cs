89868dbdb89b430d23c88a3ee70f9a1efbc4ec4a
Breen Young <breen.young@gmail.com>
2020-11-09 14:01:21 -0400

    optimize usings

--- Controller/Heroes/DocHavoc/Cards/ImmediateEvacCardController.cs
+++ Controller/Heroes/DocHavoc/Cards/ImmediateEvacCardController.cs
@@ -2,8 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using Handelabra.Sentinels.Engine.Controller;
 using Handelabra.Sentinels.Engine.Model;
 
