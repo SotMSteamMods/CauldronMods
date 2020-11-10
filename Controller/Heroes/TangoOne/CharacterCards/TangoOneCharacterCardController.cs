5c8a9f281e0a5de98afcff9f8288928dd797d43f
Breen Young <breen.young@gmail.com>
2020-11-10 17:17:53 -0400

    implement innate power

--- Controller/Heroes/TangoOne/CharacterCards/TangoOneCharacterCardController.cs
+++ Controller/Heroes/TangoOne/CharacterCards/TangoOneCharacterCardController.cs
@@ -1,17 +1,50 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using Handelabra.Sentinels.Engine.Controller;
 using Handelabra.Sentinels.Engine.Model;
 
-namespace CauldronMods.TangoOne
+namespace Cauldron.TangoOne
 {
     public class TangoOneCharacterCardController : HeroCharacterCardController
     {
+        private const int PowerDamageAmount = 1;
+
         public TangoOneCharacterCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
+
+        }
+
+        public override IEnumerator UsePower(int index = 0)
+        {
+            //==============================================================
+            // {TangoOne} deals 1 target 1 projectile damage.
+            //==============================================================
+
+            DamageSource damageSource = new DamageSource(base.GameController, base.CharacterCard);
+            int powerNumeral = base.GetPowerNumeral(0, PowerDamageAmount);
+
+            IEnumerator routine = base.GameController.SelectTargetsAndDealDamage(this.DecisionMaker, damageSource, powerNumeral,
+                DamageType.Projectile, new int?(1), false, new int?(1),
+                cardSource: base.GetCardSource(null));
+
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(routine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(routine);
+            }
+        }
+
+        public override IEnumerator UseIncapacitatedAbility(int index)
+        {
+            return base.UseIncapacitatedAbility(index);
         }
     }
 }
