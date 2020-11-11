7d02fd3ed487575e596a8a4bd5a5cd0ccb5454fb
Vilnor <onlytelford@yahoo.com>
2020-11-11 09:53:55 -0500

    Friendly Advice - No Tests

--- Controller/Heroes/Malichae/Cards/FriendlyAdviceCardController.cs
+++ Controller/Heroes/Malichae/Cards/FriendlyAdviceCardController.cs
@@ -6,14 +6,45 @@ using System.Linq;
 
 namespace Cauldron.Malichae
 {
-	public class FriendlyAdviceCardController : MalichaeCardController
-	{
-		public FriendlyAdviceCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
-		{
-		}
-		public override IEnumerator Play()
-		{
-			yield break;
-		}
-	}
+    public class FriendlyAdviceCardController : MalichaeCardController
+    {
+        public FriendlyAdviceCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+        }
+        public override IEnumerator Play()
+        {
+            var coroutine = GameController.SelectAndGainHP(this.DecisionMaker, 2, false,
+                                additionalCriteria: c => c.IsChallenge && c.IsInPlay,
+                                numberOfTargets: 1,
+                                requiredDecisions: 1,
+                                cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            coroutine = GameController.SelectHeroToDrawCard(this.DecisionMaker, numberOfCards: 1, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            coroutine = GameController.SelectHeroToMoveCardFromTrash(this.DecisionMaker, httc => httc.HeroTurnTaker.Deck, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+    }
 }
