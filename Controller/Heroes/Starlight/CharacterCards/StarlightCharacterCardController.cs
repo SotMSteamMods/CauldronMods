4c28d86ad9d3508a531d4d6db683e60fa6ec1103
LNBertolini <bertolini.lucas.n@gmail.com>
2020-11-12 20:23:48 -0600

    Starlight innate power and tests

--- Controller/Heroes/Starlight/CharacterCards/StarlightCharacterCardController.cs
+++ Controller/Heroes/Starlight/CharacterCards/StarlightCharacterCardController.cs
@@ -15,6 +15,15 @@ namespace Cauldron.Starlight
         public override IEnumerator UsePower(int index = 0)
         {
             //Draw a card, or play a Constellation from your trash
+            IEnumerator coroutine = DrawACardOrPlayConstellationFromTrash();
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
             yield break;
         }
 
@@ -42,6 +51,63 @@ namespace Cauldron.Starlight
 
 
             yield break;
+        }
+
+        private IEnumerator DrawACardOrPlayConstellationFromTrash()
+        {
+            List<Function> list = new List<Function>();
+            string str = "so they must draw a card.";
+            string str2 = "so they must play a constellation from their trash.";
+            string str3 = ", so the power has no effect.";
+            string str4 = ((DecisionMaker == null) ? Card.Title : DecisionMaker.TurnTaker.Name);
+            if ((DecisionMaker != null && DecisionMaker.TurnTaker.Identifier == "Guise") || Card.Identifier == "GuiseCharacter")
+            {
+                str = "so he's gotta draw one. Woo! Free card!";
+                str2 = "so he's gotta play one. Make sure it's a good one!";
+                str3 = ". Bummer!";
+            }
+            list.Add(new Function(DecisionMaker, "Draw a card", SelectionType.DrawCard, () => DrawCard(DecisionMaker.HeroTurnTaker, false), DecisionMaker != null && CanDrawCards(DecisionMaker), str4 + " cannot play any cards, " + str));
+            
+            list.Add(new Function(DecisionMaker, "Play a constellation from your trash", SelectionType.PlayCard, () => SelectAndPlayConstellationFromTrash(DecisionMaker, false), DecisionMaker != null && GetPlayableConstellationsInTrash().Count() > 0, str4 + " cannot draw any cards, " + str2));
+
+            SelectFunctionDecision selectFunction = new SelectFunctionDecision(GameController, DecisionMaker, list, false, null, str4 + " cannot draw nor play any cards" + str3, null, GetCardSource());
+            IEnumerator coroutine = GameController.SelectAndPerformFunction(selectFunction);
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+        }
+
+        private IEnumerator SelectAndPlayConstellationFromTrash(HeroTurnTakerController hero, bool optional)
+        {
+            IEnumerator coroutine = GameController.SelectAndPlayCard(hero, GetPlayableConstellationsInTrash());
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+
+        private IEnumerable<Card> GetPlayableConstellationsInTrash()
+        {
+            return DecisionMaker.HeroTurnTaker.Trash.Cards.Where((Card card) => IsConstellation(card) && GameController.CanPlayCard(FindCardController(card), false, null, false, true) == CanPlayCardResult.CanPlay);  
+        }
+
+        private bool IsConstellation(Card card)
+        {
+            if (card != null)
+            {
+                return GameController.DoesCardContainKeyword(card, "constellation");
+            }
+            return false;
         }
     }
 }
--- Testing/Heroes/StarlightTests.cs
+++ Testing/Heroes/StarlightTests.cs
@@ -29,5 +29,103 @@ namespace CauldronTests
 
             Assert.AreEqual(31, starlight.CharacterCard.HitPoints);
         }
+        [Test()]
+        public void TestStarlightPowerMayDrawCards()
+        {
+            SetupGameController("BaronBlade", "Cauldron.Starlight", "Megalopolis");
+            StartGame();
+
+            PutInTrash("AncientConstellationA");
+
+            DecisionSelectFunction = 0;
+
+            QuickHandStorage(starlight);
+            UsePower(starlight);
+            QuickHandCheck(1);
+            AssertNumberOfCardsInTrash(starlight, 1);
+        }
+
+        [Test()]
+        public void TestStarlightPowerMayPlayTrashConstellation()
+        {
+            SetupGameController("BaronBlade", "Cauldron.Starlight", "Megalopolis");
+            StartGame();
+
+            PutInTrash("AncientConstellationA");
+
+            DecisionSelectFunction = 1;
+
+            QuickHandStorage(starlight);
+            UsePower(starlight);
+            QuickHandCheck(0);
+            AssertNumberOfCardsInTrash(starlight, 0);
+        }
+        [Test()]
+        public void TestStarlightPowerPlaysOnlyOneConstellation()
+        {
+            SetupGameController("BaronBlade", "Cauldron.Starlight", "Megalopolis");
+            StartGame();
+
+            PutInTrash("AncientConstellationA", "AncientConstellationB", "NightloreArmor");
+
+            DecisionSelectFunction = 1;
+
+            QuickHandStorage(starlight);
+            AssertNumberOfCardsInTrash(starlight, 3);
+            UsePower(starlight);
+            QuickHandCheck(0);
+            AssertNumberOfCardsInTrash(starlight, 2);
+        }
+
+        [Test()]
+        public void TestStarlightPowerMustDrawWithNoConstellationsInTrash()
+        {
+            SetupGameController("BaronBlade", "Cauldron.Starlight", "Megalopolis");
+            StartGame();
+
+            PutInTrash("NightloreArmor");
+
+            //if we get a decision, we will try to play from trash
+            DecisionSelectFunction = 1;
+
+            QuickHandStorage(starlight);
+            UsePower(starlight);
+            QuickHandCheck(1);
+        }
+        [Test()]
+        public void TestStarlightPowerMustDrawWhenNotAbleToPlayCards()
+        {
+            SetupGameController("BaronBlade", "Cauldron.Starlight", "Megalopolis");
+            StartGame();
+
+            PutInTrash("NightloreArmor", "AncientConstellationA");
+            PutIntoPlay("HostageSituation");
+
+            //if we get a decision, we will try to play from trash
+            DecisionSelectFunction = 1;
+
+            QuickHandStorage(starlight);
+            UsePower(starlight);
+            QuickHandCheck(1);
+        }
+
+        [Test()]
+        public void TestStarlightPowerMustPlayTrashConstellationWhenNotAbleToDraw()
+        {
+            SetupGameController("BaronBlade", "Cauldron.Starlight", "Megalopolis");
+            StartGame();
+
+            PutInTrash("AncientConstellationB");
+            PutIntoPlay("TrafficPileup");
+
+            //if we get a decision we will try to draw
+            DecisionSelectFunction = 0;
+
+            AssertNumberOfCardsInTrash(starlight, 1);
+            UsePower(starlight);
+            AssertNumberOfCardsInTrash(starlight, 0);
+            AssertNumberOfCardsInPlay(starlight, 2);
+
+        }
     }
 }
