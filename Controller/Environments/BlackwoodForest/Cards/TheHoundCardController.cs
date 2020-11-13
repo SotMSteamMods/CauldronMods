ad77b60a22f6d3a7129856a8d4dfb3beaed6bf44
Breen Young <breen.young@gmail.com>
2020-11-13 17:09:09 -0400

    code cleanup

--- Controller/Environments/BlackwoodForest/Cards/TheHoundCardController.cs
+++ Controller/Environments/BlackwoodForest/Cards/TheHoundCardController.cs
@@ -1,9 +1,9 @@
-﻿using Handelabra.Sentinels.Engine.Controller;
-using Handelabra.Sentinels.Engine.Model;
-using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
 
 namespace Cauldron.BlackwoodForest
 {
