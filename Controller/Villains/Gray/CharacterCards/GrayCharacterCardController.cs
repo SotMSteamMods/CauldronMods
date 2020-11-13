007fdef762d536928ccf747af6508060d7972d83
Jake <mgirman520@gmail.com>
2020-11-13 15:55:04 -0500

    Finalize Gray Character Card Controller

--- Controller/Villains/Gray/CharacterCards/GrayCharacterCardController.cs
+++ Controller/Villains/Gray/CharacterCards/GrayCharacterCardController.cs
@@ -1,6 +1,7 @@
 ﻿using Handelabra.Sentinels.Engine.Controller;
 using Handelabra.Sentinels.Engine.Model;
-using System;
+using System.Collections;
+using System.Linq;
 
 namespace Cauldron.Gray
 {
@@ -10,6 +11,163 @@ namespace Cauldron.Gray
         public GrayCharacterCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
 
+        }
+        public override void AddSideTriggers()
+        {
+            //Front - Walking Nuclear Reactor
+            if (!base.Card.IsFlipped)
+            {
+                //At the end of the villain turn, if there are 3 or more radiation cards in play, flip {Gray}'s villain character cards and destroy 1 environment card.
+                base.AddSideTrigger(base.AddEndOfTurnTrigger((TurnTaker turnTaker) => turnTaker == base.TurnTaker, FlipCardResponse, TriggerType.FlipCard, additionalCriteria: (PhaseChangeAction action) => this.FindNumberOfRadiationCardsInPlay() >= 3));
+                //At the end of the villain turn, {Gray} deals the hero target with the highest HP {H -1} energy damage.
+                base.AddSideTrigger(base.AddDealDamageAtEndOfTurnTrigger(base.TurnTaker, base.Card, (Card c) => c.IsHero, TargetType.HighestHP, Game.H - 1, DamageType.Energy));
+                //Whenever a radiation card is destroyed, destroy 1 hero ongoing or equipment card and gray deals each non-villain target {H - 1} energy damage.
+                //Advanced - Whenever a radiation card is destroyed, destroy a second hero ongoing or equipment card.
+                base.AddSideTrigger(base.AddTrigger<DestroyCardAction>((DestroyCardAction action) => action.WasCardDestroyed && action.CardToDestroy.Card.DoKeywordsContain("radiation"), this.DestroyRadiationFrontResponse, new TriggerType[] { TriggerType.DestroyCard, TriggerType.DealDamage }, TriggerTiming.After));
+            }
+            //Back - Catastrophic Meltdown
+            else
+            {
+                //At the start of the villain turn, if there are 1 or 0 radiation cards in play, flip {Gray}'s character cards.
+                base.AddSideTrigger(base.AddStartOfTurnTrigger((TurnTaker turnTaker) => turnTaker == base.TurnTaker, FlipCardResponse, TriggerType.FlipCard, additionalCriteria: (PhaseChangeAction action) => this.FindNumberOfRadiationCardsInPlay() <= 1));
+                //At the start of the villain turn, destroy all but 2 hero ongoing or equipment cards. {Gray} deals each hero target {H x 2} energy damage. Play the top card of the villain deck.
+                base.AddSideTrigger(base.AddStartOfTurnTrigger((TurnTaker turnTaker) => turnTaker == base.TurnTaker, this.FlippedStartOfTurnResponse, new TriggerType[] { TriggerType.DestroyCard, TriggerType.DealDamage, TriggerType.PlayCard }));
+                //Whenever a radiation card is destroyed by a hero card, {Gray} deals that hero {H - 1} energy damage.
+                //Whenever a copy of Radioactive Cascade is destroyed, {Gray} deals the hero with the highest HP {H - 1} energy damage.
+                base.AddSideTrigger(base.AddTrigger<DestroyCardAction>((DestroyCardAction action) => action.WasCardDestroyed && action.CardToDestroy.Card.DoKeywordsContain("radiation"), this.DestroyRadiationBackResponse, TriggerType.DealDamage, TriggerTiming.After));
+                //Advanced - Reduce damage dealt to villain targets by 1.
+                if (Game.IsAdvanced)
+                {
+                    base.AddSideTrigger(base.AddReduceDamageTrigger((Card c) => c.IsVillain, 1));
+                }
+            }
+        }
+
+        private int? FindNumberOfRadiationCardsInPlay()
+        {
+            return new int?(base.FindCardsWhere((Card c) => c.IsInPlayAndHasGameText && c.DoKeywordsContain("radiation"), false, null, false).Count<Card>());
+        }
+
+        private IEnumerator FlipCardResponse(PhaseChangeAction action)
+        {
+            IEnumerator coroutine = base.GameController.FlipCard(this, cardSource: base.GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            //If flipping from front to back then destroy 1 environment after flipping
+            if (base.Card.IsFlipped)
+            {
+                coroutine = base.GameController.SelectAndDestroyCard(this.DecisionMaker, new LinqCardCriteria((Card c) => c.IsEnvironment), false);
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+            }
+            yield break;
+        }
+
+        private IEnumerator DestroyRadiationFrontResponse(DestroyCardAction action)
+        {
+            //...destroy 1 hero ongoing or equipment card and {Gray} deals each non-villain target {H - 1} energy damage
+            IEnumerator coroutine = base.GameController.SelectAndDestroyCard(this.DecisionMaker, new LinqCardCriteria((Card c) => c.IsHero && (c.IsOngoing || base.IsEquipment(c))), false);
+            IEnumerator coroutine2 = base.DealDamage(base.Card, (Card c) => c.IsNonVillainTarget, Game.H - 1, DamageType.Energy);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+                yield return base.GameController.StartCoroutine(coroutine2);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+                base.GameController.ExhaustCoroutine(coroutine2);
+            }
+            //Advanced - Whenever a radiation card is destroyed, destroy a second hero ongoing or equipment card.
+            if (Game.IsAdvanced)
+            {
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+            }
+            yield break;
+        }
+
+        private int? FindNumberOfHeroOngoingAndEquipmentInPlay()
+        {
+            return new int?(base.FindCardsWhere((Card c) => c.IsInPlayAndHasGameText && c.IsHero && (c.IsOngoing || base.IsEquipment(c)), false, null, false).Count<Card>());
+        }
+
+        private IEnumerator FlippedStartOfTurnResponse(PhaseChangeAction action)
+        {
+            //...destroy all but 2 hero ongoing or equipment cards. 
+            IEnumerator coroutine = base.GameController.SelectAndDestroyCard(this.DecisionMaker, new LinqCardCriteria((Card c) => c.IsHero && (c.IsOngoing || base.IsEquipment(c))), false);
+            while (this.FindNumberOfHeroOngoingAndEquipmentInPlay() > 2)
+            {
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+            }
+            //...{Gray} deals each hero target {H x 2} energy damage.
+            coroutine = base.GameController.DealDamage(this.DecisionMaker, base.Card, (Card c) => c.IsHero, Game.H * 2, DamageType.Energy, cardSource: base.GetCardSource());
+            //...Play the top card of the villain deck.
+            IEnumerator coroutine2 = base.GameController.PlayTopCardOfLocation(base.TurnTakerController, base.TurnTaker.Deck);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+                yield return base.GameController.StartCoroutine(coroutine2);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+                base.GameController.ExhaustCoroutine(coroutine2);
+            }
+            yield break;
+        }
+
+        private IEnumerator DestroyRadiationBackResponse(DestroyCardAction action)
+        {
+            //Whenever a radiation card is destroyed by a hero card, {Gray} deals that hero {H - 1} energy damage.
+            IEnumerator coroutine = base.DealDamage(base.Card, action.ResponsibleCard.Owner.CharacterCard, Game.H - 1, DamageType.Energy);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            //Whenever a copy of Radioactive Cascade is destroyed, {Gray} deals the hero with the highest HP {H - 1} energy damage.
+            if (action.CardToDestroy.Card.Identifier == "")
+            {
+                coroutine = base.DealDamageToHighestHP(base.Card, 1, (Card c) => c.IsHero, (Card c) => new int?(Game.H - 1), DamageType.Energy);
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+            }
+            yield break;
         }
     }
 }
