590436c66223847b218ca3c8a10b638f9fb8736a
Jake <mgirman520@gmail.com>
2020-11-13 13:31:00 -0500

    Create Unwitting Henchmen and Unstable Isotope

---
+++ Controller/Villains/Gray/Cards/UnstableIsotopeCardController.cs
@@ -0,0 +1,30 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+
+namespace Cauldron
+{
+    public class UnstableIsotopeCardController : CardController
+    {
+        public UnstableIsotopeCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+
+        public override IEnumerator Play()
+        {
+            //Reveal cards from the top of the villain deck until 2 Radiation cards are revealed. Put those cards into play and discard the rest.
+            IEnumerator coroutine = base.RevealCards_PutSomeIntoPlay_DiscardRemaining(this.DecisionMaker, base.TurnTaker.Deck, null, new LinqCardCriteria((Card c) => c.DoKeywordsContain("radiation")), revealUntilNumberOfMatchingCards: new int?(2));
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+    }
+}
---
+++ Controller/Villains/Gray/Cards/UnwittingHenchmenCardController.cs
@@ -0,0 +1,58 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cauldron
+{
+    public class UnwittingHenchmenCardController : CardController
+    {
+        public UnwittingHenchmenCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+
+        public override void AddTriggers()
+        {
+            base.AddEndOfTurnTrigger((TurnTaker turnTaker) => turnTaker == base.TurnTaker, this.DestroyEquipmentResponse, new TriggerType[] { TriggerType.DestroyCard, TriggerType.DealDamage, TriggerType.GainHP });
+        }
+
+        private IEnumerator DestroyEquipmentResponse(PhaseChangeAction action)
+        {
+            List<DestroyCardAction> storedResults = new List<DestroyCardAction>();
+            //At the end of the villain turn, destroy 1 equipment card.
+            IEnumerator coroutine = base.GameController.SelectAndDestroyCard(this.DecisionMaker, new LinqCardCriteria((Card c) => base.IsEquipment(c)), false, storedResults, cardSource: base.GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            //If a card is destroyed this way 
+            DestroyCardAction destroyAction = storedResults.FirstOrDefault<DestroyCardAction>();
+            if (destroyAction.WasCardDestroyed)
+            {
+                //...{Gray} regains 3 HP...
+                coroutine = base.GameController.GainHP(base.CharacterCard, new int?(3), cardSource: base.GetCardSource());
+            }
+            else
+            {
+                //...Otherwise this card deals the hero target with the highest HP 1 melee damage.
+                coroutine = base.DealDamageToHighestHP(base.Card, 1, (Card c) => c.IsHero, (Card c) => 1, DamageType.Melee);
+            }
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+    }
+}
