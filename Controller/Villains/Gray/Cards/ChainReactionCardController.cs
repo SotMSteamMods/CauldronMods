c462f7dfa3d6dce8b75608189cc94a7954962d8f
Jake <mgirman520@gmail.com>
2020-11-13 09:21:14 -0500

    Add Special String to Chain Reaction

--- Controller/Villains/Gray/Cards/ChainReactionCardController.cs
+++ Controller/Villains/Gray/Cards/ChainReactionCardController.cs
@@ -1,5 +1,6 @@
 ﻿using Handelabra.Sentinels.Engine.Controller;
 using Handelabra.Sentinels.Engine.Model;
+using Handelabra;
 using System;
 using System.Collections;
 using System.Linq;
@@ -10,14 +11,16 @@ namespace Cauldron
     {
         public ChainReactionCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
-
+            base.SpecialStringMaker.ShowNumberOfCards(new LinqCardCriteria((Card c) => c.IsInPlayAndHasGameText && c.DoKeywordsContain("radiation"), "radiation"));
+            base.SpecialStringMaker.ShowHeroTargetWithLowestHP(numberOfTargets: (new int?(base.FindCardsWhere((Card c) => c.IsInPlayAndHasGameText && c.DoKeywordsContain("radiation"), false, null, false).Count<Card>()) ?? default));
         }
+
+
 
         public override void AddTriggers()
         {
             //At the start of the villain turn, this card deals the X hero targets with the lowest HP 1 energy damage each, where X is the number of Radiation cards in play.
-            Func<Card, int?> X = (Card card) => new int?(base.FindCardsWhere((Card c) => c.IsInPlayAndHasGameText && c.DoKeywordsContain("radiation"), false, null, false).Count<Card>());
-            base.AddDealDamageAtStartOfTurnTrigger(base.TurnTaker, base.Card, (Card c) => c.IsHero && c.IsTarget && c.IsInPlayAndHasGameText, TargetType.LowestHP, 0, DamageType.Energy, dynamicAmount: X);
+            base.AddDealDamageAtStartOfTurnTrigger(base.TurnTaker, base.Card, (Card c) => c.IsHero && c.IsTarget && c.IsInPlayAndHasGameText, TargetType.LowestHP, 0, DamageType.Energy, dynamicAmount: (Card card) => FindNumberOfRadiationCardsInPlay());
             //At the end of the villain turn, put a random Radiation card from the villain trash into play.
             base.AddEndOfTurnTrigger((TurnTaker turnTaker) => turnTaker == base.TurnTaker, new Func<PhaseChangeAction, IEnumerator>(this.BringRadiationBackResponse), TriggerType.PutIntoPlay);
 
@@ -54,6 +57,11 @@ namespace Cauldron
                 }
             }
             yield break;
+        }
+
+        private int? FindNumberOfRadiationCardsInPlay()
+        {
+            return new int?(base.FindCardsWhere((Card c) => c.IsInPlayAndHasGameText && c.DoKeywordsContain("radiation"), false, null, false).Count<Card>());
         }
     }
 }
---
+++ Controller/Villains/Gray/Cards/HeavyRadiationCardController.cs
@@ -0,0 +1,16 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+
+namespace Cauldron
+{
+    public class HeavyRadiationCardController : CardController
+    {
+        public HeavyRadiationCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+        //Reduce damage dealt to {Gray} by 1 for each Radiation card in play.",
+        //At the end of the villain turn, if there are no Radiation cards in play, play the top card of the villain deck.
+    }
+}
