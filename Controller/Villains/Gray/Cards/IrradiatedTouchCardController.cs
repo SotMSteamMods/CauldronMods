5b79054ca16a90e53414740c7f02f5ca104cc010
Jake <mgirman520@gmail.com>
2020-11-13 10:07:16 -0500

    Create IrradiatedTouchCardController.cs

---
+++ Controller/Villains/Gray/Cards/IrradiatedTouchCardController.cs
@@ -0,0 +1,42 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Cauldron
+{
+    public class IrradiatedTouchCardController : CardController
+    {
+        public IrradiatedTouchCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+        public override void AddTriggers()
+        {
+            //At the end of the villain turn, {Gray} deals the hero target with the second highest HP {H - 2} melee and {H - 2} energy damage.
+            base.AddEndOfTurnTrigger((TurnTaker turnTaker) => turnTaker == base.TurnTaker, DealDamageResponse, new TriggerType[] { TriggerType.DealDamage });
+            //When this card is destroyed, {Gray} deals the hero target with the highest HP 2 energy damage.
+            base.AddWhenDestroyedTrigger((DestroyCardAction action) => base.DealDamageToHighestHP(base.CharacterCard, 1, (Card c) => c.IsHero, (Card c) => new int?(2), DamageType.Energy), TriggerType.DealDamage);
+        }
+
+        private IEnumerator DealDamageResponse(PhaseChangeAction action)
+        {
+            //{Gray} deals the hero target with the second highest HP {H - 2} melee and {H - 2} energy damage.
+            IEnumerator coroutine = base.DealMultipleInstancesOfDamageToHighestLowestHP(new List<DealDamageAction>
+            {
+                new DealDamageAction(base.GetCardSource(),new DamageSource(base.GameController,base.CharacterCard),null,Game.H -2,DamageType.Melee),
+                new DealDamageAction(base.GetCardSource(),new DamageSource(base.GameController,base.CharacterCard),null,Game.H -2,DamageType.Energy)
+            }, (Card c) => c.IsHero, HighestLowestHP.HighestHP, 2);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+    }
+}
