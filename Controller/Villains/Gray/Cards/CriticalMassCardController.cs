ed5a1671e4933b8de38205c09b72a0dcb126938c
Jake <mgirman520@gmail.com>
2020-11-13 08:48:36 -0500

    Update CriticalMassCardController.cs

--- Controller/Villains/Gray/Cards/CriticalMassCardController.cs
+++ Controller/Villains/Gray/Cards/CriticalMassCardController.cs
@@ -1,7 +1,9 @@
-﻿using Handelabra.Sentinels.Engine.Controller;
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
 using Handelabra.Sentinels.Engine.Model;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace Cauldron
 {
@@ -11,11 +13,32 @@ namespace Cauldron
         {
 
         }
-        //Search the villain deck and trash for all copies of Chain Reaction and put them into play. Move 1 copy of Unstable Isotope from the villain trash to the villain deck. Shuffle the villain deck.
         public override IEnumerator Play()
         {
+            //Search the villain deck and trash for all copies of Chain Reaction and put them into play.
+            IEnumerator coroutine = base.PlayCardsFromLocation(base.TurnTaker.Deck, new LinqCardCriteria((Card c) => c.Identifier == "ChainReaction"));
+            IEnumerator coroutine2 = base.PlayCardsFromLocation(base.TurnTaker.Trash, new LinqCardCriteria((Card c) => c.Identifier == "ChainReaction"));
+            //Move 1 copy of Unstable Isotope from the villain trash to the villain deck. 
+            MoveCardDestination villainTrash = new MoveCardDestination(base.TurnTaker.Trash);
+            IEnumerator coroutine3 = base.GameController.SelectCardFromLocationAndMoveIt(base.DecisionMaker, base.TurnTaker.Trash, new LinqCardCriteria((Card c) => c.Identifier == "UnstableIsotope"), villainTrash.ToEnumerable<MoveCardDestination>(), cardSource: base.GetCardSource());
+            //Shuffle the villain deck.
+            IEnumerator coroutine4 = base.ShuffleDeck(this.DecisionMaker, base.TurnTaker.Deck);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+                yield return base.GameController.StartCoroutine(coroutine2);
+                yield return base.GameController.StartCoroutine(coroutine3);
+                yield return base.GameController.StartCoroutine(coroutine4);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+                base.GameController.ExhaustCoroutine(coroutine2);
+                base.GameController.ExhaustCoroutine(coroutine3);
+                base.GameController.ExhaustCoroutine(coroutine4);
+            }
             //{Gray} deals himself 2 energy damage.
-            IEnumerator coroutine = base.DealDamage(base.CharacterCard, base.CharacterCard, 2, DamageType.Energy, cardSource: base.GetCardSource());
+            coroutine = base.DealDamage(base.CharacterCard, base.CharacterCard, 2, DamageType.Energy, cardSource: base.GetCardSource());
             if (base.UseUnityCoroutines)
             {
                 yield return base.GameController.StartCoroutine(coroutine);
