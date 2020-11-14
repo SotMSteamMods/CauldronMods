20721d36df7a702f8f58cc00de847511e5f1b3e1
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2020-11-14 08:50:07 -0600

    Created GhostCardController
    Refactored all of the affected logic into a GhostCardController

--- Controller/Environments/StSimeonsCatacombs/Cards/BreathStealerCardController.cs
+++ Controller/Environments/StSimeonsCatacombs/Cards/BreathStealerCardController.cs
@@ -7,14 +7,12 @@ using Handelabra.Sentinels.Engine.Model;
 
 namespace Cauldron.StSimeonsCatacombs
 {
-    public class BreathStealerCardController : CardController
+    public class BreathStealerCardController : GhostCardController
     {
         #region Constructors
 
-        public BreathStealerCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        public BreathStealerCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController, "Aqueducts")
         {
-            //TODO: Add a conditional special string that says something like "Aqueduct is in play so it is affected by Hero cards" and the reverse
-            base.AddThisCardControllerToList(CardControllerListType.ChangesVisibility);
         }
 
         #endregion Constructors
@@ -63,49 +61,11 @@ namespace Cauldron.StSimeonsCatacombs
             IEnumerator dealDamage = base.DealDamage(base.Card, base.GetCardThisCardIsNextTo(), 1, DamageType.Toxic, cardSource: base.GetCardSource());
             base.AddEndOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, (PhaseChangeAction pca) => dealDamage, TriggerType.DealDamage);
 
-            //This card may not be affected by hero cards unless Aqueducts is in play.
-            base.AddTrigger<MakeDecisionsAction>((MakeDecisionsAction md) => !this.IsAqueductsInPlay() && md.CardSource != null && md.CardSource.Card.Owner.IsHero, new Func<MakeDecisionsAction, IEnumerator>(this.RemoveDecisionsFromMakeDecisionsResponse), TriggerType.RemoveDecision, TriggerTiming.Before);
+            base.AddTriggers();
 
         }
 
-        private IEnumerator RemoveDecisionsFromMakeDecisionsResponse(MakeDecisionsAction md)
-        {
-            //remove this card as an option to make decisions
-            md.RemoveDecisions((IDecision d) =>d.SelectedCard == base.Card);
-            yield return base.DoNothing();
-            yield break;
-        }
-
-        public override bool? AskIfCardIsVisibleToCardSource(Card card, CardSource cardSource)
-        {
-            //check if card is from a hero and if aqueducts is in play
-            if (!this.IsAqueductsInPlay() && card == base.Card)
-            {
-                return new bool?(false);
-            }
-            return new bool?(true);
-        }
-
-
-        public override bool AskIfActionCanBePerformed(GameAction g)
-        {
-            //if aqueducts is not in play, actions from hero cards cannot affect this card
-            if (!this.IsAqueductsInPlay())
-            {
-                bool? flag = g.DoesFirstCardAffectSecondCard((Card c) => c.IsHero, (Card c) => c == base.Card);
-
-                if (flag != null && flag.Value)
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        private bool IsAqueductsInPlay()
-        {
-            return base.FindCardsWhere(c => c.IsInPlayAndHasGameText && c.Identifier == "Aqueducts").Count() > 0;
-        }
+       
 
         #endregion Methods
     }
---
+++ Controller/Environments/StSimeonsCatacombs/Cards/GhostCardController.cs
@@ -0,0 +1,71 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Linq;
+
+namespace Cauldron
+{
+    public class GhostCardController : CardController
+    {
+        #region Constructors
+
+        public GhostCardController(Card card, TurnTakerController turnTakerController, string affectedIdentifier) : base(card, turnTakerController)
+        {
+            this.AffectedIdentifier = affectedIdentifier;
+            //TODO: Add a conditional special string that says something like "Aqueduct is in play so it is affected by Hero cards" and the reverse
+            base.AddThisCardControllerToList(CardControllerListType.ChangesVisibility);
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        public override void AddTriggers()
+        {
+            //This card may not be affected by hero cards unless AffectedCard is in play.
+            base.AddTrigger<MakeDecisionsAction>((MakeDecisionsAction md) => !this.IsAffectedCardInPlay() && md.CardSource != null && md.CardSource.Card.Owner.IsHero, new Func<MakeDecisionsAction, IEnumerator>(this.RemoveDecisionsFromMakeDecisionsResponse), TriggerType.RemoveDecision, TriggerTiming.Before);
+        }
+
+        private IEnumerator RemoveDecisionsFromMakeDecisionsResponse(MakeDecisionsAction md)
+        {
+            //remove this card as an option to make decisions
+            md.RemoveDecisions((IDecision d) => d.SelectedCard == base.Card);
+            yield return base.DoNothing();
+            yield break;
+        }
+
+        public override bool? AskIfCardIsVisibleToCardSource(Card card, CardSource cardSource)
+        {
+            //check if card is from a hero and if AffectedCard is in play
+            if (!this.IsAffectedCardInPlay() && card == base.Card)
+            {
+                return new bool?(false);
+            }
+            return new bool?(true);
+        }
+
+
+        public override bool AskIfActionCanBePerformed(GameAction g)
+        {
+            //if AffectedCard is not in play, actions from hero cards cannot affect this card
+            if (!this.IsAffectedCardInPlay())
+            {
+                bool? flag = g.DoesFirstCardAffectSecondCard((Card c) => c.IsHero, (Card c) => c == base.Card);
+
+                if (flag != null && flag.Value)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private bool IsAffectedCardInPlay()
+        {
+            return base.FindCardsWhere(c => c.IsInPlayAndHasGameText && c.Identifier == AffectedIdentifier).Count() > 0;
+        }
+
+        public string AffectedIdentifier { get; private set; }
+        #endregion Methods
+    }
+}
