444be5c971e71caae4197429a52e812766c23495
Vilnor <onlytelford@yahoo.com>
2020-11-15 17:52:43 -0500

    Vanish Controller Framework

---
+++ Controller/Heroes/Vanish/Cards/ConcussiveBurstCardController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+
+namespace Cauldron.Vanish
+{
+    public class ConcussiveBurstCardController : CardController
+    {
+        public ConcussiveBurstCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+    }
+}
---
+++ Controller/Heroes/Vanish/Cards/FlickeringStrikeCardController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+
+namespace Cauldron.Vanish
+{
+    public class FlickeringStrikeCardController : CardController
+    {
+        public FlickeringStrikeCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+    }
+}
