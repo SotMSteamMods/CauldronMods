99ac89cafe6fdfd50040c46cd7666235291e905d
Vilnor <onlytelford@yahoo.com>
2020-11-15 17:52:43 -0500

    Vanish Controller Framework

---
+++ Controller/Heroes/Vanish/Cards/FlashReconCardController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+
+namespace Cauldron.Vanish
+{
+    public class FlashReconCardController : CardController
+    {
+        public FlashReconCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+    }
+}
---
+++ Testing/Heroes/VanishTests.cs
@@ -0,0 +1,41 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using Handelabra.Sentinels.UnitTest;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Cauldron.Vanish;
+
+namespace CauldronTests
+{
+    [TestFixture()]
+    public class VanishTests : BaseTest
+    {
+        #region HelperFunctions
+        protected HeroTurnTakerController vanish { get { return FindHero("Vanish"); } }
+        private void SetupIncap(TurnTakerController villain)
+        {
+            SetHitPoints(vanish.CharacterCard, 1);
+            DealDamage(villain, vanish, 2, DamageType.Melee);
+        }
+
+        #endregion HelperFunctions
+
+        [Test()]
+        public void TestLoadVanish()
+        {
+            SetupGameController("BaronBlade", "Cauldron.Vanish", "Legacy", "Bunker", "TheScholar", "Megalopolis");
+
+            Assert.AreEqual(6, this.GameController.TurnTakerControllers.Count());
+
+            Assert.IsNotNull(vanish);
+            Assert.IsInstanceOf(typeof(VanishCharacterCardController), vanish.CharacterCardController);
+
+            Assert.AreEqual(26, vanish.CharacterCard.HitPoints);
+        }
+
+    }
+}
