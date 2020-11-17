95c1ebc9441f9c7c7f2e0c34fd082431c7e7b650
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2020-11-17 16:28:39 -0600

    Modified EnhancedSenses Controller
    Cleaned up default parameters
    Pulled out DealDamageResponse for readability

--- Controller/Villains/Anathema/Cards/EnhancedSensesCardController.cs
+++ Controller/Villains/Anathema/Cards/EnhancedSensesCardController.cs
@@ -1,5 +1,6 @@
 ﻿using Handelabra.Sentinels.Engine.Controller;
 using Handelabra.Sentinels.Engine.Model;
+using System;
 using System.Collections;
 using System.Linq;
 
@@ -15,11 +16,26 @@ namespace Cauldron.Anathema
 		{
 			//Change the type of all damage to sonic.
 			base.AddChangeDamageTypeTrigger((DealDamageAction dd) => true, DamageType.Sonic);
+
 			//Whenever a Hero card enters play, this card deals that Hero Character 1 sonic damage.
-			base.AddTrigger<PlayCardAction>((PlayCardAction pca) => pca.CardToPlay.IsHero,
-				(PlayCardAction pc) => base.DealDamage(base.Card, pc.CardToPlay.Owner.CharacterCard, 1, DamageType.Sonic, false, false, false, null, null, null, false, base.GetCardSource(null)),
+			base.AddTrigger<PlayCardAction>((PlayCardAction pca) => pca.CardToPlay.IsHero, this.DealDamageResponse,
 				new TriggerType[] { TriggerType.DealDamage }, TriggerTiming.After, null, false, true, null, false, null, null, false, false);		
 		}
 
-	}
+        private IEnumerator DealDamageResponse(PlayCardAction pc)
+        {
+			//this card deals that Hero Character 1 sonic damage.
+			IEnumerator coroutine = base.DealDamage(base.Card, pc.CardToPlay.Owner.CharacterCard, 1, DamageType.Sonic, cardSource: base.GetCardSource());
+			if (base.UseUnityCoroutines)
+			{
+				yield return base.GameController.StartCoroutine(coroutine);
+			}
+			else
+			{
+				base.GameController.ExhaustCoroutine(coroutine);
+			}
+
+			yield break;
+		}
+    }
 }
