6963d73b4a58d8e7b5b43333b5f3295567d59719
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2020-11-17 16:44:54 -0600

    Modified RazorScales Controller
    Pulled out criteria
    Cleaned up default parameters

--- Controller/Villains/Anathema/Cards/RazorScalesCardController.cs
+++ Controller/Villains/Anathema/Cards/RazorScalesCardController.cs
@@ -15,14 +15,15 @@ namespace Cauldron.Anathema
 		public override void AddTriggers()
 		{
 			//The first time a Villain target is dealt damage each turn, this card deals the source of that damage 2 melee damage.
-			base.AddTrigger<DealDamageAction>((DealDamageAction dd) => !base.IsPropertyTrue("FirstDamageToVillainTargetThisTurn", null) && dd.DidDealDamage && dd.DamageSource.IsTarget && base.IsVillainTarget(dd.Target), new Func<DealDamageAction, IEnumerator>(this.FirstDamageDealtResponse), TriggerType.DealDamage, TriggerTiming.After, ActionDescription.DamageTaken, false, true, null, false, null, null, false, false);
+			Func<DealDamageAction, bool> criteria = (DealDamageAction dd) => !base.IsPropertyTrue(FirstDamageToVillainTargetThisTurn) && dd.DidDealDamage && dd.DamageSource.IsTarget && base.IsVillainTarget(dd.Target);
+			base.AddTrigger<DealDamageAction>(criteria, this.FirstDamageDealtResponse, TriggerType.DealDamage, TriggerTiming.After, ActionDescription.DamageTaken);
 		}
 
 		private IEnumerator FirstDamageDealtResponse(DealDamageAction dd)
 		{
 			//this card deals the source of that damage 2 melee damage.
 			base.SetCardPropertyToTrueIfRealAction(FirstDamageToVillainTargetThisTurn);
-			IEnumerator coroutine = base.DealDamage(base.Card, dd.DamageSource.Card, 2, DamageType.Melee, false, false, true, null, null, null, false, null);
+			IEnumerator coroutine = base.DealDamage(base.Card, dd.DamageSource.Card, 2, DamageType.Melee, isCounterDamage: true);
 			if (base.UseUnityCoroutines)
 			{
 				yield return base.GameController.StartCoroutine(coroutine);
