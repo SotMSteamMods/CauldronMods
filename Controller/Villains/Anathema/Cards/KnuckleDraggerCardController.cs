1be9d2581dcb1da484552bf5ebd62e5be2e80bc9
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2020-11-17 16:39:45 -0600

    Modified Knuckle Dragger Controller
    Cleaned up default parameters
    Added needed additional TriggerType

--- Controller/Villains/Anathema/Cards/KnuckleDraggerCardController.cs
+++ Controller/Villains/Anathema/Cards/KnuckleDraggerCardController.cs
@@ -17,14 +17,18 @@ namespace Cauldron.Anathema
 		{
 			//At the end of the Villain Turn, Anathema deals the Hero character with the highest HP {H+1} melee damage. If that Hero takes damage this way, they must destroy 1 of their ongoing cards."
 
-			base.AddEndOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, new Func<PhaseChangeAction, IEnumerator>(this.DealDamageResponse), TriggerType.DealDamage, null, false);
+			base.AddEndOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, this.DealDamageResponse, new TriggerType[]
+				{
+					TriggerType.DealDamage,
+					TriggerType.DestroyCard
+				});
 		}
 
 		private IEnumerator DealDamageResponse(PhaseChangeAction phaseChange)
 		{
 			//Anathema deals the Hero character with the highest HP {H+1} melee damage..
 			List<DealDamageAction> storedResults = new List<DealDamageAction>();
-			IEnumerator coroutine = base.DealDamageToHighestHP(base.CharacterCard, 1, (Card c) => c.IsHeroCharacterCard, (Card c) => new int?(base.H + 1), DamageType.Melee, false, false, storedResults, null, null, null, false, false);
+			IEnumerator coroutine = base.DealDamageToHighestHP(base.CharacterCard, 1, (Card c) => c.IsHeroCharacterCard, (Card c) => new int?(base.H + 1), DamageType.Melee, storedResults: storedResults);
 			if (base.UseUnityCoroutines)
 			{
 				yield return base.GameController.StartCoroutine(coroutine);
@@ -33,11 +37,12 @@ namespace Cauldron.Anathema
 			{
 				base.GameController.ExhaustCoroutine(coroutine);
 			}
-			DealDamageAction dd = storedResults.FirstOrDefault<DealDamageAction>();
+			DealDamageAction dd = storedResults.FirstOrDefault();
 			//If that Hero takes damage this way, they must destroy 1 of their ongoing cards.
 			if (dd != null && dd.DidDealDamage)
 			{
-				coroutine = base.GameController.SelectAndDestroyCard(base.FindHeroTurnTakerController(dd.Target.Owner.ToHero()), new LinqCardCriteria((Card c) => c.IsOngoing && c.Owner == dd.Target.Owner, "ongoing", true, false, null, null, false), false, null, null, base.GetCardSource(null));
+				HeroTurnTakerController httc = base.FindHeroTurnTakerController(dd.Target.Owner.ToHero());
+				coroutine = base.GameController.SelectAndDestroyCard(httc, new LinqCardCriteria((Card c) => c.IsOngoing && c.Owner == dd.Target.Owner, "ongoing"), false, cardSource: base.GetCardSource());
 				if (base.UseUnityCoroutines)
 				{
 					yield return base.GameController.StartCoroutine(coroutine);
