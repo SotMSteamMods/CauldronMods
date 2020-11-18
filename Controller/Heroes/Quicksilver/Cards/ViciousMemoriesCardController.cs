ea340fca2cce4e9fe367746bca257b9cdb43cf64
Jake <mgirman520@gmail.com>
2020-11-18 11:27:34 -0500

    Create ViciousMemoriesCardController.cs

---
+++ Controller/Heroes/Quicksilver/Cards/ViciousMemoriesCardController.cs
@@ -0,0 +1,46 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+
+namespace Cauldron
+{
+    public class ViciousMemoriesCardController : CardController
+    {
+        public ViciousMemoriesCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+            base.GameController.AddCardControllerToList(CardControllerListType.IncreasePhaseActionCount, this);
+
+        }
+
+        public override void AddTriggers()
+        {
+            //You may draw an extra card during your draw phase.
+            base.AddAdditionalPhaseActionTrigger((TurnTaker tt) => this.ShouldIncreasePhaseActionCount(tt), Phase.DrawCard, 1);
+        }
+
+        public override IEnumerator Play()
+        {
+            IEnumerator coroutine = base.IncreasePhaseActionCountIfInPhase((TurnTaker tt) => tt == base.TurnTaker, Phase.DrawCard, 1);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+
+        private bool ShouldIncreasePhaseActionCount(TurnTaker tt)
+        {
+            return tt == base.TurnTaker;
+        }
+
+        public override bool AskIfIncreasingCurrentPhaseActionCount()
+        {
+            return (base.GameController.ActiveTurnPhase.IsDrawCard) && this.ShouldIncreasePhaseActionCount(base.GameController.ActiveTurnTaker);
+        }
+    }
+}
