605060835d952cc4306a94d6d8a2bce0f6b7fdc5
Jake <mgirman520@gmail.com>
2020-11-18 11:24:00 -0500

    Test Subject Halberd 19

--- Controller/Heroes/Quicksilver/Cards/StressHardeningCardController.cs
+++ Controller/Heroes/Quicksilver/Cards/StressHardeningCardController.cs
@@ -13,16 +13,20 @@ namespace Cauldron
 
         public override void AddTriggers()
         {
+            //If {Quicksilver} currently has less than her max HP, increase damage she deals to non-hero targets by 1.
+            //If {Quicksilver} has 10 or fewer HP, increase damage she deals to non-hero targets by an additional 1.
             base.AddIncreaseDamageTrigger((DealDamageAction action) => action.DamageSource.Card == base.CharacterCard && !action.Target.IsHero, (DealDamageAction action) => DamageIncrease(action));
         }
 
         private int DamageIncrease(DealDamageAction action)
         {
             int increase = 0;
+            //If {Quicksilver} currently has less than her max HP, increase damage she deals to non-hero targets by 1.
             if (base.CharacterCard.MaximumHitPoints > base.CharacterCard.HitPoints)
             {
                 increase++;
             }
+            //If {Quicksilver} has 10 or fewer HP, increase damage she deals to non-hero targets by an additional 1.
             if (base.CharacterCard.HitPoints <= 10)
             {
                 increase++;
