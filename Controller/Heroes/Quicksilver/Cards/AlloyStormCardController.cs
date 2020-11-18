a0487e3a4520fdfe10c28286c002823effb98e79
Jake <mgirman520@gmail.com>
2020-11-17 19:40:29 -0500

    Cleanup decklist

---
+++ Controller/Heroes/Quicksilver/Cards/AlloyStormCardController.cs
@@ -0,0 +1,14 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+
+namespace Cauldron
+{
+    public class AlloyStormCardController : CardController
+    {
+        public AlloyStormCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+    }
+}
---
+++ Controller/Heroes/Quicksilver/CharacterCards/QuicksilverCharacterCardController.cs
@@ -0,0 +1,14 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+
+namespace Cauldron
+{
+    public class QuicksilverCharacterCardController : CardController
+    {
+        public QuicksilverCharacterCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+    }
+}
