7b0b06bd9fc9cb432525039a3fc1e2fce8c1d699
Jake <mgirman520@gmail.com>
2020-11-18 23:04:51 -0500

    Create Mercury Strike

--- Controller/Heroes/Quicksilver/Cards/ForestOfNeedlesCardController.cs
+++ Controller/Heroes/Quicksilver/Cards/ForestOfNeedlesCardController.cs
@@ -14,9 +14,6 @@ namespace Cauldron.Quicksilver
         //{Quicksilver} may deal 6 melee damage to a target with more than 8HP, or 3 melee damage to a target with 8 or fewer HP.
         public override IEnumerator Play()
         {
-            var a = new Func<Card, int>((Card c) => this.DynamicDamage(c));
-            Func<Card, int> damage = (Card c) => this.DynamicDamage(c);
-            Func<int> targets = new Func<int>(() => 1);
             IEnumerator coroutine = base.GameController.SelectTargetsAndDealDamage(base.HeroTurnTakerController, new DamageSource(base.GameController, base.CharacterCard), (Card c) => this.DynamicDamage(c), DamageType.Melee, () => 1, false, 1, cardSource: base.GetCardSource());
             if (base.UseUnityCoroutines)
             {
---
+++ Controller/Heroes/Quicksilver/Cards/MercuryStrikeCardController.cs
@@ -0,0 +1,36 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+
+namespace Cauldron.Quicksilver
+{
+    public class MercuryStrikeCardController : ComboCardController
+    {
+        public MercuryStrikeCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+
+        public override IEnumerator Play()
+        {
+            //{Quicksilver} deals 1 target 2 melee damage, 
+            IEnumerator coroutine = base.GameController.SelectTargetsAndDealDamage(base.HeroTurnTakerController, new DamageSource(base.GameController, base.CharacterCard), 2, DamageType.Melee, 1, false, 1, cardSource: base.GetCardSource());
+            //then 1 target 1 melee damage. 
+            IEnumerator coroutine2 = base.GameController.SelectTargetsAndDealDamage(base.HeroTurnTakerController, new DamageSource(base.GameController, base.CharacterCard), 1, DamageType.Melee, 1, false, 1, cardSource: base.GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+                yield return base.GameController.StartCoroutine(coroutine2);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+                base.GameController.ExhaustCoroutine(coroutine2);
+            }
+            //You may play a Finisher, or {Quicksilver} may deal herself 2 melee damage and play a Combo.
+            base.Play();
+            yield break;
+        }
+    }
+}
