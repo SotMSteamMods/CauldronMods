9ab4845006989bf8abbd2d512b46df33ca8e9f39
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2020-11-20 06:16:46 -0600

    Update MakeshiftShelterCardController.cs

--- Controller/Environments/Northspar/Cards/MakeshiftShelterCardController.cs
+++ Controller/Environments/Northspar/Cards/MakeshiftShelterCardController.cs
@@ -11,9 +11,53 @@ namespace Cauldron.Northspar
 
         public MakeshiftShelterCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
-
+            base.AddThisCardControllerToList(CardControllerListType.MakesIndestructible);
         }
 
+        public override void AddTriggers()
+        {
+            //At the start of the environment turn, you may shuffle a card from the environment trash into the deck. if Tak Ahab is in the environment trash, shuffle him into the deck.
+            base.AddStartOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, this.ShuffleCardsResponse, TriggerType.ShuffleCardIntoDeck);
+        }
 
+        private IEnumerator ShuffleCardsResponse(PhaseChangeAction pca)
+        {
+            //you may shuffle a card from the environment trash into the deck.
+            LinqCardCriteria cardCriteria = new LinqCardCriteria((Card c) => c.Location == base.TurnTaker.Trash);
+            Func<Card, IEnumerator> actionWithCard = (Card c) => base.GameController.ShuffleCardIntoLocation(base.DecisionMaker, c, base.TurnTaker.Deck, false, cardSource: base.GetCardSource());
+            IEnumerator coroutine = base.GameController.SelectCardsAndDoAction(base.DecisionMaker, cardCriteria, SelectionType.ShuffleCardFromTrashIntoDeck, actionWithCard, new int?(1), true, cardSource: base.GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+
+            //if Tak Ahab is in the environment trash, shuffle him into the deck.
+            if (base.TurnTaker.Trash.HasCard(FindCard(TakAhabIdentifier)))
+            {
+                cardCriteria = new LinqCardCriteria((Card c) => c.Location == base.TurnTaker.Trash && c.Identifier == TakAhabIdentifier);
+                actionWithCard = (Card c) => base.GameController.ShuffleCardIntoLocation(base.DecisionMaker, c, base.TurnTaker.Deck, false, cardSource: base.GetCardSource());
+                coroutine = base.GameController.SelectCardsAndDoAction(base.DecisionMaker, cardCriteria, SelectionType.ShuffleCardFromTrashIntoDeck, actionWithCard, new int?(1), cardSource: base.GetCardSource());
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+
+            }
+           
+        }
+
+public override bool AskIfCardIsIndestructible(Card card)
+        {
+            //This card and Supply Depot are indestructible.
+            return card == base.Card || card.Identifier == "SupplyDepot";
+        }
     }
 }
