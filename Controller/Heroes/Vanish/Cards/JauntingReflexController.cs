a2b3d41f9ddf0ffb18c251492805aad942b7c183
Vilnor <onlytelford@yahoo.com>
2020-11-19 23:11:57 -0500

    Jaunting Reflex

--- Controller/Heroes/Vanish/Cards/JauntingReflexController.cs
+++ Controller/Heroes/Vanish/Cards/JauntingReflexController.cs
@@ -90,10 +90,10 @@ namespace Cauldron.Vanish
                 }
             }
         }
-        /*
+
         public override void AddTriggers()
         {
-            AddTrigger<DealDamageAction>(dda => !dda.DamageSource.IsHero && dda.Target.IsHero && dda.DidDealDamage, null, TriggerType.UsePower, TriggerTiming.After, isActionOptional: true);
+            AddTrigger<DealDamageAction>(dda => !dda.DamageSource.IsHero && dda.Target.IsHero && dda.DidDealDamage && WasNotUsedThisTurn(), HeroDamagedResponse, TriggerType.UsePower, TriggerTiming.After, isActionOptional: true);
         }
 
         private bool WasNotUsedThisTurn()
@@ -105,18 +105,70 @@ namespace Cauldron.Vanish
         {
             List<DiscardCardAction> results = new List<DiscardCardAction>();
             var coroutine = GameController.SelectAndDiscardCard(DecisionMaker, optional: true, storedResults: results, cardSource: GetCardSource());
-            
-                        
-
-            //
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
 
             if (DidDiscardCards(results, 1))
             {
                 base.SetCardPropertyToTrueIfRealAction("UsedJauntingReflex");
 
                 coroutine = GameController.SelectAndUsePower(DecisionMaker, optional: true, cardSource: GetCardSource());
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
             }
         }
-        */
+
+        public override IEnumerator UsePower(int index = 0)
+        {
+            //Calling Destroy Card, then Draw Card(2) didn't work.  Only a single card would be drawn for I'm sure great reasons.
+            //instead I check that Card is destroyable first.
+            if (GameController.IsCardIndestructible(Card))
+            {
+                var coroutine = GameController.SendMessageAction($"{Card.Title} is Indestructible so no card will be drawn.", Priority.Medium, GetCardSource());
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+            }
+            else
+            {
+                var coroutine = DrawCards(DecisionMaker, 2);
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+                coroutine = GameController.DestroyCard(DecisionMaker, Card, cardSource: GetCardSource());
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+            }
+
+            
+        }
     }
 }
