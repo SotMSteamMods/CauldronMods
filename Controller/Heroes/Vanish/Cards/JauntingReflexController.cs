72ef0344c0349b0dd0de4f25c8544e89d469f859
Vilnor <onlytelford@yahoo.com>
2020-11-19 21:59:35 -0500

    Jaunting In progress

--- Controller/Heroes/Vanish/Cards/JauntingReflexController.cs
+++ Controller/Heroes/Vanish/Cards/JauntingReflexController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using Handelabra.Sentinels.Engine.Controller;
@@ -12,5 +13,33 @@ namespace Cauldron.Vanish
         {
 
         }
+        /*
+        public override void AddTriggers()
+        {
+            AddTrigger<DealDamageAction>(dda => !dda.DamageSource.IsHero && dda.Target.IsHero && dda.DidDealDamage, null, TriggerType.UsePower, TriggerTiming.After, isActionOptional: true);
+        }
+
+        private bool WasNotUsedThisTurn()
+        {
+            return !base.Journal.CardPropertiesEntriesThisTurn(Card).Any(j => j.Key == "UsedJauntingReflex");
+        }
+
+        private IEnumerator HeroDamagedResponse(DealDamageAction action)
+        {
+            List<DiscardCardAction> results = new List<DiscardCardAction>();
+            var coroutine = GameController.SelectAndDiscardCard(DecisionMaker, optional: true, storedResults: results, cardSource: GetCardSource());
+            
+                        
+
+            //
+
+            if (DidDiscardCards(results, 1))
+            {
+                base.SetCardPropertyToTrueIfRealAction("UsedJauntingReflex");
+
+                coroutine = GameController.SelectAndUsePower(DecisionMaker, optional: true, cardSource: GetCardSource());
+            }
+        }
+        */
     }
 }
