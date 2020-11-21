9c4fbcaa08c13eaedb1820135db284b8108b5092
Vilnor <onlytelford@yahoo.com>
2020-11-21 12:24:51 -0500

    Gas Mask Cleanup

--- Controller/Heroes/DocHavoc/Cards/GasMaskCardController.cs
+++ Controller/Heroes/DocHavoc/Cards/GasMaskCardController.cs
@@ -22,23 +22,22 @@ namespace Cauldron.DocHavoc
             // Whenever an environment card is destroyed, {DocHavoc} regains 2 HP.
             //==============================================================
 
-            Func<DestroyCardAction, bool> criteria = (destroyCard =>
-                    destroyCard.WasCardDestroyed
-                    && destroyCard.CardToDestroy != null
-                    && destroyCard.CardToDestroy.Card.IsEnvironment
-                    && this.GameController.IsCardVisibleToCardSource(destroyCard.CardToDestroy.Card,
-                        this.GetCardSource()));
-            this.AddTrigger<DestroyCardAction>(criteria, new Func<DestroyCardAction, IEnumerator>(this.GainHpResponse), TriggerType.GainHP, TriggerTiming.After);
+            this.AddTrigger<DestroyCardAction>(WasEnviromentCardDestroyed, this.GainHpResponse, TriggerType.GainHP, TriggerTiming.After);
 
+        }
+
+        private bool WasEnviromentCardDestroyed(DestroyCardAction destroyCard)
+        {
+            return destroyCard.WasCardDestroyed && destroyCard.CardToDestroy != null &&
+                    destroyCard.CardToDestroy.Card.IsEnvironment &&
+                    this.GameController.IsCardVisibleToCardSource(destroyCard.CardToDestroy.Card, this.GetCardSource());
         }
 
         private IEnumerator GainHpResponse(DestroyCardAction destroyCard)
         {
-            IEnumerator gainHpRoutine =
-                this.GameController.GainHP(this.CharacterCard, HpGain, cardSource: this.GetCardSource());
+            IEnumerator gainHpRoutine = this.GameController.GainHP(this.CharacterCard, HpGain, cardSource: this.GetCardSource());
             if (this.UseUnityCoroutines)
             {
-
                 yield return this.GameController.StartCoroutine(gainHpRoutine);
             }
             else
