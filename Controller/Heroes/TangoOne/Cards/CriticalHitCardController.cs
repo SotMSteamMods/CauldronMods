24603c15644bf61e50583f29952431f9e3acf3d8
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2020-11-21 02:25:04 -0600

    Critical Hit Fix

--- Controller/Heroes/TangoOne/Cards/CriticalHitCardController.cs
+++ Controller/Heroes/TangoOne/Cards/CriticalHitCardController.cs
@@ -41,52 +41,69 @@ namespace Cauldron.TangoOne
 
         private IEnumerator RevealTopCardFromDeckResponse(DealDamageAction dda)
         {
-            // Ask if player wants to discard off the top of their deck
 
-            YesNoDecision yesNo = new YesNoDecision(base.GameController, base.HeroTurnTakerController,
+            if (dda.IsPretend)
+            {
+                IEnumerator pretend = base.CancelAction(dda);
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(pretend);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(pretend);
+                }
+            }
+            else
+            {
+                // Ask if player wants to discard off the top of their deck
+
+                YesNoDecision yesNo = new YesNoDecision(base.GameController, base.HeroTurnTakerController,
                 SelectionType.DiscardFromDeck, false, cardSource: GetCardSource());
-            
-            IEnumerator routine = base.GameController.MakeDecisionAction(yesNo, true);
 
-            if (base.UseUnityCoroutines)
-            {
-                yield return base.GameController.StartCoroutine(routine);
-            }
-            else
-            {
-                base.GameController.ExhaustCoroutine(routine);
+                IEnumerator routine = base.GameController.MakeDecisionAction(yesNo, true);
+
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(routine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(routine);
+                }
+
+                // Return if they chose not to discard from their deck
+                if (yesNo.Answer == null || !yesNo.Answer.Value)
+                {
+                    yield break;
+                }
+
+                // Move card from top of their deck to the trash
+                List<MoveCardAction> moveCardActions = new List<MoveCardAction>();
+                IEnumerator discardCardRoutine = base.GameController.DiscardTopCard(this.TurnTaker.Deck, moveCardActions,
+                    card => true, this.TurnTaker, base.GetCardSource());
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(discardCardRoutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(discardCardRoutine);
+                }
+
+                // Check to see if the card was moved and contains the keyword "critical", if it didn't, break and damage proceeds normally
+                if (moveCardActions.Count <= 0 || !moveCardActions.First().WasCardMoved ||
+                    !IsCritical(moveCardActions.First().CardToMove))
+                {
+                    yield break;
+                }
+
+                // Card had the "critical" keyword, increase the damage
+                ModifyDealDamageAction mdda = new IncreaseDamageAction(this.GameController, dda, DamageIncrease, false);
+                dda.AddDamageModifier(mdda);
             }
 
-            // Return if they chose not to discard from their deck
-            if (yesNo.Answer == null || !yesNo.Answer.Value)
-            {
-                yield break;
-            }
-
-            // Move card from top of their deck to the trash
-            List<MoveCardAction> moveCardActions = new List<MoveCardAction>();
-            IEnumerator discardCardRoutine = base.GameController.DiscardTopCard(this.TurnTaker.Deck, moveCardActions, 
-                card => true, this.TurnTaker, base.GetCardSource());
-            if (base.UseUnityCoroutines)
-            {
-                yield return base.GameController.StartCoroutine(discardCardRoutine);
-            }
-            else
-            {
-                base.GameController.ExhaustCoroutine(discardCardRoutine);
-            }
-
-            // Check to see if the card was moved and contains the keyword "critical", if it didn't, break and damage proceeds normally
-            if (moveCardActions.Count <= 0 || !moveCardActions.First().WasCardMoved ||
-                !IsCritical(moveCardActions.First().CardToMove))
-            {
-                yield break;
-            }
-
-            // Card had the "critical" keyword, increase the damage
-            ModifyDealDamageAction mdda = new IncreaseDamageAction(this.GameController, dda, DamageIncrease, false);
-            dda.AddDamageModifier(mdda);
-
+            yield break;
         }
     }
 }
