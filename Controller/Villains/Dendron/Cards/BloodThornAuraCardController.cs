ce9be2d4d94831bc910991ad87e3a5f9db77be27
byoung <breen.young@gmail.com>
2020-11-22 00:08:44 -0400

    Fix Trigger.Timing before behavior

--- Controller/Villains/Dendron/Cards/BloodThornAuraCardController.cs
+++ Controller/Villains/Dendron/Cards/BloodThornAuraCardController.cs
@@ -34,18 +34,36 @@ namespace Cauldron.Dendron
 
         private IEnumerator DealDamageResponse(DealDamageAction dda)
         {
-            int damageToDeal = this.Game.H - 2;
 
-            IEnumerator dealDamageRoutine = this.DealDamage(dda.Target, dda.DamageSource.Card, damageToDeal, DamageType.Radiant);
-
-            if (base.UseUnityCoroutines)
+            if (dda.IsPretend)
             {
-                yield return base.GameController.StartCoroutine(dealDamageRoutine);
+                IEnumerator pretend = base.CancelAction(dda);
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(pretend);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(pretend);
+                }
             }
             else
             {
-                base.GameController.ExhaustCoroutine(dealDamageRoutine);
+                int damageToDeal = this.Game.H - 2;
+
+                IEnumerator dealDamageRoutine = this.DealDamage(dda.Target, dda.DamageSource.Card, damageToDeal, DamageType.Radiant);
+
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(dealDamageRoutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(dealDamageRoutine);
+                }
             }
+
+
         }
     }
 }
