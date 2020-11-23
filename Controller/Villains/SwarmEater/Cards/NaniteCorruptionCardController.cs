5e447c6f8dbc84192d1eeadf71e90a25e222cdd0
Jake <mgirman520@gmail.com>
2020-11-22 21:17:20 -0500

    Create NaniteCorruptionCardController.cs

---
+++ Controller/Villains/SwarmEater/Cards/NaniteCorruptionCardController.cs
@@ -0,0 +1,40 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+
+namespace Cauldron
+{
+    public class NaniteCorruptionCardController : CardController
+    {
+        public NaniteCorruptionCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+
+        public override IEnumerator Play()
+        {
+            //Reveal cards from the top of the villain deck until {H - 1} targets are revealed. Put those cards into play and discard the rest.
+            IEnumerator coroutine = base.RevealCards_PutSomeIntoPlay_DiscardRemaining(base.TurnTakerController, base.TurnTaker.Deck, Game.H - 1, new LinqCardCriteria((Card c) => c.IsTarget), revealUntilNumberOfMatchingCards: Game.H - 1);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            //{SwarmEater} deals the {H} targets other than itself with the lowest HP 2 projectile damage each.
+            coroutine = base.DealDamageToLowestHP(base.CharacterCard, 1, (Card c) => c != base.CharacterCard, (Card c) => 2, DamageType.Melee, numberOfTargets: Game.H);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+    }
+}
