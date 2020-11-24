4e96ec823f2db0f44da626d4e7e4da495a3e86eb
LNBertolini <bertolini.lucas.n@gmail.com>
2020-11-23 19:38:55 -0600

    Improve Liquid Metal reveal logic
    Formerly, it would find the first Combo, put the rest back *in reverse order*, then find the first Finisher. This could matter if you specifically put a finisher on top. Now it finds the first combo *or* finisher, then finds the first of the other kind.

--- Controller/Heroes/Quicksilver/Cards/LiquidMetalCardController.cs
+++ Controller/Heroes/Quicksilver/Cards/LiquidMetalCardController.cs
@@ -2,6 +2,9 @@
 using Handelabra.Sentinels.Engine.Model;
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Handelabra;
 
 namespace Cauldron.Quicksilver
 {
@@ -14,10 +17,36 @@ namespace Cauldron.Quicksilver
 
         public override IEnumerator Play()
         {
-            //Reveal cards from the top of your deck until you reveal a Combo...
-            IEnumerator coroutine = base.RevealCards_MoveMatching_ReturnNonMatchingCards(base.TurnTakerController, base.TurnTaker.Deck, false, false, true, new LinqCardCriteria((Card c) => c.DoKeywordsContain("combo")), 1, shuffleSourceAfterwards: false);
-            //...and a Finisher and put them into your hand. Shuffle the other revealed cards back into your deck.
-            IEnumerator coroutine2 = base.RevealCards_MoveMatching_ReturnNonMatchingCards(base.TurnTakerController, base.TurnTaker.Deck, false, false, true, new LinqCardCriteria((Card c) => c.DoKeywordsContain("finisher")), 1);
+            //Reveal cards from the top of your deck until you reveal a [Combo or a Finisher]...
+            IEnumerator coroutine = base.RevealCards_MoveMatching_ReturnNonMatchingCards(base.TurnTakerController, base.TurnTaker.Deck, false, false, true, new LinqCardCriteria((Card c) => c.DoKeywordsContain(new string[] { "combo", "finisher" })), 1, shuffleSourceAfterwards: false);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+
+            var cardToHandEntry = Journal.MoveCardEntriesThisTurn().Where((MoveCardJournalEntry mcje) => mcje.CardSource == this.Card && mcje.ToLocation == HeroTurnTaker.Hand).LastOrDefault();
+            Card foundCard = null;
+            if (cardToHandEntry != null)
+            {
+                foundCard = cardToHandEntry.Card;
+            }
+
+            List<string> missingKeywords = new List<string> { };
+            if (foundCard == null || !foundCard.DoKeywordsContain("finisher"))
+            {
+                missingKeywords.Add("finisher");
+            }
+            if (foundCard == null || !foundCard.DoKeywordsContain("combo"))
+            {
+                missingKeywords.Add("combo");
+            }
+
+            //...and [the kind you didn't find first] and put them into your hand. Shuffle the other revealed cards back into your deck.
+            IEnumerator coroutine2 = base.RevealCards_MoveMatching_ReturnNonMatchingCards(base.TurnTakerController, base.TurnTaker.Deck, false, false, true, new LinqCardCriteria((Card c) => c.DoKeywordsContain(missingKeywords)), 1);
             if (base.UseUnityCoroutines)
             {
                 yield return base.GameController.StartCoroutine(coroutine);
