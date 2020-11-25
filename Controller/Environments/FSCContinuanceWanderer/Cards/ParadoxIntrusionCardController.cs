9d1243432015ba3b4b5d244d9e1642aba9a97a6f
Vilnor <onlytelford@yahoo.com>
2020-11-25 10:28:04 -0500

    modified X query with some Obliveon proofing, may not be needed.

--- Controller/Environments/FSCContinuanceWanderer/Cards/ParadoxIntrusionCardController.cs
+++ Controller/Environments/FSCContinuanceWanderer/Cards/ParadoxIntrusionCardController.cs
@@ -24,7 +24,7 @@ namespace Cauldron.FSCContinuanceWanderer
         private IEnumerator DealDamageResponse(PhaseChangeAction pca)
         {
             //At the end of the environment turn, this card deals the hero target with the highest HP {H} energy damage.
-            IEnumerator coroutine = base.DealDamageToHighestHP(base.Card, 1, (Card c) => c.IsHero && c.IsTarget, (Card c) => new int?(base.H), DamageType.Energy);
+            IEnumerator coroutine = base.DealDamageToHighestHP(base.Card, 1, (Card c) => c.IsHero && c.IsTarget, (Card c) => base.H, DamageType.Energy);
             if (base.UseUnityCoroutines)
             {
                 yield return base.GameController.StartCoroutine(coroutine);
@@ -34,8 +34,8 @@ namespace Cauldron.FSCContinuanceWanderer
                 base.GameController.ExhaustCoroutine(coroutine);
             }
             //Then, this card deals X villain targets 2 energy damage each, where x is the number of time vortex cards in the environment trash.
-            Func<int> X = () => base.FindCardsWhere((Card c) => c.DoKeywordsContain("time vortex") && c.IsInTrash && c.IsEnvironment).Count();
-            coroutine = base.DealDamage(base.Card, (Card c) => c.IsVillainTarget, (Card c) => new int?(2), DamageType.Energy, dynamicNumberOfTargets: X);
+            Func<int> X = () =>  base.FindCardsWhere((Card c) => c.Location == this.TurnTaker.Trash && c.IsEnvironment && c.IsInTrash && c.DoKeywordsContain("time vortex")).Count();
+            coroutine = base.DealDamage(base.Card, (Card c) => c.IsVillainTarget, (Card c) => 2, DamageType.Energy, dynamicNumberOfTargets: X);
             if (base.UseUnityCoroutines)
             {
                 yield return base.GameController.StartCoroutine(coroutine);
