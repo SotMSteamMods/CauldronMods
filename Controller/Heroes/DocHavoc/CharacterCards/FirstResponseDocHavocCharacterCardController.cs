085445a3d7d97a04674390a01780bdf58713f30b
byoung <breen.young@gmail.com>
2020-11-27 00:24:06 -0400

    Initial First Response Doc Havoc work

---
+++ Controller/Heroes/DocHavoc/CharacterCards/FirstResponseDocHavocCharacterCardController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+
+namespace Cauldron.DocHavoc
+{
+    public class FirstResponseDocHavocCharacterCardController : HeroCharacterCardController
+    {
+        private const int PowerDamageToDeal = 1;
+        private const int Incapacitate1HpThreshold = 6;
+        private const int Incapacitate2CardsToDraw = 1;
+        private const int Incapacitate3CardsToDestroy = 2;
+
+        public FirstResponseDocHavocCharacterCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+        }
+
+        public override IEnumerator UsePower(int index = 0)
+        {
+            //==============================================================
+            // Draw a card. {DocHavoc} may deal 1 target 1 melee damage.
+            //==============================================================
+
+            IEnumerator routine = this.DrawCard(this.HeroTurnTaker);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(routine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(routine);
+            }
+
+            int powerNumeral = GetPowerNumeral(0, PowerDamageToDeal);
+            DamageSource ds = new DamageSource(this.GameController, this.TurnTaker);
+            IEnumerator routine2 = base.GameController.SelectTargetsAndDealDamage(this.HeroTurnTakerController, ds, powerNumeral,
+                DamageType.Melee, 1, true, 0, cardSource: base.GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(routine2);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(routine2);
+            }
+        }
+
+        public override IEnumerator UseIncapacitatedAbility(int index)
+        {
+            IEnumerator coroutine;
+            switch (index)
+            {
+                case 0:
+
+                    //==============================================================
+                    // Until the start of your next turn, reduce damage dealt to targets with fewer than 6 HP by
+                    //==============================================================
+
+                    break;
+
+                case 1:
+
+                    //==============================================================
+                    // One player may take a one-shot from their trash and put it on top of their deck.
+                    //==============================================================
+
+                    List<SelectCardDecision> storedResults = new List<SelectCardDecision>();
+                    IEnumerator coroutine = base.GameController.SelectCardAndStoreResults(base.HeroTurnTakerController, SelectionType.MoveCardOnDeck, 
+                        new LinqCardCriteria(c => c.IsInTrash && c.Location.IsHero && c.IsOneShot, "in any hero's trash", useCardsSuffix: false, useCardsPrefix: true), 
+                        storedResults, optional: false,  cardSource: GetCardSource());
+
+                    if (base.UseUnityCoroutines)
+                    {
+                        yield return base.GameController.StartCoroutine(coroutine);
+                    }
+                    else
+                    {
+                        base.GameController.ExhaustCoroutine(coroutine);
+                    }
+
+                    if (storedResults.Any((SelectCardDecision d) => d.Completed && d.SelectedCard != null))
+                    {
+                        Card selectedCard = storedResults.FirstOrDefault().SelectedCard;
+                        Location hand = selectedCard.Location.OwnerTurnTaker.ToHero().Hand;
+                        coroutine = base.GameController.MoveCard(base.TurnTakerController, selectedCard, hand, toBottom: false, isPutIntoPlay: false, playCardIfMovingToPlayArea: true, null, showMessage: false, storedResults.Cast<IDecision>(), null, null, evenIfIndestructible: false, flipFaceDown: false, null, isDiscard: false, evenIfPretendGameOver: false, shuffledTrashIntoDeck: false, doesNotEnterPlay: false, GetCardSource());
+                        if (base.UseUnityCoroutines)
+                        {
+                            yield return base.GameController.StartCoroutine(coroutine);
+                        }
+                        else
+                        {
+                            base.GameController.ExhaustCoroutine(coroutine);
+                        }
+                    }
+
+
+
+                    /*
+                    coroutine = GameController.SelectHeroToMoveCardFromTrash(base.HeroTurnTakerController,
+                        httc => httc.HeroTurnTaker.Deck,
+                        optionalMoveCard: false,
+                        cardCriteria: new LinqCardCriteria(card => card.IsOneShot, "one-shot"), 
+                        cardSource: GetCardSource());
+
+                    if (base.UseUnityCoroutines)
+                    {
+                        yield return base.GameController.StartCoroutine(coroutine);
+                    }
+                    else
+                    {
+                        base.GameController.ExhaustCoroutine(coroutine);
+                    }
+                    */
+
+                    break;
+
+                case 2:
+
+                    //==============================================================
+                    // Destroy 2 environment targets.
+                    //==============================================================
+
+                    coroutine = base.GameController.SelectAndDestroyCards(base.HeroTurnTakerController, 
+                        new LinqCardCriteria(c => c.IsEnvironment, "environment"), Incapacitate3CardsToDestroy,
+                        requiredDecisions: 0, cardSource: GetCardSource());
+                    if (base.UseUnityCoroutines)
+                    {
+                        yield return base.GameController.StartCoroutine(coroutine);
+                    }
+                    else
+                    {
+                        base.GameController.ExhaustCoroutine(coroutine);
+                    }
+
+                    break;
+
+
+            }
+        }
+    }
+}
---
+++ Testing/Heroes/DocHavocPromoTests.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Cauldron.DocHavoc;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using Handelabra.Sentinels.UnitTest;
+
+using NUnit.Framework;
+
+namespace CauldronTests
+{
+    [TestFixture]
+    public class DocHavocPromoTests : BaseTest
+    {
+        protected HeroTurnTakerController DocHavoc => FindHero("DocHavoc");
+
+        private const string DeckNamespaceFirstResponse = "Cauldron.DocHavoc/FirstResponseDocHavocCharacter";
+
+        [Test]
+        public void TestDocHavocFirstResponseLoads()
+        {
+            // Arrange & Act
+            SetupGameController("BaronBlade", DeckNamespaceFirstResponse, "Megalopolis");
+
+            // Assert
+            Assert.AreEqual(3, this.GameController.TurnTakerControllers.Count());
+            Assert.IsNotNull(DocHavoc);
+            Assert.IsInstanceOf(typeof(FirstResponseDocHavocCharacterCardController), DocHavoc.CharacterCardController);
+
+            Assert.AreEqual(32, DocHavoc.CharacterCard.HitPoints);
+        }
+
+        [Test]
+        public void TestInnatePowerFirstResponse()
+        {
+            // Arrange
+            SetupGameController("BaronBlade", DeckNamespaceFirstResponse, "Ra", "Legacy", "Megalopolis");
+
+            StartGame();
+
+            Card mdp = GetCardInPlay("MobileDefensePlatform");
+            QuickHPStorage(mdp);
+            QuickHandStorage(DocHavoc);
+            DecisionYesNo = true;
+            DecisionSelectTarget = mdp;
+
+            // Act
+            GoToUsePowerPhase(DocHavoc);
+            UsePower(DocHavoc);
+
+            // Assert
+            QuickHPCheck(-1);
+            QuickHandCheck(1);
+        }
+
+        [Test]
+        public void TestInnatePowerFirstResponseNoDamage()
+        {
+            // Arrange
+            SetupGameController("BaronBlade", DeckNamespaceFirstResponse, "Ra", "Legacy", "Megalopolis");
+
+            StartGame();
+
+            Card mdp = GetCardInPlay("MobileDefensePlatform");
+            QuickHPStorage(mdp);
+            QuickHandStorage(DocHavoc);
+            DecisionYesNo = false;
+
+            // Act
+            GoToUsePowerPhase(DocHavoc);
+            UsePower(DocHavoc);
+
+            // Assert
+            QuickHPCheck(0);
+            QuickHandCheck(1);
+        }
+
+        [Test]
+        public void TestFirstResponseIncapacitateOption1()
+        {
+            Assert.True(false, "TODO");
+        }
+
+        [Test]
+        public void TestFirstResponseIncapacitateOption2()
+        {
+            // Arrange
+            SetupGameController("BaronBlade", DeckNamespaceFirstResponse, "Ra", "Legacy", "InsulaPrimalis");
+
+            Card dangerSense = GetCard("DangerSense");
+            Card thokk = GetCard("Thokk");
+
+            PutInTrash(legacy, dangerSense);
+            PutInTrash(legacy, thokk);
+
+            StartGame();
+
+            DecisionSelectCard = legacy.CharacterCard;
+
+            SetHitPoints(DocHavoc.CharacterCard, 1);
+            DealDamage(baron, DocHavoc, 2, DamageType.Melee);
+
+            // Act
+            GoToUseIncapacitatedAbilityPhase(DocHavoc);
+            UseIncapacitatedAbility(DocHavoc, 1);
+
+            // Assert
+
+            Assert.True(false, "TODO");
+        }
+
+        [Test]
+        public void TestFirstResponseIncapacitateOption3_Destroy2Cards()
+        {
+            // Arrange
+            SetupGameController("BaronBlade", DeckNamespaceFirstResponse, "Ra", "Legacy", "InsulaPrimalis");
+
+            StartGame();
+
+            PutIntoPlay("EnragedTRex");
+            Card enragedTRex = GetCardInPlay("EnragedTRex");
+
+            PutIntoPlay("ObsidianField");
+            Card obsidianField = GetCardInPlay("ObsidianField");
+
+            DecisionSelectCards = new[] {enragedTRex, obsidianField};
+
+            SetHitPoints(DocHavoc.CharacterCard, 1);
+            DealDamage(baron, DocHavoc, 2, DamageType.Melee);
+
+            // Act
+            GoToUseIncapacitatedAbilityPhase(DocHavoc);
+            UseIncapacitatedAbility(DocHavoc, 2);
+
+            // Assert
+            AssertIncapacitated(DocHavoc);
+            AssertInTrash(enragedTRex);
+            AssertInTrash(obsidianField);
+        }
+
+        [Test]
+        public void TestFirstResponseIncapacitateOption3_Destroy1Card()
+        {
+            // Arrange
+            SetupGameController("BaronBlade", DeckNamespaceFirstResponse, "Ra", "Legacy", "InsulaPrimalis");
+
+            StartGame();
+
+            PutIntoPlay("EnragedTRex");
+            Card enragedTRex = GetCardInPlay("EnragedTRex");
+
+            DecisionSelectCard = enragedTRex;
+
+            SetHitPoints(DocHavoc.CharacterCard, 1);
+            DealDamage(baron, DocHavoc, 2, DamageType.Melee);
+
+            // Act
+            GoToUseIncapacitatedAbilityPhase(DocHavoc);
+            UseIncapacitatedAbility(DocHavoc, 2);
+
+            // Assert
+            AssertIncapacitated(DocHavoc);
+            AssertInTrash(enragedTRex);
+        }
+
+        [Test]
+        public void TestFirstResponseIncapacitateOption3_NoEligibleCards()
+        {
+            // Arrange
+            SetupGameController("BaronBlade", DeckNamespaceFirstResponse, "Ra", "Legacy", "InsulaPrimalis");
+
+            StartGame();
+
+            SetHitPoints(DocHavoc.CharacterCard, 1);
+            DealDamage(baron, DocHavoc, 2, DamageType.Melee);
+
+            // Act
+            GoToUseIncapacitatedAbilityPhase(DocHavoc);
+            UseIncapacitatedAbility(DocHavoc, 2);
+
+            // Assert
+            AssertIncapacitated(DocHavoc);
+            AssertNumberOfCardsInTrash(env, 0);
+        }
+    }
+}
