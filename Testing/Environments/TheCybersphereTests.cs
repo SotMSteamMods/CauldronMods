45a1452942de626f1b42f03c5dbfa75b0c2ccfcb
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2020-11-28 01:54:58 -0600

    Added Dr3dnt Controller + Tests

--- Testing/Environments/TheCybersphereTests.cs
+++ Testing/Environments/TheCybersphereTests.cs
@@ -62,14 +62,34 @@ namespace CauldronTests
 
             SetHitPoints(haka, 15);
 
+            GoToPlayCardPhase(cybersphere);
             Card b3h3mth = PlayCard("B3h3mth");
 
             //At the end of the environment turn, this card deals the non-environment target with the second lowest HP 4 fire damage.
             //2nd lowest is Haka
 
-            QuickHPStorage(baron, ra, legacy, haka);
+            QuickHPStorage(baron.CharacterCard, ra.CharacterCard, legacy.CharacterCard, haka.CharacterCard, b3h3mth);
             GoToEndOfTurn(cybersphere);
-            QuickHPCheck(0, 0, 0, -4);
+            QuickHPCheck(0, 0, 0, -4, 0);
+
+        }
+
+        [Test()]
+        public void TestDr3Dnt()
+        {
+            SetupGameController("BaronBlade", "Ra", "Legacy", "Haka", "Cauldron.TheCybersphere");
+            StartGame();
+
+            //destroy mdp to make him vulnerable
+            DestroyCard(GetCardInPlay("MobileDefensePlatform"), baron.CharacterCard);
+
+            GoToPlayCardPhase(cybersphere);
+            Card dr3Dnt = PlayCard("Dr3Dnt");
+
+            //At the end of the environment turn, this card deals each other target 2 projectile damage.
+            QuickHPStorage(baron.CharacterCard, ra.CharacterCard, legacy.CharacterCard, haka.CharacterCard, dr3Dnt);
+            GoToEndOfTurn(cybersphere);
+            QuickHPCheck(-2, -2, -2, -2, 0);
 
         }
 
