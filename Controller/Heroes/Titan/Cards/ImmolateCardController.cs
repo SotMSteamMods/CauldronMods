9e09632a6deb7f84f84f23abf7a89bf6a6336062
Jake <mgirman520@gmail.com>
2020-11-30 11:29:34 -0500

    Namespaces

--- Controller/Heroes/Titan/Cards/ImmolateCardController.cs
+++ Controller/Heroes/Titan/Cards/ImmolateCardController.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-namespace Cauldron
+namespace Cauldron.Titan
 {
     public class ImmolateCardController : CardController
     {
---
+++ Controller/Heroes/Titan/Cards/JuggernautStrikeCardController.cs
@@ -0,0 +1,14 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+
+namespace Cauldron.Titan
+{
+    public class JuggernautStrikeCardController : CardController
+    {
+        public JuggernautStrikeCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+    }
+}
