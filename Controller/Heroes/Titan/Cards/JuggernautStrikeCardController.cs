dd36790872eb305811ef4c8c0a4949921a1d567f
Jake <mgirman520@gmail.com>
2020-11-30 11:47:11 -0500

    Update JuggernautStrikeCardController.cs

--- Controller/Heroes/Titan/Cards/JuggernautStrikeCardController.cs
+++ Controller/Heroes/Titan/Cards/JuggernautStrikeCardController.cs
@@ -1,6 +1,9 @@
 ﻿using Handelabra.Sentinels.Engine.Controller;
 using Handelabra.Sentinels.Engine.Model;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Cauldron.Titan
 {
@@ -9,6 +12,50 @@ namespace Cauldron.Titan
         public JuggernautStrikeCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
 
+        }
+
+        public override IEnumerator Play()
+        {
+            Card titanform = FindCard("Titanform");
+            IEnumerator coroutine = null;
+            //If Titanform is in your hand you may play it now.
+            if (titanform.Location.IsHand && titanform.Location.OwnerTurnTaker == base.TurnTaker)
+            {
+                coroutine = base.GameController.PlayCard(base.TurnTakerController, titanform, optional: true, cardSource: base.GetCardSource());
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+            }
+
+            //{Titan} deals 1 target 4 infernal damage...
+            IEnumerable<Card> choices = FindCardsWhere(new LinqCardCriteria((Card c) => c.IsTarget && c.IsInPlay));
+            List<SelectCardDecision> selectTargets = new List<SelectCardDecision>();
+            coroutine = base.GameController.SelectTargetsAndDealDamage(base.HeroTurnTakerController, new DamageSource(base.GameController, base.CharacterCard), 4, DamageType.Infernal, 1, false, 1, storedResultsDecisions: selectTargets, cardSource: base.GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+
+            //..and each other target from that deck 1 projectile damage.
+            coroutine = base.GameController.DealDamage(base.HeroTurnTakerController, base.CharacterCard, (Card c) => c.NativeDeck == selectTargets.FirstOrDefault().SelectedCard.NativeDeck, 1, DamageType.Projectile, cardSource: base.GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
         }
     }
 }
