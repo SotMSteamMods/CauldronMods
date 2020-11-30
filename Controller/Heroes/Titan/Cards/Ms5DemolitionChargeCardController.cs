7a68b8d971b2c02fb2d162249914177c6cd71fa1
Jake <mgirman520@gmail.com>
2020-11-30 14:51:25 -0500

    Create Ms5DemolitionChargeCardController.cs

---
+++ Controller/Heroes/Titan/Cards/Ms5DemolitionChargeCardController.cs
@@ -0,0 +1,46 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+
+namespace Cauldron.Titan
+{
+    public class Ms5DemolitionChargeCardController : CardController
+    {
+        public Ms5DemolitionChargeCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+
+        public override IEnumerator Play()
+        {
+            //Destroy all environment cards.
+            //For each environment card destroyed this way, {Titan} deals himself 1 fire damage.
+            IEnumerator coroutine = base.DestroyCardsAndDoActionBasedOnNumberOfCardsDestroyed(base.HeroTurnTakerController, new LinqCardCriteria((Card c) => c.IsEnvironment), this.DealDamageResponse);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+
+        private IEnumerator DealDamageResponse(int count)
+        {
+            //...{Titan} deals himself 1 fire damage.
+            IEnumerator coroutine = base.DealDamage(base.CharacterCard, base.CharacterCard, 1, DamageType.Fire, cardSource: base.GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+    }
+}
