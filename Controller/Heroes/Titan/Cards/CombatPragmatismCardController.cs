ccd00481e3e65f8c5e51e98d36795103fe9e0322
Jake <mgirman520@gmail.com>
2020-11-30 10:21:50 -0500

    Initialize
    And add Oni to Decklist

---
+++ Controller/Heroes/Titan/Cards/CombatPragmatismCardController.cs
@@ -0,0 +1,14 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+
+namespace Cauldron.Titan
+{
+    public class CombatPragmatismCardController : CardController
+    {
+        public CombatPragmatismCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+    }
+}
