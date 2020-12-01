0ea0c89ffd234b15509cc7eab9c237c1208286f6
Jake <mgirman520@gmail.com>
2020-11-30 23:28:07 -0500

    Decklist Icons

---
+++ Controller/Heroes/Titan/CharacterCards/MinistryOfStrategicScienceTitanCharacter.cs
@@ -0,0 +1,96 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System.Collections;
+
+namespace Cauldron.Titan
+{
+    public class MinistryOfStrategicScienceTitanCharacterCardController : HeroCharacterCardController
+    {
+        public MinistryOfStrategicScienceTitanCharacterCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+        public override IEnumerator UseIncapacitatedAbility(int index)
+        {
+            IEnumerator coroutine;
+            switch (index)
+            {
+                case 0:
+                    {
+                        //One player may use a power now.
+                        coroutine = base.GameController.SelectHeroToUsePower(base.HeroTurnTakerController, cardSource: base.GetCardSource());
+                        if (base.UseUnityCoroutines)
+                        {
+                            yield return base.GameController.StartCoroutine(coroutine);
+                        }
+                        else
+                        {
+                            base.GameController.ExhaustCoroutine(coroutine);
+                        }
+                        break;
+                    }
+                case 1:
+                    {
+                        //One player may play a card now.
+                        coroutine = base.SelectHeroToPlayCard(this.HeroTurnTakerController);
+                        if (base.UseUnityCoroutines)
+                        {
+                            yield return base.GameController.StartCoroutine(coroutine);
+                        }
+                        else
+                        {
+                            base.GameController.ExhaustCoroutine(coroutine);
+                        }
+                        break;
+
+                    }
+                case 2:
+                    {
+                        //Increase the next damage dealt by a hero target by 1."
+                        IncreaseDamageStatusEffect statusEffect = new IncreaseDamageStatusEffect(1)
+                        {
+                            NumberOfUses = 1,
+                            SourceCriteria = { IsHero = true }
+                        };
+                        coroutine = base.AddStatusEffect(statusEffect);
+                        if (base.UseUnityCoroutines)
+                        {
+                            yield return base.GameController.StartCoroutine(coroutine);
+                        }
+                        else
+                        {
+                            base.GameController.ExhaustCoroutine(coroutine);
+                        }
+                        break;
+                    }
+            }
+        }
+
+        public override IEnumerator UsePower(int index = 0)
+        {
+            //You may play an ongoing card.
+            IEnumerator coroutine = base.GameController.SelectAndPlayCardFromHand(base.HeroTurnTakerController, true, cardCriteria: new LinqCardCriteria((Card c) => c.IsOngoing), cardSource: base.GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+
+            //Draw a card.
+            coroutine = base.DrawCard();
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+    }
+
+}
