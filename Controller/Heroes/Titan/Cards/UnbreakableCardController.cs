c6ae15c3d7f5ff43099d89b5571b27a47bab2f25
Jake <mgirman520@gmail.com>
2020-11-30 22:30:31 -0500

    Update UnbreakableCardController.cs

--- Controller/Heroes/Titan/Cards/UnbreakableCardController.cs
+++ Controller/Heroes/Titan/Cards/UnbreakableCardController.cs
@@ -1,6 +1,7 @@
 ﻿using Handelabra.Sentinels.Engine.Controller;
 using Handelabra.Sentinels.Engine.Model;
 using System;
+using System.Collections;
 
 namespace Cauldron.Titan
 {
@@ -11,8 +12,16 @@ namespace Cauldron.Titan
 
         }
 
-        //Skip any effects which would act at the end of the villain turn.
-        //You may not use Powers. You may not draw cards.
-        //At the start of your turn, destroy this card.
+        public override void AddTriggers()
+        {
+            //Skip any effects which would act at the end of the villain turn.
+            base.AddTrigger<PhaseChangeAction>((PhaseChangeAction action) => action.ToPhase.IsEnd && action.ToPhase.IsVillain && base.GameController.IsTurnTakerVisibleToCardSource(action.ToPhase.TurnTaker, base.GetCardSource()), (PhaseChangeAction action) => base.CancelAction(action), TriggerType.CancelAction, TriggerTiming.Before);
+            //You may not use powers.
+            base.CannotUsePowers((TurnTakerController ttc) => ttc == base.HeroTurnTakerController);
+            //You may not draw cards.
+            base.CannotDrawCards((TurnTakerController ttc) => ttc == base.HeroTurnTakerController);
+            //At the start of your turn, destroy this card.
+            base.AddStartOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, this.DestroyThisCardResponse, TriggerType.DestroySelf);
+        }
     }
 }
