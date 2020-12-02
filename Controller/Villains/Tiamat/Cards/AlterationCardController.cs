21a3270119058cd19ce62c7e97613d45a6fda398
Jake <mgirman520@gmail.com>
2020-12-01 22:18:51 -0500

    Update AlterationCardController.cs
    Closes #285 - Flipped Alteration play order

--- Controller/Villains/Tiamat/Cards/AlterationCardController.cs
+++ Controller/Villains/Tiamat/Cards/AlterationCardController.cs
@@ -15,8 +15,8 @@ namespace Cauldron.Tiamat
         public override IEnumerator Play()
         {
             //Play the top card of the environment deck. Play the top card of the villain deck.
-            IEnumerator coroutine = base.GameController.PlayTopCard(this.DecisionMaker, base.TurnTakerController, cardSource: base.GetCardSource());
-            IEnumerator coroutine2 = base.GameController.PlayTopCard(this.DecisionMaker, base.FindEnvironment(), cardSource: base.GetCardSource());
+            IEnumerator coroutine = base.GameController.PlayTopCard(this.DecisionMaker, base.FindEnvironment(), cardSource: base.GetCardSource());
+            IEnumerator coroutine2 = base.GameController.PlayTopCard(this.DecisionMaker, base.TurnTakerController, cardSource: base.GetCardSource());
             if (base.UseUnityCoroutines)
             {
                 yield return base.GameController.StartCoroutine(coroutine);
