d3dfdbb61e3bd7ab7cd331a457ada1232cea7d3f
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2020-12-02 01:55:40 -0600

    Added SubClass Controller
    Added Functions for Moving to the left or right of a card
    Added Functions for Getting the number of cards to the left or right of a card
    Added Functions for Moving to the far left or right of the environment play area

--- Controller/Environments/SuperstormAkela/CardSubClasses/SuperstormAkelaCardController.cs
+++ Controller/Environments/SuperstormAkela/CardSubClasses/SuperstormAkelaCardController.cs
@@ -1,8 +1,10 @@
 ﻿using Handelabra.Sentinels.Engine.Controller;
 using Handelabra.Sentinels.Engine.Model;
-using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using Handelabra;
 
 namespace Cauldron.SuperstormAkela
 {
@@ -13,6 +15,146 @@ namespace Cauldron.SuperstormAkela
         {
 
         }
+
+        protected IEnumerator MoveCardToFarLeft(Card card)
+        {
+            List<Card> list = TurnTaker.PlayArea.Cards.ToList();
+            list.Remove(card);
+            list.Insert(0, card);
+            list.ForEach(delegate (Card c)
+            {
+                base.GameController.Game.AssignPlayCardIndex(c);
+            });
+
+            IEnumerator coroutine = GameController.SendMessageAction("Moved " + card.Title + " to the far left of the environment's play area.", Priority.Medium, GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+
+            Log.Debug(card.Title + " was moved to the far left of the environment's play area.");
+
+            yield break;
+        }
+
+
+        protected IEnumerator MoveCardToFarRight(Card card)
+        {
+            List<Card> list = TurnTaker.PlayArea.Cards.ToList();
+            list.Remove(card);
+            list.Add(card);
+            list.ForEach(delegate (Card c)
+            {
+                base.GameController.Game.AssignPlayCardIndex(c);
+            });
+
+            IEnumerator coroutine = GameController.SendMessageAction("Moved " + card.Title + " to the far right of the environment's play area.", Priority.Medium, GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+
+            Log.Debug(card.Title + " was moved to the far right of the environment's play area.");
+            yield break;
+        }
+
+        protected IEnumerator MoveToTheLeftOfCard(Card card, Card cardToMoveLeftOf)
+        {
+            List<Card> list = TurnTaker.PlayArea.Cards.ToList();
+            list.Remove(card);
+            int index = list.IndexOf(cardToMoveLeftOf);
+            list.Insert(index, card);
+            list.ForEach(delegate (Card c)
+            {
+                base.GameController.Game.AssignPlayCardIndex(c);
+            });
+
+            IEnumerator coroutine = GameController.SendMessageAction("Moved " + card.Title + " to the left of " + cardToMoveLeftOf.Title, Priority.Medium, GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+
+            Log.Debug(card.Title + " was moved to the left of " + cardToMoveLeftOf.Title + " in the environment's play area.");
+
+            yield break;
+        }
+
+        protected IEnumerator MoveToTheRightOfCard(Card card, Card cardToMoveRightOf)
+        {
+            List<Card> list = TurnTaker.PlayArea.Cards.ToList();
+            list.Remove(card);
+            int index = list.IndexOf(cardToMoveRightOf);
+            list.Insert(index + 1, card);
+            list.ForEach(delegate (Card c)
+            {
+                base.GameController.Game.AssignPlayCardIndex(c);
+            });
+            IEnumerator coroutine = GameController.SendMessageAction("Moved " + card.Title + " to the right of " + cardToMoveRightOf.Title, Priority.Medium, GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+
+            Log.Debug(card.Title + " was moved to the right of " + cardToMoveRightOf.Title + " in the environment's play area.");
+
+            yield break;
+        }
+
+        protected int? GetNumberOfCardsToTheLeftOfThisOne(Card card)
+        {
+            if (!TurnTaker.PlayArea.HasCard(card))
+            {
+                return null;
+            }
+            int position = FindCardPositionInLocation(TurnTaker.PlayArea, card);
+            List<Card> orderedPlayArea = GetOrderedCardsInLocation(TurnTaker.PlayArea).ToList();
+            int numCardsToTheLeft = position;
+            return new int?(numCardsToTheLeft);
+        }
+
+        protected int? GetNumberOfCardsToTheRightOfThisOne(Card card)
+        {
+            if (!TurnTaker.PlayArea.HasCard(card))
+            {
+                return null;
+            }
+            int position = FindCardPositionInLocation(TurnTaker.PlayArea, card);
+            List<Card> orderedPlayArea = GetOrderedCardsInLocation(TurnTaker.PlayArea).ToList();
+            int numCardsToTheRight = orderedPlayArea.Count() - position + 1;
+            return new int?(numCardsToTheRight);
+        }
+
+        private IEnumerable<Card> GetOrderedCardsInLocation(Location location)
+        {
+            return location.Cards.OrderBy((Card c) => c.PlayIndex);
+        }
+
+        private int FindCardPositionInLocation(Location location, Card c)
+        {
+            return GetOrderedCardsInLocation(location).ToList().IndexOf(c);
+        }
+
+
+
+
+
 
 
     }
