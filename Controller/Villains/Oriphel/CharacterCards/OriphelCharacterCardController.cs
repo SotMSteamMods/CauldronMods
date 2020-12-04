30353fab2f29b99d0a96cdeacb04f13de0b76051
LNBertolini <bertolini.lucas.n@gmail.com>
2020-12-04 10:22:13 -0600

    Add Oriphel back side triggers and tests

--- Controller/Villains/Oriphel/CharacterCards/OriphelCharacterCardController.cs
+++ Controller/Villains/Oriphel/CharacterCards/OriphelCharacterCardController.cs
@@ -29,15 +29,47 @@ namespace Cauldron.Oriphel
             }
             else
             {
-                //"When {Oriphel} is flipped to this side, shuffle all relics in the villain trash into the villain deck.",
                 //"Reduce damage dealt to {Oriphel} by 1.",
-                //"At the end of the villain turn, {Oriphel} deals the 2 hero targets with the highest HP {H - 1} infernal damage each.",
-                //"When there are 2 villain relics in the villain trash, flip {Oriphel}'s villain character cards."
+                AddSideTrigger(AddReduceDamageTrigger((Card c) => c == this.Card, 1));
 
+                //"At the end of the villain turn, {Oriphel} deals the 2 hero targets with the highest HP {H - 1} infernal damage each.",
+                AddSideTrigger(AddDealDamageAtEndOfTurnTrigger(this.TurnTaker, this.Card, (Card c) => c.IsHero, TargetType.HighestHP, H - 1, DamageType.Infernal, numberOfTargets: 2));
+
+                //"When there are 2 villain relics in the villain trash, flip {Oriphel}'s villain character cards."
+                AddSideTrigger(AddTrigger<GameAction>(CheckCardsInTrashCriteria, FlipThisCharacterCardResponse, TriggerType.FlipCard, TriggerTiming.After));
             }
             AddDefeatedIfDestroyedTriggers();
         }
+        
+        public override IEnumerator AfterFlipCardImmediateResponse()
+        {
+            RemoveSideTriggers();
+            if (this.Card.IsFlipped)
+            {
+                //"When {Oriphel} is flipped to this side, shuffle all relics in the villain trash into the villain deck.",
+                var relicsInTrash = FindCardsWhere((Card c) => c.Location == TurnTaker.Trash && c.IsRelic, true, GetCardSource());
+                IEnumerator coroutine = GameController.ShuffleCardsIntoLocation(DecisionMaker, relicsInTrash, TurnTaker.Deck, cardSource: GetCardSource());
+                if (UseUnityCoroutines)
+                {
+                    yield return GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    GameController.ExhaustCoroutine(coroutine);
+                }
+            }
+            AddSideTriggers();
+            yield break;
+        }
 
+        private bool CheckCardsInTrashCriteria(GameAction ga)
+        {
+            if (ga is MoveCardAction || ga is BulkMoveCardsAction || ga is DestroyCardAction)
+            {
+                return FindCardsWhere((Card c) => c.Location == TurnTaker.Trash && c.IsRelic, true, GetCardSource()).Count() >= 2;
+            }
+            return false;
+        }
         private IEnumerator DestroyOngoingAndPlayCardResponse(CardEntersPlayAction cep)
         {
             IEnumerator coroutine = GameController.DestroyCard(DecisionMaker, cep.CardEnteringPlay, cardSource: GetCardSource());
--- Testing/Villains/OriphelTests.cs
+++ Testing/Villains/OriphelTests.cs
@@ -143,6 +143,11 @@ namespace CauldronTests
             QuickShuffleStorage(oriphel);
             Card guardian = PlayCard("High" + name);
 
+            //stack deck so we know what Jade's extra play will be
+            PutOnDeck("Sandstorm");
+            PutOnDeck("WorldShardkey");
+            PutOnDeck("Mirage");
+
             DestroyCard(guardian);
             QuickShuffleCheck(1);
             AssertNumberOfCardsInPlay((Card c) => c.IsRelic && c.IsVillain, 1);
@@ -199,6 +204,90 @@ namespace CauldronTests
 
             AssertIsInPlay(goon);
             AssertInTrash(grand);
+        }
+        [Test]
+        public void TestOriphelImmediateFlipResponse()
+        {
+            SetupGameController("Cauldron.Oriphel", "Legacy", "Ra", "Tempest", "Megalopolis");
+            StartGame();
+            CleanupStartingCards();
+
+            PutInTrash(new string[] { "MoonShardkey", "SunShardkey", "VeilShardkey", "WorldShardkey" });
+            QuickShuffleStorage(oriphel);
+
+            FlipCard(oriphel);
+
+            AssertNumberOfCardsInTrash(oriphel, 0);
+            Assert.AreEqual(oriphel.CharacterCard.Title, "Oriphel");
+            QuickShuffleCheck(1);
+        }
+        [Test]
+        public void TestOriphelDamageReduction()
+        {
+            SetupGameController("Cauldron.Oriphel", "Legacy", "Ra", "Tempest", "Megalopolis");
+            StartGame();
+            CleanupStartingCards();
+
+            QuickHPStorage(oriphel, legacy);
+            FlipCard(oriphel);
+
+            DealDamage(legacy, oriphel, 2, DTM);
+            QuickHPCheck(-1, 0);
+            DealDamage(oriphel, legacy, 2, DTM);
+            QuickHPCheck(0, -2);
+        }
+        [Test]
+        public void TestOriphelEndOfTurnDamageHIs3()
+        {
+            SetupGameController("Cauldron.Oriphel", "Legacy", "Ra", "Tempest", "Megalopolis");
+            StartGame();
+            CleanupStartingCards();
+
+            FlipCard(oriphel);
+
+            QuickHPStorage(oriphel, legacy, ra, tempest);
+            GoToEndOfTurn();
+            QuickHPCheck(0, -2, -2, 0);
+        }
+        [Test]
+        public void TestOriphelEndOfTurnDamageHIs4()
+        {
+            SetupGameController("Cauldron.Oriphel", "Legacy", "Ra", "Tempest", "Haka", "Megalopolis");
+            StartGame();
+            CleanupStartingCards();
+
+            FlipCard(oriphel);
+
+            QuickHPStorage(oriphel, legacy, ra, tempest, haka);
+            GoToEndOfTurn();
+            QuickHPCheck(0, -3, 0, 0, -3);
+        }
+        [Test]
+        public void TestOriphelFlipConditionDestruction()
+        {
+            SetupGameController("Cauldron.Oriphel", "Legacy", "Ra", "Tempest", "Haka", "Megalopolis");
+            StartGame();
+            CleanupStartingCards();
+
+            FlipCard(oriphel);
+            PutInTrash("MoonShardkey");
+            Card sun = PlayCard("SunShardkey");
+            DestroyCard(sun);
+            AssertNotFlipped(oriphel);
+        }
+        [Test]
+        public void TestOriphelFlipConditionDiscardFromDeck()
+        {
+            SetupGameController("Cauldron.Oriphel", "Legacy", "Ra", "Tempest", "Knyfe", "Megalopolis");
+            StartGame();
+            CleanupStartingCards();
+
+            FlipCard(oriphel);
+            PutInTrash("MoonShardkey");
+            PutOnDeck("SunShardkey");
+
+            PlayCard("WreckingUppercut");
+            AssertNotFlipped(oriphel);
         }
     }
 }
