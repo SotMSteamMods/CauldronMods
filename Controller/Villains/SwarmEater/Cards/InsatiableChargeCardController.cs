c15502632e97fd0c14c0480a3f6bb6e3d178b06c
Breen Young <breen.young@gmail.com>
2020-12-04 14:44:10 -0400

    code cleanup, check DamageSource for nulls, use IsEquipement method instead of checking keywords

--- Controller/Villains/SwarmEater/Cards/InsatiableChargeCardController.cs
+++ Controller/Villains/SwarmEater/Cards/InsatiableChargeCardController.cs
@@ -1,6 +1,5 @@
 ﻿using Handelabra.Sentinels.Engine.Controller;
 using Handelabra.Sentinels.Engine.Model;
-using System;
 using System.Collections;
 
 namespace Cauldron.SwarmEater
@@ -9,7 +8,6 @@ namespace Cauldron.SwarmEater
     {
         public InsatiableChargeCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
-
         }
 
         public override IEnumerator Play()
@@ -24,19 +22,21 @@ namespace Cauldron.SwarmEater
             {
                 base.GameController.ExhaustCoroutine(coroutine);
             }
-            yield break;
         }
 
         public override void AddTriggers()
         {
             //For each target destroyed by {SwarmEater} this way...
-            base.AddTrigger<DestroyCardAction>((DestroyCardAction d) => d.CardToDestroy.Card.IsTarget && d.WasCardDestroyed && d.ActionSource is DealDamageAction && ((DealDamageAction)d.ActionSource).DamageSource.IsSameCard(base.CharacterCard), new Func<DestroyCardAction, IEnumerator>(this.DestroyTargetResponse), TriggerType.DestroyCard, TriggerTiming.After, ActionDescription.Unspecified, false, true, null, false, null, null, false, false);
+            base.AddTrigger<DestroyCardAction>((DestroyCardAction d) => d.CardToDestroy.Card.IsTarget 
+                && d.WasCardDestroyed && d.ActionSource is DealDamageAction dealDamageAction 
+                && dealDamageAction.DamageSource != null 
+                && dealDamageAction.DamageSource.IsSameCard(base.CharacterCard), this.DestroyTargetResponse, TriggerType.DestroyCard, TriggerTiming.After, ActionDescription.Unspecified, false, true, null, false, null, null);
         }
 
         private IEnumerator DestroyTargetResponse(DestroyCardAction action)
         {
             //...destroy 1 hero ongoing or equipment card.
-            IEnumerator coroutine = base.GameController.SelectAndDestroyCard(this.DecisionMaker, new LinqCardCriteria((Card c) => c.IsHero && (c.IsOngoing || c.DoKeywordsContain("equipment"))), false);
+            IEnumerator coroutine = base.GameController.SelectAndDestroyCard(this.DecisionMaker, new LinqCardCriteria((Card c) => c.IsHero && (c.IsOngoing || IsEquipment(c))), false);
             if (base.UseUnityCoroutines)
             {
                 yield return base.GameController.StartCoroutine(coroutine);
@@ -45,7 +45,6 @@ namespace Cauldron.SwarmEater
             {
                 base.GameController.ExhaustCoroutine(coroutine);
             }
-            yield break;
         }
     }
 }
