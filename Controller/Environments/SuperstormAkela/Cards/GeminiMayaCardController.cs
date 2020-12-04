10f7bf0502ab13acf99932de66ed173b8c421181
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2020-12-03 23:52:27 -0600

    Added GeminiMayaController

--- Controller/Environments/SuperstormAkela/Cards/GeminiMayaCardController.cs
+++ Controller/Environments/SuperstormAkela/Cards/GeminiMayaCardController.cs
@@ -14,6 +14,31 @@ namespace Cauldron.SuperstormAkela
 
         }
 
+        public override void AddTriggers()
+        {
+            //Increase all project damage dealt by 1.
+            AddIncreaseDamageTrigger((DealDamageAction dd) => dd.DamageType == DamageType.Projectile, 1);
+
+            //At the end of the environment turn, this card deals the 2 targets with the highest HP X+1 lightning damage each, where X is the number of environment cards to the left of this one.
+            AddEndOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, DealDamageResponse, TriggerType.DealDamage);
+        }
+
+        private IEnumerator DealDamageResponse(PhaseChangeAction pca)
+        {
+            // this card deals the 2 targets with the highest HP X+1 lightning damage each, where X is the number of environment cards to the left of this one.
+
+            Func<Card, int?> amount = (Card c) => GetNumberOfCardsToTheLeftOfThisOne(base.Card) + 1;
+            IEnumerator coroutine = DealDamageToHighestHP(base.Card, 1, (Card c) => c.IsTarget, amount, DamageType.Lightning, numberOfTargets: () => 2);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
 
     }
 }
