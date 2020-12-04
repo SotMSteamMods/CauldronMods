819a24463cd071f949a62b9dedbf424bfa17bf16
Breen Young <breen.young@gmail.com>
2020-12-04 14:34:28 -0400

    Check damage source for null

--- Controller/Villains/SwarmEater/Cards/HuntingGroundsCardController.cs
+++ Controller/Villains/SwarmEater/Cards/HuntingGroundsCardController.cs
@@ -15,9 +15,9 @@ namespace Cauldron.SwarmEater
         public override void AddTriggers()
         {
             //Reduce damage dealt to {SwarmEater} by environment targets by 1.
-            base.AddReduceDamageTrigger((DealDamageAction action) => action.Target == base.CharacterCard && action.DamageSource.IsEnvironmentTarget, (DealDamageAction action) => 1);
+            base.AddReduceDamageTrigger((DealDamageAction action) => action.DamageSource != null && action.Target == base.CharacterCard && action.DamageSource.IsEnvironmentTarget, (DealDamageAction action) => 1);
             //Increase damage dealt by {SwarmEater} to environment targets by 1.
-            base.AddIncreaseDamageTrigger((DealDamageAction action) => action.Target.IsEnvironmentTarget && action.DamageSource.Card == base.CharacterCard, 1);
+            base.AddIncreaseDamageTrigger((DealDamageAction action) => action.DamageSource != null && action.Target.IsEnvironmentTarget && action.DamageSource.Card == base.CharacterCard, 1);
             //Whenever {SwarmEater} destroys a target, play the top card of the environment deck.
             base.AddTrigger<DestroyCardAction>((DestroyCardAction action) => action.ResponsibleCard == base.CharacterCard, base.PlayTheTopCardOfTheEnvironmentDeckResponse, TriggerType.PlayCard, TriggerTiming.After);
         }
