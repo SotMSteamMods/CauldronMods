03ccfe95010d6550e8bf6396d0741e21f869153b
byoung <breen.young@gmail.com>
2020-12-04 23:40:41 -0400

    stub character card

---
+++ Controller/Heroes/Cypher/CharacterCards/CypherCharacterCardController.cs
@@ -0,0 +1,26 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+
+namespace Cauldron.Cypher
+{
+    public class CypherCharacterCardController : CypherBaseCharacterCardController
+    {
+
+        public CypherCharacterCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+
+        public override IEnumerator UsePower(int index = 0)
+        {
+            return base.UsePower(index);
+        }
+
+        public override IEnumerator UseIncapacitatedAbility(int index)
+        {
+            return base.UseIncapacitatedAbility(index);
+        }
+    }
+}
