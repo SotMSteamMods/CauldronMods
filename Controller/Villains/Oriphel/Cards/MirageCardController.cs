06a72a4d6edfb94ec71ce484211b1b17385c0ca2
LNBertolini <bertolini.lucas.n@gmail.com>
2020-12-04 23:43:43 -0600

    Mirage controller

--- Controller/Villains/Oriphel/Cards/MirageCardController.cs
+++ Controller/Villains/Oriphel/Cards/MirageCardController.cs
@@ -15,7 +15,45 @@ namespace Cauldron.Oriphel
         public override IEnumerator Play()
         {
             //"Reveal the top 2 cards of the villain deck. Put any revealed targets or Transformation cards into play and discard the rest.",
+            var fakeStorage = new List<Card> { };
+            IEnumerator coroutine = RevealCards_PutSomeIntoPlay_DiscardRemaining(TurnTakerController, TurnTaker.Deck, 2, new LinqCardCriteria((Card c) => c.IsTarget || IsTransformation(c)), true, fakeStorage, fakeStorage, fakeStorage);
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+
             //"Each Goon deals the hero target with the highest HP 1 fire damage."
+            coroutine = GameController.SelectCardsAndDoAction(DecisionMaker,
+                                                    new LinqCardCriteria((Card c) => c.IsInPlayAndHasGameText && IsGoon(c), "goon"),
+                                                    SelectionType.CardToDealDamage,
+                                                    HitHighestHP,
+                                                    cardSource: GetCardSource());
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+
+        private IEnumerator HitHighestHP(Card c)
+        {
+            IEnumerator coroutine = DealDamageToHighestHP(c, 1, (Card target) => target.IsHero, (target) => 1, DamageType.Fire);
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
             yield break;
         }
     }
