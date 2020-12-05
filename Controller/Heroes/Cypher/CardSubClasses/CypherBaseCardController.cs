9a33913a9b2a7c4983c2b474a2d20287ac3c35b5
byoung <breen.young@gmail.com>
2020-12-04 23:40:30 -0400

    helper base classes

---
+++ Controller/Heroes/Cypher/CardSubClasses/CypherBaseCardController.cs
@@ -0,0 +1,56 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cauldron.Cypher
+{
+    public class CypherBaseCardController : CardController
+    {
+        public CypherBaseCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+        }
+
+        protected bool IsAugment(Card card)
+        {
+            return card != null && base.GameController.DoesCardContainKeyword(card, "augment");
+        }
+
+        protected bool IsAugmented(Card hero)
+        {
+            return hero.NextToLocation.HasCards && hero.GetAllNextToCards(false).Any(IsAugment);
+        }
+
+        protected List<Card> GetAugmentsInPlay()
+        {
+            return FindCardsWhere(c => c.IsInPlay && IsAugment(c)).ToList();
+        }
+
+        protected List<Card> GetAugmentedHeroCards()
+        {
+            return FindCardsWhere(c => c.IsHero && c.NextToLocation.HasCards && c.NextToLocation.Cards.Any(IsAugment)).ToList();
+        }
+
+        protected List<TurnTaker> GetAugmentedHeroTurnTakers()
+        {
+            return FindTurnTakersWhere(tt =>
+                tt.IsHero && tt.CharacterCard.NextToLocation.HasCards &&
+                tt.CharacterCard.NextToLocation.Cards.Any(IsAugment)).ToList();
+        }
+
+        protected List<Card> GetAugmentsForHero(Card hero)
+        {
+            return !IsAugmented(hero) ? new List<Card>() : hero.GetAllNextToCards(false).Where(IsAugment).ToList();
+        }
+
+        protected List<Card> GetValidAugmentMoveHeroes(Card sourceHero)
+        {
+            return FindCardsWhere(c => c != sourceHero && c.IsHero).ToList();
+        }
+
+        protected List<TurnTaker> GetValidAugmentMoveHeroes(TurnTaker sourceHero)
+        {
+            return FindTurnTakersWhere(tt => tt != sourceHero && tt.IsHero).ToList();
+        }
+    }
+}
---
+++ Controller/Heroes/Cypher/CardSubClasses/CypherBaseCharacterCardController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+
+namespace Cauldron.Cypher
+{
+    public class CypherBaseCharacterCardController : HeroCharacterCardController
+    {
+        public CypherBaseCharacterCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+
+        protected bool IsAugment(Card card)
+        {
+            return card != null && base.GameController.DoesCardContainKeyword(card, "augment");
+        }
+
+        protected List<Card> GetAugmentedHeroes()
+        {
+            return FindCardsWhere(c => c.IsHero && c.NextToLocation.HasCards && c.NextToLocation.Cards.Any(IsAugment)).ToList();
+        }
+    }
+}
