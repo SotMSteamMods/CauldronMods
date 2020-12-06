5705f941b0cdf1df60a141370da460fd3081a0a4
Jake <mgirman520@gmail.com>
2020-12-05 22:47:42 -0500

    Create BeforeTheThunderCardController.cs

---
+++ Controller/Heroes/Cricket/Cards/BeforeTheThunderCardController.cs
@@ -0,0 +1,56 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cauldron
+{
+    public class BeforeTheThunderCardController : CardController
+    {
+        public BeforeTheThunderCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+
+        public override IEnumerator Play()
+        {
+            //Draw 3 cards.
+            IEnumerator coroutine = base.DrawCards(base.HeroTurnTakerController, 3);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+
+            //Select a deck to reveal from
+            List<SelectLocationDecision> storedResults = new List<SelectLocationDecision>();
+            coroutine = base.GameController.SelectADeck(base.HeroTurnTakerController, SelectionType.RevealTopCardOfDeck, null, storedResults, cardSource: base.GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+
+            List<Card> list = new List<Card>();
+            //Reveal the top card of 1 deck, then replace it.
+            coroutine = base.GameController.RevealCards(base.TurnTakerController, storedResults.FirstOrDefault().SelectedLocation.Location, 1, list, revealedCardDisplay: RevealedCardDisplay.ShowRevealedCards, cardSource: base.GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+    }
+}
