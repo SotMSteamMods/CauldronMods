226285ddfce10db735e22a4afbe5fc071928e8d7
Jake <mgirman520@gmail.com>
2020-12-06 13:34:44 -0500

    Update Swarming Frequency
    Namespace update

---
+++ Controller/Heroes/Cricket/Cards/VantagePointCardController.cs
@@ -0,0 +1,14 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+
+namespace Cauldron
+{
+    public class VantagePointCardController : CardController
+    {
+        public VantagePointCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+    }
+}
