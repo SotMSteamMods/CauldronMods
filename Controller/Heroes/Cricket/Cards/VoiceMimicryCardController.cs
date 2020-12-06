4fc433edd2482b3e1422feb8eb091effc5f342fc
Jake <mgirman520@gmail.com>
2020-12-06 13:50:09 -0500

    Create VoiceMimicryCardController.cs

---
+++ Controller/Heroes/Cricket/Cards/VoiceMimicryCardController.cs
@@ -0,0 +1,43 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cauldron.Cricket
+{
+    public class VoiceMimicryCardController : CardController
+    {
+        public VoiceMimicryCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+
+        public override IEnumerator Play()
+        {
+            List<SelectLocationDecision> storedResults = new List<SelectLocationDecision>();
+            //Play the top card of a deck.
+            IEnumerator coroutine = base.GameController.SelectADeck(base.HeroTurnTakerController, SelectionType.PlayTopCard, null, storedResults, cardSource: base.GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+
+            coroutine = base.GameController.PlayTopCardOfLocation(base.TurnTakerController, storedResults.FirstOrDefault().SelectedLocation.Location, cardSource: base.GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+    }
+}
