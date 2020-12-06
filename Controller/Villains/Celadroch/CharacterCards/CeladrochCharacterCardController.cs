a978c99361927fbc1d3f4855e002659189cf00f9
Vilnor <onlytelford@yahoo.com>
2020-12-05 21:17:06 -0500

    a

--- Controller/Villains/Celadroch/CharacterCards/CeladrochCharacterCardController.cs
+++ Controller/Villains/Celadroch/CharacterCards/CeladrochCharacterCardController.cs
@@ -3,6 +3,7 @@ using Handelabra.Sentinels.Engine.Model;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 
 
 namespace Cauldron.Celadroch
@@ -35,7 +36,7 @@ namespace Cauldron.Celadroch
          */
 
         public static readonly string StormPoolIdentifier = "StormPool";
-
+        public static readonly string[] MinionKeywords = new[] { "elemental", "zombie", "demon", "chosen" };
 
         public TokenPool StormPool => CharacterCard.FindTokenPool(StormPoolIdentifier);
 
@@ -80,13 +81,18 @@ namespace Cauldron.Celadroch
             // Flipped side (The Dark Mountain)
             else
             {
+                //At the start of the villain turn, add 1 token to the storm pool.
                 AddStartOfTurnTrigger(tt => tt == TurnTaker, pca => GameController.AddTokensToPool(StormPool, 1, GetCardSource()), TriggerType.AddTokensToPool);
 
-                AddStartOfTurnTrigger(tt => tt == TurnTaker, pca => DoNothing(), new[] { TriggerType.ModifyTokens, TriggerType.PlayCard });
+                //You may play the top card of the villain deck to remove 2 cards from the storm pool.
+                AddStartOfTurnTrigger(tt => tt == TurnTaker, StartOfTurnOptionalCardPlayResponse, new[] { TriggerType.ModifyTokens, TriggerType.PlayCard });
 
-                AddTrigger<PlayCardAction>(pca => pca.CardToPlay.Owner == TurnTaker && !pca.IsPutIntoPlay && pca.Origin == TurnTaker.Deck && !pca.FromBottom,
-                                            pca => DoNothing(), TriggerType.PutIntoPlay, TriggerTiming.After);
+                // Whenever an elemental, zombie, demon, or chosen is played from the top of the villain deck...
+                Func<Card, bool> playCriteria = c => c.Owner == TurnTaker && c.GetKeywords().Intersect(MinionKeywords, StringComparer.Ordinal).Any();
+                AddTrigger<PlayCardAction>(pca => !pca.IsPutIntoPlay && pca.Origin == TurnTaker.Deck && !pca.FromBottom && playCriteria(pca.CardToPlay),
+                                            pca => SummonMinionsReponse(pca.CardToPlay), TriggerType.PutIntoPlay, TriggerTiming.After);
 
+                //At the end of the villain turn, {Celadroch} deals the {H - 1} hero targets with the highest HP X projectile damage each, where X is the number of tokens in the storm pool minus 2.
                 AddDealDamageAtEndOfTurnTrigger(TurnTaker, CharacterCard, c => c.IsHero && c.IsTarget,
                     targetType: TargetType.HighestHP,
                     amount: StormPool.CurrentValue >= 2 ? StormPool.CurrentValue - 2 : 0, //make sure we aren't dealing negative damage
@@ -94,8 +100,10 @@ namespace Cauldron.Celadroch
                     highestLowestRanking: 1,
                     numberOfTargets: H - 1);
 
+                //If Forsaken Crusader is in the villain trash, put it into play.
                 AddEndOfTurnTrigger(tt => tt == TurnTaker, pca => DoNothing(), TriggerType.PlayCard);
 
+                //If a villain card was not played this turn, destroy {H} hero ongoing and/or equipment cards.
                 AddEndOfTurnTrigger(tt => tt == TurnTaker, pca => DoNothing(), TriggerType.DestroyCard);
 
                 base.AddDefeatedIfDestroyedTriggers();
@@ -124,7 +132,6 @@ namespace Cauldron.Celadroch
                 base.GameController.ExhaustCoroutine(coroutine);
             }
         }
-
 
         public override bool CanBeDestroyed => base.CharacterCard.IsFlipped;
 
@@ -173,5 +180,68 @@ namespace Cauldron.Celadroch
             yield break;
         }
 
+        private IEnumerator StartOfTurnOptionalCardPlayResponse(PhaseChangeAction action)
+        {
+            List<YesNoCardDecision> result = new List<YesNoCardDecision>();
+            var coroutine = GameController.MakeYesNoCardDecision(DecisionMaker, SelectionType.RemoveTokens, CharacterCard, storedResults: result, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+
+            if (DidPlayerAnswerYes(result))
+            {
+                coroutine = GameController.RemoveTokensFromPool(StormPool, 2, cardSource: GetCardSource());
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+
+                coroutine = PlayTheTopCardOfTheVillainDeckWithMessageResponse(action);
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+            }
+        }
+
+        private IEnumerator SummonMinionsReponse(Card card)
+        {
+            var keyword = card.GetKeywords().Intersect(MinionKeywords, StringComparer.Ordinal).First();
+            string msg = $"{TurnTaker.Name} summons forth his {keyword} minions.";
+
+            var coroutine = GameController.SendMessageAction(msg, Priority.High, GetCardSource(), new[] { card }, true);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+
+            var cards = FindCardsWhere(c => (c.Location == TurnTaker.Deck || c.Location == TurnTaker.Trash) && c != card && c.DoKeywordsContain(keyword));
+            coroutine = GameController.MoveCards(TurnTakerController, cards, TurnTaker.PlayArea, isPutIntoPlay: true, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+        }
     }
 }
