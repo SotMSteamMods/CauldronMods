b669a0be8ddd5644db2f2f3a221462c517ff29c8
Jake <mgirman520@gmail.com>
2020-12-05 21:26:13 -0500

    Initialize

---
+++ Controller/Heroes/Cricket/Cards/AcousticDistortionCardController.cs
@@ -0,0 +1,14 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+
+namespace Cauldron
+{
+    public class AcousticDistortionCardController : CardController
+    {
+        public AcousticDistortionCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+    }
+}
---
+++ Controller/Heroes/Cricket/CharacterCards/CricketCharacterCardController.cs
@@ -0,0 +1,14 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+
+namespace Cauldron
+{
+    public class CricketCharacterCardController : HeroCharacterCardController
+    {
+        public CricketCharacterCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+    }
+}
