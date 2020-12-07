5a499344a322cba1545dc70239a4338ac73977a1
Jake <mgirman520@gmail.com>
2020-12-06 21:24:45 -0500

    Update FirstResponseCricketCharacterCardController.cs

--- Controller/Heroes/Cricket/CharacterCards/FirstResponseCricketCharacterCardController.cs
+++ Controller/Heroes/Cricket/CharacterCards/FirstResponseCricketCharacterCardController.cs
@@ -21,8 +21,8 @@ namespace Cauldron.Cricket
             {
                 case 0:
                     {
-                        //One player may draw a card now.
-                        coroutine = base.GameController.SelectHeroToDrawCard(base.HeroTurnTakerController, cardSource: base.GetCardSource());
+                        //One player may play a card now.
+                        coroutine = base.GameController.SelectHeroToPlayCard(base.HeroTurnTakerController, cardSource: base.GetCardSource());
                         if (base.UseUnityCoroutines)
                         {
                             yield return base.GameController.StartCoroutine(coroutine);
@@ -35,9 +35,26 @@ namespace Cauldron.Cricket
                     }
                 case 1:
                     {
+                        //Shuffle the environment trash into the environment deck.
                         List<SelectLocationDecision> storedLocation = new List<SelectLocationDecision>();
-                        //Look at the top card of a deck and replace or discard it.
-                        coroutine = base.GameController.SelectADeck(base.HeroTurnTakerController, SelectionType.RevealTopCardOfDeck, null, storedLocation, cardSource: base.GetCardSource());
+                        Location envDeck = base.Game.EnvironmentTurnTakers.FirstOrDefault().Deck;
+                        if (base.Game.EnvironmentTurnTakers.Count() > 1)
+                        {
+                            coroutine = base.GameController.SelectADeck(base.HeroTurnTakerController, SelectionType.ShuffleTrashIntoDeck, null, storedLocation, cardSource: base.GetCardSource());
+                            if (base.UseUnityCoroutines)
+                            {
+                                yield return base.GameController.StartCoroutine(coroutine);
+                            }
+                            else
+                            {
+                                base.GameController.ExhaustCoroutine(coroutine);
+                            }
+                            if (DidSelectLocation(storedLocation))
+                            {
+                                envDeck = storedLocation.FirstOrDefault().SelectedLocation.Location;
+                            }
+                        }
+                        coroutine = base.GameController.ShuffleTrashIntoDeck(base.TurnTakerController, overrideDeck: envDeck);
                         if (base.UseUnityCoroutines)
                         {
                             yield return base.GameController.StartCoroutine(coroutine);
@@ -46,9 +63,24 @@ namespace Cauldron.Cricket
                         {
                             base.GameController.ExhaustCoroutine(coroutine);
                         }
-                        if (DidSelectLocation(storedLocation))
+                        break;
+                    }
+                case 2:
+                    {
+                        //1 hero target regains 2 HP.
+                        List<SelectTargetDecision> selectedTarget = new List<SelectTargetDecision>();
+                        coroutine = base.GameController.SelectTargetAndStoreResults(base.HeroTurnTakerController, base.FindCardsWhere(new LinqCardCriteria((Card c) => c.IsHero && c.IsTarget && c.IsInPlayAndHasGameText)), selectedTarget, cardSource: base.GetCardSource());
+                        if (base.UseUnityCoroutines)
                         {
-                            coroutine = base.RevealCard_DiscardItOrPutItOnDeck(base.HeroTurnTakerController, base.TurnTakerController, storedLocation.FirstOrDefault().SelectedLocation.Location, false);
+                            yield return base.GameController.StartCoroutine(coroutine);
+                        }
+                        else
+                        {
+                            base.GameController.ExhaustCoroutine(coroutine);
+                        }
+                        if (selectedTarget.Any())
+                        {
+                            coroutine = base.GameController.GainHP(selectedTarget.FirstOrDefault().SelectedCard, 2, cardSource: base.GetCardSource());
                             if (base.UseUnityCoroutines)
                             {
                                 yield return base.GameController.StartCoroutine(coroutine);
@@ -60,31 +92,18 @@ namespace Cauldron.Cricket
                         }
                         break;
                     }
-                case 2:
-                    {
-                        //Each target deals themselves 1 sonic damage.
-                        coroutine = base.GameController.DealDamageToSelf(base.HeroTurnTakerController, null, 1, DamageType.Sonic, cardSource: base.GetCardSource());
-                        if (base.UseUnityCoroutines)
-                        {
-                            yield return base.GameController.StartCoroutine(coroutine);
-                        }
-                        else
-                        {
-                            base.GameController.ExhaustCoroutine(coroutine);
-                        }
-                        break;
-                    }
             }
             yield break;
         }
 
         public override IEnumerator UsePower(int index = 0)
         {
-            //Select a target. Reduce damage dealt by that target by 1 until the start of your next turn.
-            List<SelectTargetDecision> storedResults = new List<SelectTargetDecision>();
-            //Select a target. 
-            IEnumerator coroutine = base.GameController.SelectTargetAndStoreResults(base.HeroTurnTakerController, base.FindCardsWhere(new LinqCardCriteria((Card c) => c.IsTarget && c.IsInPlayAndHasGameText)), storedResults, cardSource
-                : base.GetCardSource());
+            //{Cricket} deals 2 targets 1 sonic damage each. You may use a power now.
+            int targetNumeral = GetPowerNumeral(0, 2);
+            int damageNumeral = GetPowerNumeral(1, 1);
+
+            //{Cricket} deals 2 targets 1 sonic damage each.
+            IEnumerator coroutine = base.GameController.SelectTargetsAndDealDamage(base.HeroTurnTakerController, new DamageSource(base.GameController, base.CharacterCard), damageNumeral, DamageType.Sonic, targetNumeral, false, targetNumeral, cardSource: base.GetCardSource());
             if (base.UseUnityCoroutines)
             {
                 yield return base.GameController.StartCoroutine(coroutine);
@@ -94,22 +113,15 @@ namespace Cauldron.Cricket
                 base.GameController.ExhaustCoroutine(coroutine);
             }
 
-            if (storedResults != null && storedResults.FirstOrDefault().SelectedCard != null)
+            //You may use a power now.
+            coroutine = base.GameController.SelectAndUsePower(base.HeroTurnTakerController, cardSource: base.GetCardSource());
+            if (base.UseUnityCoroutines)
             {
-                //Reduce damage dealt by that target by 1 until the start of your next turn.
-                ReduceDamageStatusEffect statusEffect = new ReduceDamageStatusEffect(1);
-                statusEffect.SourceCriteria.IsSpecificCard = storedResults.FirstOrDefault().SelectedCard;
-                statusEffect.UntilStartOfNextTurn(base.TurnTaker);
-                statusEffect.UntilTargetLeavesPlay(storedResults.FirstOrDefault().SelectedCard);
-                coroutine = base.AddStatusEffect(statusEffect);
-                if (base.UseUnityCoroutines)
-                {
-                    yield return base.GameController.StartCoroutine(coroutine);
-                }
-                else
-                {
-                    base.GameController.ExhaustCoroutine(coroutine);
-                }
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
             }
             yield break;
         }
