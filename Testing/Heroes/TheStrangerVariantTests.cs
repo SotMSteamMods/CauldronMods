268e88f0c5b3e3d486b9b403a7ec31c371647087
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2020-12-08 05:52:18 -0600

    Added RunecarvedStranger Controller + Tests

--- Testing/Heroes/TheStrangerVariantTests.cs
+++ Testing/Heroes/TheStrangerVariantTests.cs
@@ -78,5 +78,104 @@ namespace CauldronTests
             Assert.AreEqual(27, stranger.CharacterCard.HitPoints);
         }
 
+        [Test()]
+        public void TestRunecarvedStrangerInnatePower()
+        {
+            SetupGameController("BaronBlade", "Cauldron.TheStranger/RunecarvedTheStrangerCharacter", "Haka", "Ra", "Megalopolis");
+            StartGame();
+
+            SetHitPoints(ra, 10);
+            
+            GoToUsePowerPhase(stranger);
+            DecisionSelectCard = ra.CharacterCard;
+            //1 hero target regains 2 HP.
+            QuickHPStorage(ra);
+            UsePower(stranger.CharacterCard);
+            QuickHPCheck(2);
+        }
+
+        [Test()]
+        public void TestRunecarvedStrangerIncap1()
+        {
+            SetupGameController("BaronBlade", "Cauldron.TheStranger/RunecarvedTheStrangerCharacter", "Haka", "Ra", "Megalopolis");
+            StartGame();
+
+            SetupIncap(baron);
+            AssertIncapacitated(stranger);
+
+            Card mere = PutInHand("Mere");
+
+
+            //One player may play a card now.
+            GoToUseIncapacitatedAbilityPhase(stranger);
+            DecisionSelectTarget = haka.CharacterCard;
+            DecisionSelectCard = mere;
+            QuickHandStorage(haka);
+            UseIncapacitatedAbility(stranger, 0);
+            //should have one fewer card in haka's hand
+            QuickHandCheck(-1);
+            AssertInPlayArea(haka, mere);
+
+        }
+
+        [Test()]
+        public void TestRunecarvedStrangerIncap2()
+        {
+            SetupGameController("BaronBlade", "Cauldron.TheStranger/RunecarvedTheStrangerCharacter", "Haka", "Ra", "Megalopolis");
+            StartGame();
+
+            SetupIncap(baron);
+            AssertIncapacitated(stranger);
+
+            Card dominion = PlayCard("Dominion");
+            Card enduring = PlayCard("EnduringIntercession");
+            Card backlash = PlayCard("BacklashField");
+            Card field = PlayCard("LivingForceField");
+            Card police = PlayCard("PoliceBackup");
+            Card pileup = PlayCard("TrafficPileup");
+
+            //Destroy 1 hero ongoing, 1 non-hero ongoing, and 1 environment card.
+            GoToUseIncapacitatedAbilityPhase(stranger);
+            DecisionSelectCards = new Card[] { enduring, field, pileup };
+            UseIncapacitatedAbility(stranger, 1);
+            AssertIsInPlay(dominion, backlash, police);
+            AssertInTrash(enduring, field, pileup);
+
+        }
+
+        [Test()]
+        public void TestRunecarvedStrangerIncap3()
+        {
+            SetupGameController("BaronBlade", "Cauldron.TheStranger/RunecarvedTheStrangerCharacter", "Haka", "Ra", "Megalopolis");
+            StartGame();
+
+            SetHitPoints(baron, 20);
+            SetHitPoints(haka, 20);
+
+            SetupIncap(baron);
+            AssertIncapacitated(stranger);
+
+            //Villain targets may not regain HP until the start of your turn.
+            GoToUseIncapacitatedAbilityPhase(stranger);
+            UseIncapacitatedAbility(stranger, 2);
+
+            //should prevent villain
+            QuickHPStorage(baron);
+            PlayCard("FleshRepairNanites");
+            QuickHPCheckZero();
+
+            //should not prevent hero
+            QuickHPStorage(haka);
+            PlayCard("VitalitySurge");
+            QuickHPCheck(2);
+
+            //should expire at start of next turn
+            GoToStartOfTurn(stranger);
+            QuickHPStorage(baron);
+            PlayCard("FleshRepairNanites");
+            QuickHPCheck(10);
+        }
+
+
     }
 }
