9906ae17494f4a4056502f63486022f40f0fc367
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2020-12-08 17:45:02 -0600

    Initialized Controller + Tests
    Added Shuffle Transformations on Flip Effect

---
+++ Controller/Villains/Anathema/CharacterCards/AcceleratedEvolutionAnathemaCharacterCardController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+
+namespace Cauldron.Anathema
+{
+	public class AcceleratedEvolutionAnathemaCharacterCardController : VillainCharacterCardController
+	{
+		public AcceleratedEvolutionAnathemaCharacterCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+		{
+			
+		}
+
+		//number of villain targets in play other than Anathema
+		private int NumberOfVillainTargetsInPlay
+		{
+			get
+			{ 
+				return base.FindCardsWhere((Card c) => base.IsVillainTarget(c) && c.IsInPlay && c != base.CharacterCard).Count();
+			}
+		}
+
+		private bool IsArm(Card card)
+		{
+			return card != null && base.GameController.DoesCardContainKeyword(card, "arm");
+		}
+
+		private bool IsHead(Card card)
+		{
+			return card != null && base.GameController.DoesCardContainKeyword(card, "head");
+		}
+
+		private bool IsBody(Card card)
+		{
+			return card != null && base.GameController.DoesCardContainKeyword(card, "body");
+		}
+
+		private bool IsArmOrHead(Card c)
+		{
+			return IsArm(c) || IsHead(c);
+		}
+
+		public override void AddSideTriggers()
+		{
+			//on his front side
+			if (!base.Card.IsFlipped)
+			{
+				//Whenever {Anathema} destroys an arm or head card, put that under {Anathema}'s villain character card.
+
+				//At the end of the villain turn, reveal the top card of the villain deck. If an arm or head card is revealed, put it under {Anathema}'s character card, otherwise discard it. 
+				//Then if there are {H} or more cards under {Anathema}, flip his villain character card.
+				
+				if (base.IsGameAdvanced)
+				{
+					//At the end of the villain turn, {Anathema} regains {H - 2} HP.
+				}
+			}
+			else
+			{
+				//Arm and head cards are indestructible during the villain turn.
+
+				//When explosive transformation enters play, flip {Anathema}'s character cards.
+
+
+				if (base.IsGameAdvanced)
+				{
+					//At the end of the villain turn {Anathema} regains 1 HP for each villain target in play.
+				}
+			}
+
+			base.AddDefeatedIfDestroyedTriggers();
+		}
+
+		public override IEnumerator AfterFlipCardImmediateResponse()
+		{
+			//Remove and Add existing Side Triggers
+			IEnumerator coroutine = base.AfterFlipCardImmediateResponse();
+			if (base.UseUnityCoroutines)
+			{
+				yield return base.GameController.StartCoroutine(coroutine);
+			}
+			else
+			{
+				base.GameController.ExhaustCoroutine(coroutine);
+			}
+
+			//When {Anathema} flips to this side, put all cards from underneath him into play. 
+
+			//Shuffle all copies of explosive transformation from the villain trash into the villain deck.
+			IEnumerable<Card> cardsToShuffle = FindCardsWhere((Card c) => c.Identifier == "ExplosiveTransformation" && base.TurnTaker.Trash.HasCard(c));
+			coroutine = GameController.ShuffleCardsIntoLocation(DecisionMaker, cardsToShuffle, base.TurnTaker.Deck, cardSource: GetCardSource());
+			if (base.UseUnityCoroutines)
+			{
+				yield return base.GameController.StartCoroutine(coroutine);
+			}
+			else
+			{
+				base.GameController.ExhaustCoroutine(coroutine);
+			}
+			yield break;
+		}
+
+
+
+	}
+}
---
+++ Testing/Villains/AnathemaVariantTests.cs
@@ -0,0 +1,192 @@
+﻿using NUnit.Framework;
+using System;
+using Handelabra.Sentinels.Engine.Model;
+using Handelabra.Sentinels.Engine.Controller;
+using System.Linq;
+using System.Collections;
+using Handelabra.Sentinels.UnitTest;
+using System.Reflection;
+using Cauldron.Anathema;
+using Handelabra;
+using System.Collections.Generic;
+
+namespace CauldronTests
+{
+    [TestFixture()]
+    public class AnathemaVariantTests : BaseTest
+    {
+        #region AnathemaHelperFunctions
+
+        protected TurnTakerController anathema { get { return FindVillain("Anathema"); } }
+
+        protected void AssertNumberOfArmsInPlay(TurnTakerController ttc, int number)
+        {
+            var cardsInPlay = ttc.TurnTaker.GetAllCards().Where(c => c.IsInPlay && this.IsArm(c));
+            var actual = cardsInPlay.Count();
+            Assert.AreEqual(number, actual, String.Format("{0} should have had {1} cards in play, but actually had {2}: {3}", ttc.Name, number, actual, cardsInPlay.Select(c => c.Title).ToCommaList()));
+        }
+
+        protected List<Card> GetListOfArmsInPlay(TurnTakerController ttc)
+        {
+            var cardsInPlay = ttc.TurnTaker.GetAllCards().Where(c => c.IsInPlay && this.IsArm(c));
+            List<Card> listOfHead = new List<Card>();
+            foreach (Card c in cardsInPlay)
+            {
+                listOfHead.Add(c);
+            }
+            return listOfHead;
+        }
+
+        protected void AssertNumberOfHeadInPlay(TurnTakerController ttc, int number)
+        {
+            var cardsInPlay = ttc.TurnTaker.GetAllCards().Where(c => c.IsInPlay && this.IsHead(c));
+            var actual = cardsInPlay.Count();
+            Assert.AreEqual(number, actual, String.Format("{0} should have had {1} cards in play, but actually had {2}: {3}", ttc.Name, number, actual, cardsInPlay.Select(c => c.Title).ToCommaList()));
+        }
+
+        protected List<Card> GetListOfHeadsInPlay(TurnTakerController ttc)
+        {
+            var cardsInPlay = ttc.TurnTaker.GetAllCards().Where(c => c.IsInPlay && this.IsHead(c));
+            List<Card> listOfHead = new List<Card>();
+            foreach(Card c in cardsInPlay)
+            {
+                listOfHead.Add(c);
+            }
+            return listOfHead;
+        }
+
+        protected void AssertNumberOfBodyInPlay(TurnTakerController ttc, int number)
+        {
+            var cardsInPlay = ttc.TurnTaker.GetAllCards().Where(c => c.IsInPlay && this.IsBody(c));
+            var actual = cardsInPlay.Count();
+            Assert.AreEqual(number, actual, String.Format("{0} should have had {1} cards in play, but actually had {2}: {3}", ttc.Name, number, actual, cardsInPlay.Select(c => c.Title).ToCommaList()));
+        }
+
+        protected List<Card> GetListOfBodyInPlay(TurnTakerController ttc)
+        {
+            var cardsInPlay = ttc.TurnTaker.GetAllCards().Where(c => c.IsInPlay && this.IsBody(c));
+            List<Card> listOfHead = new List<Card>();
+            foreach (Card c in cardsInPlay)
+            {
+                listOfHead.Add(c);
+            }
+            return listOfHead;
+        }
+
+        private bool IsArm(Card card)
+        {
+            return card != null && base.GameController.DoesCardContainKeyword(card, "arm", false, false);
+        }
+
+        private bool IsHead(Card card)
+        {
+            return card != null && base.GameController.DoesCardContainKeyword(card, "head", false, false);
+        }
+
+        private bool IsBody(Card card)
+        {
+            return card != null && base.GameController.DoesCardContainKeyword(card, "body", false, false);
+        }
+
+        private bool IsArmHeadOrBody(Card c)
+        {
+            return IsArm(c) || IsHead(c) || IsBody(c);
+        }
+
+
+        private void ResetAnathemaDeck()
+        {
+            //Destroy all arms, heads, and body in play
+            DestroyCards((Card c) => this.IsArmHeadOrBody(c));
+            //Shuffle all arms, heads, and body back into the deck
+            ShuffleTrashIntoDeck(anathema);
+        }
+        protected void AddImmuneToDamageTrigger(TurnTakerController ttc, bool heroesImmune, bool villainsImmune)
+        {
+            ImmuneToDamageStatusEffect immuneToDamageStatusEffect = new ImmuneToDamageStatusEffect();
+            immuneToDamageStatusEffect.TargetCriteria.IsHero = new bool?(heroesImmune);
+            immuneToDamageStatusEffect.TargetCriteria.IsVillain = new bool?(villainsImmune);
+            immuneToDamageStatusEffect.UntilStartOfNextTurn(ttc.TurnTaker);
+            this.RunCoroutine(this.GameController.AddStatusEffect(immuneToDamageStatusEffect, true, new CardSource(ttc.CharacterCardController)));
+        }
+
+        protected void AssertDamageTypeChanged(HeroTurnTakerController httc, Card source, Card target, int amount, DamageType initialDamageType, DamageType expectedDamageType)
+        {
+            List<DealDamageAction> storedResults = new List<DealDamageAction>();
+            this.RunCoroutine(this.GameController.DealDamage(httc, source, (Card c) => c == target, amount, initialDamageType, false, false, storedResults, null, null, false, null, null, false, false, new CardSource(GetCardController(source))));
+            
+            if(storedResults != null)
+            {
+                DealDamageAction dd = storedResults.FirstOrDefault<DealDamageAction>();
+                DamageType actualDamageType = dd.DamageType;
+                Assert.AreEqual(expectedDamageType, actualDamageType, $"Expected damage type: {expectedDamageType}. Actual damage type: {actualDamageType}");
+            }
+            else
+            {
+                Assert.Fail("storedResults was null");
+            }
+
+        }
+        #endregion
+
+        [Test()]
+        public void TestAcceleratedEvolutionAnathemaLoadedProperly()
+        {
+            SetupGameController("Cauldron.Anathema/AcceleratedEvolutionAnathemaCharacter", "Legacy", "Megalopolis");
+
+            Assert.AreEqual(3, this.GameController.TurnTakerControllers.Count());
+
+            Assert.IsNotNull(anathema);
+            Assert.IsInstanceOf(typeof(AcceleratedEvolutionAnathemaCharacterCardController), anathema.CharacterCardController);
+
+            Assert.AreEqual(70, anathema.CharacterCard.HitPoints);
+        }
+
+        [Test()]
+        public void TestAcceleratedEvolutionAnathemaStartGame()
+        {
+            SetupGameController("Cauldron.Anathema/AcceleratedEvolutionAnathemaCharacter", "Legacy", "Megalopolis");
+
+            StartGame();
+
+            //check that there are 5 cards in play total (anathema, 2 arms, 1 head, and 1 body)
+            AssertNumberOfCardsInPlay(anathema, 5);
+
+            //check that there are 2 arms in play
+            AssertNumberOfArmsInPlay(anathema, 2);
+
+            //check that there is 1 body in play
+            AssertNumberOfBodyInPlay(anathema, 1);
+
+            //check that there is 1 head in play
+            AssertNumberOfHeadInPlay(anathema, 1);
+            
+        }
+
+        [Test()]
+        public void TestAcceleratedEvolutionAnathemaOnFlip_ShuffleExplosiveTransformations()
+        {
+            SetupGameController("Cauldron.Anathema/AcceleratedEvolutionAnathemaCharacter", "Legacy", "Megalopolis");
+            StartGame();
+
+            IEnumerable<Card> transformations = FindCardsWhere((Card c) => anathema.TurnTaker.Deck.HasCard(c) && c.Identifier == "ExplosiveTransformation");
+            PutInTrash(transformations);
+            AssertInTrash(transformations);
+
+            //Shuffle all copies of explosive transformation from the villain trash into the villain deck.
+            QuickShuffleStorage(anathema);
+            AssertNotFlipped(anathema);
+            FlipCard(anathema);
+            AssertFlipped(anathema);
+            AssertInDeck(transformations);
+            QuickShuffleCheck(1);
+
+
+
+        }
+
+
+
+
+    }
+}
