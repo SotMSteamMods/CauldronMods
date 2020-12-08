41c5629e8fafbf632916f5e0d97c0fc5a3711d44
Jake <mgirman520@gmail.com>
2020-12-08 15:28:32 -0500

    Initialize Hivemind
    Update TurnTakerController
    Create CardController
    Update Decklist

--- Controller/Villains/SwarmEater/CharacterCards/SwarmEaterTurnTakerController.cs
+++ Controller/Villains/SwarmEater/CharacterCards/SwarmEaterTurnTakerController.cs
@@ -16,37 +16,69 @@ namespace Cauldron.SwarmEater
 
         public override IEnumerator StartGame()
         {
-            //Reveal cards from the top of the villain deck until {H - 1} targets, 
-            int targetCount = base.H - 1;
-            //2 traits, 
-            int traitCount = 2;
-            //and Single-Minded Pursuit are revealed. 
-            int pursuitCount = 1;
-            IEnumerator coroutine = null;
-            while (targetCount + traitCount + pursuitCount != 0)
+            if (base.CharacterCardController is SwarmEaterCharacterCardController)
             {
-                //{H - 1} targets,
-                Func<Card, bool> targetCriteria = (Card c) => false;
-                if (targetCount != 0)
-                {
-                    targetCriteria = (Card c) => c.IsTarget;
-                }
+                //Reveal cards from the top of the villain deck until {H - 1} targets, 
+                int targetCount = base.H - 1;
                 //2 traits, 
-                Func<Card, bool> traitCriteria = (Card c) => false;
-                if (traitCount != 0)
-                {
-                    traitCriteria = (Card c) => c.DoKeywordsContain("trait");
-                }
+                int traitCount = 2;
                 //and Single-Minded Pursuit are revealed. 
-                Func<Card, bool> pursuitCriteria = (Card c) => false;
-                if (pursuitCount != 0)
+                int pursuitCount = 1;
+                IEnumerator coroutine = null;
+                while (targetCount + traitCount + pursuitCount != 0)
                 {
-                    pursuitCriteria = (Card c) => c.Identifier == "SingleMindedPursuit";
+                    //{H - 1} targets,
+                    Func<Card, bool> targetCriteria = (Card c) => false;
+                    if (targetCount != 0)
+                    {
+                        targetCriteria = (Card c) => c.IsTarget;
+                    }
+                    //2 traits, 
+                    Func<Card, bool> traitCriteria = (Card c) => false;
+                    if (traitCount != 0)
+                    {
+                        traitCriteria = (Card c) => c.DoKeywordsContain("trait");
+                    }
+                    //and Single-Minded Pursuit are revealed. 
+                    Func<Card, bool> pursuitCriteria = (Card c) => false;
+                    if (pursuitCount != 0)
+                    {
+                        pursuitCriteria = (Card c) => c.Identifier == "SingleMindedPursuit";
+                    }
+                    Func<Card, bool> revealCriteria = (Card c) => c != null && (targetCriteria(c) || traitCriteria(c) || pursuitCriteria(c));
+                    List<Card> playedCard = new List<Card>();
+                    //Put them into play, and shuffle the rest of the revealed cards back into the villain deck.
+                    coroutine = base.CharacterCardController.RevealCards_MoveMatching_ReturnNonMatchingCards(this, base.TurnTaker.Deck, false, true, false, new LinqCardCriteria(revealCriteria), 1, storedPlayResults: playedCard, shuffleSourceAfterwards: false);
+                    if (base.UseUnityCoroutines)
+                    {
+                        yield return base.GameController.StartCoroutine(coroutine);
+                    }
+                    else
+                    {
+                        base.GameController.ExhaustCoroutine(coroutine);
+                    }
+
+                    if (!playedCard.Any())
+                    {
+                        continue;
+                    }
+
+                    Card card = playedCard[0];
+                    if (card.IsTarget)
+                    {
+                        targetCount--;
+                    }
+                    else if (card.DoKeywordsContain("trait"))
+                    {
+                        traitCount--;
+                    }
+                    else if (card.Identifier == "SingleMindedPursuit")
+                    {
+                        pursuitCount--;
+                    }
                 }
-                Func<Card, bool> revealCriteria = (Card c) => c != null && (targetCriteria(c) || traitCriteria(c) || pursuitCriteria(c));
-                List<Card> playedCard = new List<Card>();
-                //Put them into play, and shuffle the rest of the revealed cards back into the villain deck.
-                coroutine = base.CharacterCardController.RevealCards_MoveMatching_ReturnNonMatchingCards(this, base.TurnTaker.Deck, false, true, false, new LinqCardCriteria(revealCriteria), 1, storedPlayResults: playedCard, shuffleSourceAfterwards: false);
+                //Shuffle the villain deck.
+                coroutine = base.GameController.ShuffleLocation(base.TurnTaker.Deck);
                 if (base.UseUnityCoroutines)
                 {
                     yield return base.GameController.StartCoroutine(coroutine);
@@ -55,34 +87,29 @@ namespace Cauldron.SwarmEater
                 {
                     base.GameController.ExhaustCoroutine(coroutine);
                 }
-
-                if (!playedCard.Any())
-                {
-                    continue;
-                }
-
-                Card card = playedCard[0];
-                if (card.IsTarget)
-                {
-                    targetCount--;
-                }
-                else if (card.DoKeywordsContain("trait"))
-                {
-                    traitCount--;
-                }
-                else if (card.Identifier == "SingleMindedPursuit")
-                {
-                    pursuitCount--;
-                }
             }
-            coroutine = base.GameController.ShuffleLocation(base.TurnTaker.Deck);
-            if (base.UseUnityCoroutines)
+            else if (base.CharacterCardController is DistributedHivemindSwarmEaterCharacter)
             {
-                yield return base.GameController.StartCoroutine(coroutine);
-            }
-            else
-            {
-                base.GameController.ExhaustCoroutine(coroutine);
+                //Search the villain deck for Converted Biomass and Absorbed Nanites and remove them from the game.
+                IEnumerator coroutine = base.RemoveCardsFromGame((Card c) => base.FindCardController(c) is AbsorbedNanitesCardController || base.FindCardController(c) is ConvertedBiomassCardController);
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+                //Shuffle the villain deck.
+                coroutine = base.GameController.ShuffleLocation(base.TurnTaker.Deck);
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
             }
         }
     }
