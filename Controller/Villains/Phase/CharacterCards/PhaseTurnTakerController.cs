6e58f8d328fe6541d3cf0c872fd6250457af047e
Jake <mgirman520@gmail.com>
2020-12-09 11:37:37 -0500

    Create PhaseTurnTakerController.cs

---
+++ Controller/Villains/Phase/CharacterCards/PhaseTurnTakerController.cs
@@ -0,0 +1,41 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+
+namespace Cauldron.Phase
+{
+    public class PhaseTurnTakerController : TurnTakerController
+    {
+        public PhaseTurnTakerController(TurnTaker turnTaker, GameController gameController) : base(turnTaker, gameController)
+        {
+
+        }
+
+        public override IEnumerator StartGame()
+        {
+            //Search the villain deck for the card Reinforced Wall and put it into play.
+            IEnumerator coroutine = base.GameController.PlayCard(this, base.GameController.FindCard("ReinforcedWall"));
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+
+            //Shuffle the villain deck.
+            coroutine = base.GameController.ShuffleLocation(base.TurnTaker.Deck);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+    }
+}
