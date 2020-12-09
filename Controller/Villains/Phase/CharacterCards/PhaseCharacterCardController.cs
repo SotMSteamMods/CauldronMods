5c7dad15469fbbfbcad3fccc7d631a287f0af902
Jake <mgirman520@gmail.com>
2020-12-08 23:18:40 -0500

    Create Almost Got Her

---
+++ Controller/Villains/Phase/CharacterCards/PhaseCharacterCardController.cs
@@ -0,0 +1,14 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+
+namespace Cauldron.Phase
+{
+    public class PhaseCharacterCardController : VillainCharacterCardController
+    {
+        public PhaseCharacterCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+    }
+}
