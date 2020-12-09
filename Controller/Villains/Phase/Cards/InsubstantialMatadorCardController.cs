ebbbfea21aa2a9548fd809afe0fad279d5038a74
Jake <mgirman520@gmail.com>
2020-12-09 10:56:03 -0500

    Namespace

--- Controller/Villains/Phase/Cards/InsubstantialMatadorCardController.cs
+++ Controller/Villains/Phase/Cards/InsubstantialMatadorCardController.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections;
 using System.Linq;
 
-namespace Cauldron
+namespace Cauldron.Phase
 {
     public class InsubstantialMatadorCardController : CardController
     {
