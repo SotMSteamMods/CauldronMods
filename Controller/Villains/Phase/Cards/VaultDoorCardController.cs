b8bbc970b686e9c984b9e36cb1be78a04fd3e688
Jake <mgirman520@gmail.com>
2020-12-09 11:34:15 -0500

    Create VaultDoorCardController.cs

---
+++ Controller/Villains/Phase/Cards/VaultDoorCardController.cs
@@ -0,0 +1,22 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+
+namespace Cauldron.Phase
+{
+    public class VaultDoorCardController : CardController
+    {
+        public VaultDoorCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+
+        public override void AddTriggers()
+        {
+            //{Phase} is immune to damage.
+            base.AddImmuneToDamageTrigger((DealDamageAction action) => action.Target == base.CharacterCard);
+            //When this card would be dealt 2 or less damage, prevent that damage.
+            base.AddPreventDamageTrigger((DealDamageAction action) => action.Target == base.Card && action.Amount <= 2);
+        }
+    }
+}
