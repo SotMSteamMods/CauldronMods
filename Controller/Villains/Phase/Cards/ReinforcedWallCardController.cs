1b7cefb238a36724a4a1bc26c3825efae6184a1f
Jake <mgirman520@gmail.com>
2020-12-09 11:02:02 -0500

    Namespaces

---
+++ Controller/Villains/Phase/Cards/ReinforcedWallCardController.cs
@@ -0,0 +1,14 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+
+namespace Cauldron.Phase
+{
+    public class ReinforcedWallCardController : CardController
+    {
+        public ReinforcedWallCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+    }
+}
