d9e34b080423607adfd3034898a45b439fa84190
Vilnor <onlytelford@yahoo.com>
2020-12-09 22:03:10 -0500

    Shadow Stalker Converted StartOfTurnTrigger => DealDamage to AddDealDamageAtStartOfTurnTrigger Obliveon proofing moved destroy card criteria to separate method for clarity linq card criteria text added damage preview info to FindTargetWithHighestHitPoints

--- Controller/Environments/BlackwoodForest/Cards/ShadowStalkerCardController.cs
+++ Controller/Environments/BlackwoodForest/Cards/ShadowStalkerCardController.cs
@@ -22,7 +22,6 @@ namespace Cauldron.BlackwoodForest
 
         private const int PsychicDamageToDeal = 5;
 
-
         public ShadowStalkerCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
             base.SpecialStringMaker.ShowHighestHP();
@@ -31,30 +30,29 @@ namespace Cauldron.BlackwoodForest
         public override void AddTriggers()
         {
             // At the end of the environment turn, destroy 1 ongoing card.
-            base.AddEndOfTurnTrigger(tt => tt == base.TurnTaker, EndOfTurnDestroyOngoingResponse,
-                TriggerType.DestroyCard);
+            AddEndOfTurnTrigger(tt => tt == TurnTaker, EndOfTurnDestroyOngoingResponse, TriggerType.DestroyCard);
 
             // At the start of the environment turn this card deals each hero target {H - 2} melee damage.
-            base.AddStartOfTurnTrigger(tt => tt == base.TurnTaker, 
-                StartOfTurnDealDamageResponse, TriggerType.DealDamage);
+            AddDealDamageAtStartOfTurnTrigger(TurnTaker, Card, c => c.IsHero && c.IsTarget && GameController.IsCardVisibleToCardSource(c, GetCardSource()),
+                TargetType.All, H - 2, DamageType.Melee);
 
             // Whenever an environment card is destroyed, deal the target with the highest HP 5 psychic damage
-            base.AddTrigger<DestroyCardAction>(dca => dca.CardToDestroy.Card.IsEnvironment,
-                this.DestroyEnvironmentResponse,
-                new[]
-                {
-                    TriggerType.DestroyCard
-                }, TriggerTiming.After, null, false, true, true);
+            AddTrigger<DestroyCardAction>(EnvironmentCardDestroyedCriteria, DestroyEnvironmentResponse, TriggerType.DestroyCard, TriggerTiming.After);
 
             base.AddTriggers();
+        }
+
+        private bool EnvironmentCardDestroyedCriteria(DestroyCardAction dca)
+        {
+            return dca.WasCardDestroyed && dca.CardToDestroy.Card.IsEnvironment && GameController.IsCardVisibleToCardSource(dca.CardToDestroy.Card, GetCardSource());
         }
 
         private IEnumerator EndOfTurnDestroyOngoingResponse(PhaseChangeAction pca)
         {
             // Destroy 1 ongoing card
             IEnumerator destroyCardRoutine = base.GameController.SelectAndDestroyCard(this.DecisionMaker,
-                new LinqCardCriteria(card => card.IsOngoing), 
-                false, cardSource: this.GetCardSource());
+                new LinqCardCriteria(card => card.IsOngoing, "ongoing"),
+                false, cardSource: GetCardSource());
 
             if (base.UseUnityCoroutines)
             {
@@ -66,57 +64,37 @@ namespace Cauldron.BlackwoodForest
             }
         }
 
-        private IEnumerator StartOfTurnDealDamageResponse(PhaseChangeAction pca)
-        {
-            // Deal each hero target {H - 2} melee damage
-            int damageToDeal = Game.H - 2;
-
-            IEnumerator dealDamageRoutine
-                = this.DealDamage(this.Card,
-                    card => !card.Equals(this.Card) && card.IsHero && !card.IsIncapacitatedOrOutOfGame,
-                    damageToDeal, DamageType.Melee);
-
-            if (base.UseUnityCoroutines)
-            {
-                yield return base.GameController.StartCoroutine(dealDamageRoutine);
-            }
-            else
-            {
-                base.GameController.ExhaustCoroutine(dealDamageRoutine);
-            }
-        }
-
         private IEnumerator DestroyEnvironmentResponse(DestroyCardAction dca)
         {
             List<Card> highestHpTarget = new List<Card>();
-            IEnumerator highestHpRoutine 
-                = base.GameController.FindTargetWithHighestHitPoints(1, card => card.IsTarget, highestHpTarget);
-
+            var damageInfo = new DealDamageAction(GetCardSource(), new DamageSource(GameController, Card), null, PsychicDamageToDeal, DamageType.Psychic);
+            var coroutine = GameController.FindTargetWithHighestHitPoints(1, card => card.IsTarget, highestHpTarget,
+                                dealDamageInfo: new[] { damageInfo },
+                                evenIfCannotDealDamage: true,
+                                cardSource: GetCardSource());
             if (base.UseUnityCoroutines)
             {
-                yield return base.GameController.StartCoroutine(highestHpRoutine);
+                yield return base.GameController.StartCoroutine(coroutine);
             }
             else
             {
-                base.GameController.ExhaustCoroutine(highestHpRoutine);
+                base.GameController.ExhaustCoroutine(coroutine);
             }
 
-            if (!highestHpTarget.Any())
+            if (highestHpTarget.Any())
             {
-                yield break;
-            }
+                var target = highestHpTarget.First();
 
-            // Deal 5 psychic damage
-            IEnumerator dealDamageRoutine = this.DealDamage(this.Card, highestHpTarget.First(), 
-                PsychicDamageToDeal, DamageType.Psychic, cardSource: this.GetCardSource());
-
-            if (base.UseUnityCoroutines)
-            {
-                yield return base.GameController.StartCoroutine(dealDamageRoutine);
-            }
-            else
-            {
-                base.GameController.ExhaustCoroutine(dealDamageRoutine);
+                // Deal 5 psychic damage
+                coroutine = this.DealDamage(this.Card, target, PsychicDamageToDeal, DamageType.Psychic, cardSource: this.GetCardSource());
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
             }
         }
     }
