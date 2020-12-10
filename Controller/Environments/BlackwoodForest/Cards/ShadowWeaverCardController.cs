f009e432d00acf865b85cbd8fbd671cb34cf50eb
Vilnor <onlytelford@yahoo.com>
2020-12-09 22:05:36 -0500

    Shadow Weaver Corrected damage dealt and special string to HeroCharacter instead of HeroTarget Converted EndOfTurnTrigger to DealDamageAtEndOfTurnTrigger Obliveon Proofing

--- Controller/Environments/BlackwoodForest/Cards/ShadowWeaverCardController.cs
+++ Controller/Environments/BlackwoodForest/Cards/ShadowWeaverCardController.cs
@@ -23,14 +23,16 @@ namespace Cauldron.BlackwoodForest
 
         public ShadowWeaverCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
-            base.SpecialStringMaker.ShowHeroTargetWithLowestHP();
+            base.SpecialStringMaker.ShowHeroCharacterCardWithLowestHP();
         }
 
         public override void AddTriggers()
         {
             // At the end of the environment turn, this card deals the hero with the lowest HP {H - 2} toxic damage.
-            base.AddEndOfTurnTrigger(tt => tt == base.TurnTaker, EndOfTurnDealDamageResponse,
-                TriggerType.DealDamage);
+            AddDealDamageAtEndOfTurnTrigger(TurnTaker, Card, c => c.IsHeroCharacterCard && GameController.IsCardVisibleToCardSource(c, GetCardSource()),
+                    TargetType.LowestHP, H - 2, DamageType.Psychic,
+                    highestLowestRanking: 1,
+                    numberOfTargets: 1);
 
             // When this card is destroyed, it deals each target 1 psychic damage.
             base.AddWhenDestroyedTrigger(DestroyCardResponse, TriggerType.DealDamage);
@@ -38,55 +40,16 @@ namespace Cauldron.BlackwoodForest
             base.AddTriggers();
         }
 
-        private IEnumerator EndOfTurnDealDamageResponse(PhaseChangeAction pca)
-        {
-            List<Card> storedResults = new List<Card>();
-            IEnumerator findTargetWithLowestHpRoutine = base.GameController.FindTargetsWithLowestHitPoints(1, 1,
-                c => c.IsHero && !c.IsIncapacitatedOrOutOfGame, storedResults);
-
-            if (base.UseUnityCoroutines)
-            {
-                yield return base.GameController.StartCoroutine(findTargetWithLowestHpRoutine);
-            }
-            else
-            {
-                base.GameController.ExhaustCoroutine(findTargetWithLowestHpRoutine);
-            }
-
-            if (!storedResults.Any())
-            {
-                yield break;
-            }
-
-            // At the end of the environment turn, this card deals the hero with the lowest HP {H - 2} toxic damage.
-            int damageToDeal = Game.H - 2;
-
-            IEnumerator dealDamageRoutine = this.DealDamage(this.Card, storedResults.First(), 
-                damageToDeal, DamageType.Toxic);
-
-            if (base.UseUnityCoroutines)
-            {
-                yield return base.GameController.StartCoroutine(dealDamageRoutine);
-            }
-            else
-            {
-                base.GameController.ExhaustCoroutine(dealDamageRoutine);
-            }
-        }
-
         private IEnumerator DestroyCardResponse(DestroyCardAction dca)
         {
-            IEnumerator dealDamageRoutine
-                = this.DealDamage(this.Card,
-                    card => !card.Equals(this.Card) && card.IsTarget, PsychicDamageToDeal, DamageType.Psychic);
-
+            var coroutine = this.DealDamage(this.Card, card => card.IsTarget, PsychicDamageToDeal, DamageType.Psychic);
             if (base.UseUnityCoroutines)
             {
-                yield return base.GameController.StartCoroutine(dealDamageRoutine);
+                yield return base.GameController.StartCoroutine(coroutine);
             }
             else
             {
-                base.GameController.ExhaustCoroutine(dealDamageRoutine);
+                base.GameController.ExhaustCoroutine(coroutine);
             }
         }
     }
