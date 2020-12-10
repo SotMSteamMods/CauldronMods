b2f0e84eebd6b56b144e8e4ddcd47eb0e18d769c
Vilnor <onlytelford@yahoo.com>
2020-12-09 21:51:34 -0500

    Desolation added damage preview to character card daamge fancied up discard obliveon proofing

--- Controller/Environments/BlackwoodForest/Cards/DesolationCardController.cs
+++ Controller/Environments/BlackwoodForest/Cards/DesolationCardController.cs
@@ -17,41 +17,66 @@ namespace Cauldron.BlackwoodForest
 
         public static readonly string Identifier = "Desolation";
 
-        public DesolationCardController(Card card, TurnTakerController turnTakerController) : base(card,
-            turnTakerController)
+        public DesolationCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
         }
 
         public override void AddTriggers()
         {
             // Destroy self at end of env. turn
-            base.AddEndOfTurnTrigger(tt => tt == base.TurnTaker,
-                base.DestroyThisCardResponse,
-                TriggerType.DestroySelf);
+            base.AddEndOfTurnTrigger(tt => tt == base.TurnTaker, base.DestroyThisCardResponse, TriggerType.DestroySelf);
 
             base.AddTriggers();
         }
 
+        private IEnumerator DiscardAllBut1CardResponse(HeroTurnTakerController httc)
+        {
+            return base.GameController.SelectCardsFromLocationAndMoveThem(httc, httc.HeroTurnTaker.Hand,
+                        null, httc.NumberOfCardsInHand - 1,
+                        new LinqCardCriteria(card => card.Location == httc.HeroTurnTaker.Hand && GameController.IsLocationVisibleToSource(card.Location, GetCardSource()), "hand"),
+                        new[] { new MoveCardDestination(httc.TurnTaker.Trash) },
+                        isDiscardIfMovingToTrash: true,
+                        cancelDecisionsIfTrue: () => httc.HeroTurnTaker.NumberOfCardsInHand <= 1,
+                        selectionType: SelectionType.DiscardCard,
+                        cardSource: base.GetCardSource());
+        }
+
+        private IEnumerator DealPsychicDamageResponse(HeroTurnTakerController httc)
+        {
+            List<Card> result = new List<Card>();
+            var coroutine = FindCharacterCardToTakeDamage(httc.TurnTaker, result, Card, H + 1, DamageType.Psychic);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            coroutine = DealDamage(Card, result.First(), H + 1, DamageType.Psychic, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+        }
+
         public override IEnumerator Play()
         {
-            int damageToDeal = Game.H + 1;
-
             IEnumerable<Function> Functions(HeroTurnTakerController httc) => new Function[2]
             {
                 new Function(httc, "Discard all but 1 card", SelectionType.DiscardCard,
-                    () => base.GameController.SelectCardsFromLocationAndMoveThem(httc, httc.HeroTurnTaker.Hand,
-                        httc.NumberOfCardsInHand - 1, httc.NumberOfCardsInHand - 1, 
-                        new LinqCardCriteria(card => card.Location == httc.HeroTurnTaker.Hand, "hand"),
-                        new[] { new MoveCardDestination(httc.TurnTaker.Trash) }, cardSource: base.GetCardSource())),
+                    () => DiscardAllBut1CardResponse(httc),
+                    httc.HeroTurnTaker.Hand.HasCards),
 
-                new Function(httc, $"Take {damageToDeal} psychic damage", SelectionType.DealDamage,
-                    () => this.DealDamage(this.Card, (Card c) => httc.CharacterCards.Contains(c) && c.IsTarget, damageToDeal, DamageType.Psychic))
+                new Function(httc, $"Take {H + 1} psychic damage", SelectionType.DealDamage,
+                    () => DealPsychicDamageResponse(httc))
             };
 
-            IEnumerator coroutine = EachPlayerSelectsFunction(h =>
-                    !h.IsIncapacitatedOrOutOfGame, Functions, Game.H, null,
-                h => h.Name + " has no cards in their hand.");
-
+            IEnumerator coroutine = EachPlayerSelectsFunction(h => !h.IsIncapacitatedOrOutOfGame, Functions, Game.H, null, h => h.Name + " has no cards in their hand.");
             if (base.UseUnityCoroutines)
             {
                 yield return base.GameController.StartCoroutine(coroutine);
