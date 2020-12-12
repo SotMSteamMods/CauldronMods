37cb4fded8138a29195e004305d31b6c42946be1
Jake <mgirman520@gmail.com>
2020-12-11 22:43:02 -0500

    Create Angry Lethovore and Aquatic Sphere

---
+++ Controller/Villains/Menagerie/AquaticSphereCardController.cs
@@ -0,0 +1,52 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+
+namespace Cauldron
+{
+    public class AquaticSphereCardController : CardController
+    {
+        public AquaticSphereCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+
+        public override IEnumerator Play()
+        {
+            //When this card enters play, place the top card of the villain deck beneath it face down.
+            IEnumerator coroutine = base.GameController.MoveCard(base.TurnTakerController, base.TurnTaker.Deck.TopCard, base.Card.UnderLocation, flipFaceDown: true, cardSource: base.GetCardSource());
+            coroutine = base.GameController.GainHP(base.Card, 6, cardSource: base.GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+
+        public override void AddTriggers()
+        {
+            //Whenever a hero uses a power, they must discard a card.
+            base.AddTrigger<UsePowerAction>((UsePowerAction action) => true, this.DiscardCardResponse, TriggerType.DiscardCard, TriggerTiming.After);
+        }
+
+        private IEnumerator DiscardCardResponse(UsePowerAction action)
+        {
+            //...they must discard a card.
+            IEnumerator coroutine = base.GameController.SelectAndDiscardCard(action.HeroUsingPower, cardSource: base.GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+    }
+}
---
+++ Controller/Villains/Menagerie/CharacterCards/MenagerieCharacterCardController.cs
@@ -0,0 +1,14 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+
+namespace Cauldron.Menagerie
+{
+    public class MenagerieCharacterCardController : VillainCharacterCardController
+    {
+        public MenagerieCharacterCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+    }
+}
