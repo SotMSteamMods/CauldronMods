bd8a0213dd098ff1349346498164c307f52eef61
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2020-12-13 09:44:39 -0600

    Modified FrozenSolid_SkipPlay tests
    Fix for #461

--- Testing/Environments/NorthsparTests.cs
+++ Testing/Environments/NorthsparTests.cs
@@ -1103,11 +1103,11 @@ namespace CauldronTests
         [Test()]
         public void TestFrozenSolid_SkipPlay()
         {
-            SetupGameController("WagerMaster", "AbsoluteZero", "Legacy", "Ra", "Cauldron.Northspar");
+            SetupGameController(new string[] { "WagerMaster", "AbsoluteZero", "Legacy", "Ra", "Tachyon", "Haka", "Cauldron.Northspar" }, randomSeed: new int?(-1447644233));
             StartGame();
             SetHitPoints(az, 10);
             SetHitPoints(legacy, 10);
-
+            DestroyNonCharacterVillainCards();
             PlayCard("BreakingTheRules");
 
             DecisionSelectCard = az.CharacterCard;
