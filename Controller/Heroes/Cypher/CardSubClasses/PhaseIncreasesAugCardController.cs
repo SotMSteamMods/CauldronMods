0bcb77d217dbc558c838db11bfdfaef6db7da5c2
Vilnor <onlytelford@yahoo.com>
2020-12-12 22:35:22 -0500

    Moved FusionAug and RetinalAug to a new base class

---
+++ Controller/Heroes/Cypher/CardSubClasses/PhaseIncreasesAugCardController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+
+namespace Cauldron.Cypher
+{
+    public abstract class PhaseIncreasesAugCardController : AugBaseCardController
+    {
+        private readonly Phase _phaseToIncrease;
+
+
+        protected PhaseIncreasesAugCardController(Card card, TurnTakerController turnTakerController, Phase phase) : base(card, turnTakerController)
+        {
+            _phaseToIncrease = phase;
+            AddThisCardControllerToList(CardControllerListType.IncreasePhaseActionCount);
+        }
+
+        public override IEnumerator Play()
+        {
+            IEnumerator coroutine = IncreasePhaseActionCountIfInPhase((TurnTaker tt) => tt == base.TurnTaker, _phaseToIncrease, 1);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+        }
+
+        public override void AddTriggers()
+        {
+            //They may play an additional card during their play phase.
+            base.AddAdditionalPhaseActionTrigger(this.ShouldIncreasePhaseActionCount, _phaseToIncrease, 1);
+
+            base.AddTriggers();
+        }
+
+        private bool ShouldIncreasePhaseActionCount(TurnTaker tt)
+        {
+            return tt == base.GetCardThisCardIsNextTo(true).Owner;
+        }
+
+        public override bool AskIfIncreasingCurrentPhaseActionCount()
+        {
+            if (base.GameController.ActiveTurnPhase.Phase == _phaseToIncrease)
+            {
+                return ShouldIncreasePhaseActionCount(base.GameController.ActiveTurnTaker);
+            }
+            return false;
+        }
+    }
+}
--- Controller/Heroes/Cypher/Cards/RetinalAugCardController.cs
+++ Controller/Heroes/Cypher/Cards/RetinalAugCardController.cs
@@ -6,7 +6,7 @@ using Handelabra.Sentinels.Engine.Model;
 
 namespace Cauldron.Cypher
 {
-    public class RetinalAugCardController : AugBaseCardController
+    public class RetinalAugCardController : PhaseIncreasesAugCardController
     {
         //==============================================================
         // Play this card next to a hero. The hero next to this card is augmented.
@@ -15,22 +15,8 @@ namespace Cauldron.Cypher
 
         public static string Identifier = "RetinalAug";
 
-        public RetinalAugCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        public RetinalAugCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController, Phase.PlayCard)
         {
-            AddThisCardControllerToList(CardControllerListType.IncreasePhaseActionCount);
-        }
-
-        public override void AddTriggers()
-        {
-            //They may play an additional card during their play phase.
-            base.AddAdditionalPhaseActionTrigger(this.ShouldIncreasePhaseActionCount, Phase.PlayCard, 1);
-
-            base.AddTriggers();
-        }
-
-        private bool ShouldIncreasePhaseActionCount(TurnTaker tt)
-        {
-            return tt == base.GetCardThisCardIsNextTo(true).Owner;
         }
     }
 }
