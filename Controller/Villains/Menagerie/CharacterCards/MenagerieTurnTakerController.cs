03b8324e7f51a141d8fba40512e9c771dce96510
Jake <mgirman520@gmail.com>
2020-12-14 15:15:34 -0500

    Create MenagerieTurnTakerController.cs

---
+++ Controller/Villains/Menagerie/CharacterCards/MenagerieTurnTakerController.cs
@@ -0,0 +1,41 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+
+namespace Cauldron.Menagerie
+{
+    public class MenagerieTurnTakerController : TurnTakerController
+    {
+        public MenagerieTurnTakerController(TurnTaker turnTaker, GameController gameController) : base(turnTaker, gameController)
+        {
+
+        }
+
+        public override IEnumerator StartGame()
+        {
+            //Search the villain deck for the card Prized Catch and put it into play.
+            IEnumerator coroutine = base.PutCardIntoPlay("PrizedCatch");
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+
+            //Shuffle the villain deck.
+            coroutine = base.GameController.ShuffleLocation(base.TurnTaker.Deck);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+    }
+}
