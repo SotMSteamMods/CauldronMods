71c5c090fe42c4a7126a1c6f2538c51e9d4903c1
Jake <mgirman520@gmail.com>
2020-12-14 11:37:29 -0500

    Decklist Icons

---
+++ Controller/Villains/Menagerie/Cards/HyrianSnipeCardController.cs
@@ -0,0 +1,14 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+
+namespace Cauldron
+{
+    public class HyrianSnipeCardController : CardController
+    {
+        public HyrianSnipeCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+    }
+}
