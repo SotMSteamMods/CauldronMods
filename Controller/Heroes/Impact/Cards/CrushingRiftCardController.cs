0fb60302e83b907b2254521e996d82f32f4075e2
Vilnor <onlytelford@yahoo.com>
2020-12-21 19:42:48 -0500

    Crushing Rift Added a isCardIsVisible check and moved critertia out to separate function

--- Controller/Heroes/Impact/Cards/CrushingRiftCardController.cs
+++ Controller/Heroes/Impact/Cards/CrushingRiftCardController.cs
@@ -15,11 +15,16 @@ namespace Cauldron.Impact
         {
         }
 
+        private bool TargetCriteria(Card c)
+        {
+            return c.IsInPlayAndHasGameText && c.IsTarget && !c.IsCharacter && c.HitPoints.HasValue && c.HitPoints > 0 && c.HitPoints < c.MaximumHitPoints && GameController.IsCardVisibleToCardSource(c, GetCardSource());
+        }
+
         public override IEnumerator Play()
         {
             //"Select a non-character target in play with current HP less than its maximum HP.",
             var storedTarget = new List<SelectCardDecision> { };
-            IEnumerator coroutine = GameController.SelectCardAndStoreResults(DecisionMaker, SelectionType.SetHP, new LinqCardCriteria((Card c) => c.IsInPlayAndHasGameText && c.IsTarget && !c.IsCharacter && c.HitPoints < c.MaximumHitPoints, "", false, singular: "non-character target with less than maximum HP", plural:"non-character targets with less than maximum HP"), storedTarget, false, cardSource: GetCardSource());
+            IEnumerator coroutine = GameController.SelectCardAndStoreResults(DecisionMaker, SelectionType.SetHP, new LinqCardCriteria(TargetCriteria, "", false, singular: "non-character target with less than maximum HP", plural: "non-character targets with less than maximum HP"), storedTarget, false, cardSource: GetCardSource());
             if (base.UseUnityCoroutines)
             {
                 yield return base.GameController.StartCoroutine(coroutine);
@@ -29,16 +34,13 @@ namespace Cauldron.Impact
                 base.GameController.ExhaustCoroutine(coroutine);
             }
 
-            if(DidSelectCard(storedTarget))
+            if (DidSelectCard(storedTarget))
             {
                 //"Reduce that target to half of its current HP, rounded up."
                 var selectedTarget = GetSelectedCard(storedTarget);
                 int currentHP = selectedTarget.HitPoints ?? 0;
-                int halfHP = currentHP;
-                if(halfHP > 0)
-                {
-                    halfHP = (currentHP / 2) + currentHP % 2;
-                }
+                int halfHP = (currentHP / 2) + currentHP % 2;
+
                 coroutine = GameController.SetHP(selectedTarget, halfHP, GetCardSource());
                 if (base.UseUnityCoroutines)
                 {
