c1d09b3e94230f86836d17ad3485e97c16be1506
Vilnor <onlytelford@yahoo.com>
2020-12-24 15:11:33 -0500

    reviewed all other reflection status effect and set caneffectstack=true

--- Controller/Heroes/Quicksilver/CharacterCards/RenegadeQuicksilverCharacterCardController.cs
+++ Controller/Heroes/Quicksilver/CharacterCards/RenegadeQuicksilverCharacterCardController.cs
@@ -38,6 +38,7 @@ namespace Cauldron.Quicksilver
                         //The next time a hero is dealt damage, they may play a card.
                         OnDealDamageStatusEffect statusEffect = new OnDealDamageStatusEffect(base.Card, "PlayCardResponse", "The next time a hero is dealt damage, they may play a card.", new TriggerType[] { TriggerType.PlayCard }, base.TurnTaker, base.Card);
                         statusEffect.NumberOfUses = 1;
+                        statusEffect.CanEffectStack = true;
                         Incap2Count++;
                         IEnumerator coroutine2 = base.AddStatusEffect(statusEffect, true);
                         if (base.UseUnityCoroutines)
