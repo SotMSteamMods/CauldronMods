343f081f5104f2b9c60a9d9c02c984143f5e3ed3
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-01-04 02:08:55 -0600

    Added MapToLostChoth Controller

--- CauldronMods/Controller/Environments/VaultFive/Cards/MapToLostChothCardController.cs
+++ CauldronMods/Controller/Environments/VaultFive/Cards/MapToLostChothCardController.cs
@@ -16,6 +16,47 @@ namespace Cauldron.VaultFive
 
         public override IEnumerator UniqueOnPlayEffect()
         {
+            //1 player discards a card...
+            List<TurnTaker> usedTurnTakers = new List<TurnTaker>();
+            List<SelectTurnTakerDecision> storedResults = new List<SelectTurnTakerDecision>();
+            IEnumerator coroutine = base.GameController.SelectHeroToDiscardCard(DecisionMaker, storedResultsTurnTaker: storedResults, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            if(DidSelectTurnTaker(storedResults))
+            {
+                usedTurnTakers.Add(GetSelectedTurnTaker(storedResults));
+            }
+            //... a second player plays a card...
+            storedResults = new List<SelectTurnTakerDecision>();
+            coroutine = SelectHeroToPlayCard(DecisionMaker, optionalPlayCard: false, storedResultsTurnTaker: storedResults, heroCriteria: new LinqTurnTakerCriteria((TurnTaker tt) => !usedTurnTakers.Contains(tt)));
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            if (DidSelectTurnTaker(storedResults))
+            {
+                usedTurnTakers.Add(GetSelectedTurnTaker(storedResults));
+            }
+            //... and a third player’s hero uses a power
+            coroutine = GameController.SelectHeroToUsePower(DecisionMaker, optionalUsePower: false, additionalCriteria: new LinqTurnTakerCriteria((TurnTaker tt) => !usedTurnTakers.Contains(tt)), cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
             yield break;
         }
     }
