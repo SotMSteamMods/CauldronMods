67ec19e00a990fcb18216cb4b987a8748505c0b0
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-01-04 02:09:06 -0600

    Added SigQuilsSign Controller

--- CauldronMods/Controller/Environments/VaultFive/Cards/SigQuilsSignCardController.cs
+++ CauldronMods/Controller/Environments/VaultFive/Cards/SigQuilsSignCardController.cs
@@ -16,6 +16,26 @@ namespace Cauldron.VaultFive
 
         public override IEnumerator UniqueOnPlayEffect()
         {
+            //increase damage dealt to and by targets from its deck by 1 until the start of their next turn
+            IncreaseDamageStatusEffect effect1 = new IncreaseDamageStatusEffect(1);
+            effect1.SourceCriteria.NativeDeck = Card.NativeDeck;
+            effect1.UntilStartOfNextTurn(Card.Owner);
+            IEnumerator addEffect1 = AddStatusEffect(effect1);
+            IncreaseDamageStatusEffect effect2 = new IncreaseDamageStatusEffect(1);
+            effect2.TargetCriteria.NativeDeck = Card.NativeDeck;
+            effect2.UntilStartOfNextTurn(Card.Owner);
+            IEnumerator addEffect2 = AddStatusEffect(effect2);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(addEffect1);
+                yield return base.GameController.StartCoroutine(addEffect2);
+
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(addEffect1);
+                base.GameController.ExhaustCoroutine(addEffect2);
+            }
             yield break;
         }
     }
