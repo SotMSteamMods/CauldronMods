7c5349fe03f382e16d172d17f42200a3c0937179
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-01-04 02:09:20 -0600

    Added TheCoinThatStrays Controller

--- CauldronMods/Controller/Environments/VaultFive/Cards/TheCoinThatStraysCardController.cs
+++ CauldronMods/Controller/Environments/VaultFive/Cards/TheCoinThatStraysCardController.cs
@@ -15,6 +15,52 @@ namespace Cauldron.VaultFive
         }
         public override IEnumerator UniqueOnPlayEffect()
         {
+            //a hero from its deck
+            List<Card> storedResults = new List<Card>();
+            IEnumerator coroutine = SelectActiveHeroCharacterCardToDoAction(storedResults, SelectionType.HeroToDealDamage);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            Card hero = storedResults.FirstOrDefault();
+            if (hero == null)
+            {
+                yield break;
+            }
+            //... deals themselves...
+            coroutine = DealDamage(hero, hero, 1, DamageType.Psychic, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            //...and 1 other target 1 psychic damage each...
+            coroutine = base.GameController.SelectTargetsAndDealDamage(DecisionMaker, new DamageSource(GameController, hero), 1, DamageType.Psychic, 1, false, 1, additionalCriteria: (Card c) => c != hero, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            //...and plays a card
+            coroutine = SelectAndPlayCardFromHand(FindHeroTurnTakerController(hero.Owner.ToHero()));
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
             yield break;
         }
     }
