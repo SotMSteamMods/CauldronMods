b7a21bc9d5d21e0e51632005fc97835086027c09
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-01-05 01:44:29 -0600

    Modifed UtilitySubClass
    Added basic deck functions

--- CauldronMods/Controller/Environments/VaultFive/CardSubClasses/VaultFiveUtilityCardController.cs
+++ CauldronMods/Controller/Environments/VaultFive/CardSubClasses/VaultFiveUtilityCardController.cs
@@ -13,5 +13,21 @@ namespace Cauldron.VaultFive
         protected VaultFiveUtilityCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
         }
+        public static readonly string ArtifactKeyword = "artifact";
+
+        protected bool IsArtifact(Card card)
+        {
+            return card.DoKeywordsContain(ArtifactKeyword);
+        }
+
+        protected IEnumerable<TurnTaker> GetPlayersWithArtifactsInHand()
+        {
+            return FindTurnTakersWhere((TurnTaker tt) => tt.IsHero && !tt.IsIncapacitatedOrOutOfGame && DoesPlayerHaveArtifactInHand(tt.ToHero()));
+        }
+
+        protected bool DoesPlayerHaveArtifactInHand(HeroTurnTaker htt)
+        {
+            return htt.Hand.Cards.Any((Card c) => IsArtifact(c));
+        }
     }
 }
