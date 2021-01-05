6931c1e63782a38758dd1556abeeace38d3f6a0e
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-01-05 01:44:46 -0600

    Added DeeperIntoTheVault Controller

--- CauldronMods/Controller/Environments/VaultFive/Cards/DeeperIntoTheVaultCardController.cs
+++ CauldronMods/Controller/Environments/VaultFive/Cards/DeeperIntoTheVaultCardController.cs
@@ -13,5 +13,27 @@ namespace Cauldron.VaultFive
         public DeeperIntoTheVaultCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
         }
+
+        public override void AddTriggers()
+        {
+            //Increase all psychic damage dealt by 1.
+            AddIncreaseDamageTrigger((DealDamageAction dd) => dd.DamageType == DamageType.Psychic, 1);
+        }
+
+        public override IEnumerator Play()
+        {
+            //When this card enters play, reveal the top {H} cards of the environment deck. Put any Artifact cards revealed into play and discard the other revealed cards.
+            IEnumerator coroutine = RevealCards_PutSomeIntoPlay_DiscardRemaining(TurnTakerController, TurnTaker.Deck, Game.H, new LinqCardCriteria((Card c) => IsArtifact(c), "artifact"), isPutIntoPlay: true);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+
+            yield break;
+        }
     }
 }
