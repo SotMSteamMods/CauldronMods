59a6e84e2d79470ff27d51018432518c19192e8c
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-01-05 23:57:10 -0600

    Added TheRatsInTheWalls Controller

--- CauldronMods/Controller/Environments/VaultFive/Cards/TheRatsInTheWallsCardController.cs
+++ CauldronMods/Controller/Environments/VaultFive/Cards/TheRatsInTheWallsCardController.cs
@@ -12,6 +12,44 @@ namespace Cauldron.VaultFive
     {
         public TheRatsInTheWallsCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
+            SpecialStringMaker.ShowSpecialString(() => BuildPlayersWithArtifactInHandSpecialString());
+        }
+
+        public override void AddTriggers()
+        {
+            //Whenever a player draws an Artifact card, they may draw a card.
+            AddTrigger<DrawCardAction>((DrawCardAction dca) => dca.IsSuccessful && dca.CardToDraw != null && IsArtifact(dca.CardToDraw), DrawCardResponse, TriggerType.DrawCard, TriggerTiming.After);
+            //Increase damage dealt to heroes with Artifact cards in their hands by 1
+            AddIncreaseDamageTrigger((DealDamageAction dd) => dd.Target != null && dd.Target.IsHeroCharacterCard && GetPlayersWithArtifactsInHand().Contains(dd.Target.Owner), 1);
+        }
+
+        private IEnumerator DrawCardResponse(DrawCardAction dca)
+        {
+            IEnumerator coroutine = DrawCard(hero: dca.HeroTurnTaker, optional: true);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+
+        public override IEnumerator Play()
+        {
+            //When this card enters play, it deals each target 1 psychic damage.",
+            IEnumerator coroutine = DealDamage(Card, (Card c) => c.IsTarget, 1, DamageType.Psychic);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
         }
     }
 }
