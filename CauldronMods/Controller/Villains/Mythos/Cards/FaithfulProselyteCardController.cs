5cdea6309580518b652ffea6ef6e861c896656e6
Jake <mgirman520@gmail.com>
2021-01-06 12:40:35 -0500

    Update FaithfulProselyteCardController.cs

--- CauldronMods/Controller/Villains/Mythos/Cards/FaithfulProselyteCardController.cs
+++ CauldronMods/Controller/Villains/Mythos/Cards/FaithfulProselyteCardController.cs
@@ -12,6 +12,46 @@ namespace Cauldron.Mythos
     {
         public FaithfulProselyteCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
+
+        }
+
+        public override IEnumerator Play()
+        {
+            //{MythosMadness} When this card enters play, destroy {H - 2} equipment cards.
+            if (base.IsTopCardMatching(MythosMadnessDeckIdentifier))
+            {
+                IEnumerator coroutine = base.GameController.SelectAndDestroyCards(this.DecisionMaker, new LinqCardCriteria((Card c) => base.IsEquipment(c)), base.Game.H - 2, cardSource: base.GetCardSource());
+                if (UseUnityCoroutines)
+                {
+                    yield return GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    GameController.ExhaustCoroutine(coroutine);
+                }
+            }
+            yield break;
+        }
+
+        public override void AddTriggers()
+        {
+            //At the end of the villain turn, this card deals the hero target with the second highest HP {H - 1} lightning damage.
+            base.AddEndOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, this.DealDamageResponse, TriggerType.DealDamage);
+        }
+
+        private IEnumerator DealDamageResponse(PhaseChangeAction action)
+        {
+            //...this card deals the hero target with the second highest HP {H - 1} lightning damage.
+            IEnumerator coroutine = base.DealDamageToHighestHP(this.Card, 2, (Card c) => c.IsHero, (Card c) => base.Game.H - 1, DamageType.Lightning);
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
         }
     }
 }
