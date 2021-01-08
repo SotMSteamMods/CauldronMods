9e815579405bbac906a6684b258d9f16615de481
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-01-08 04:59:06 -0600

    Initialized Test Suite

---
+++ Testing/Environments/CatchwaterHarborTests.cs
@@ -0,0 +1,34 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using Handelabra.Sentinels.UnitTest;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CauldronTests
+{
+    [TestFixture()]
+    public class CatchwaterHarborTests : BaseTest
+    {
+
+        #region CatchwaterHarborHelperFunctions
+
+        protected TurnTakerController catchwater { get { return FindEnvironment(); } }
+        protected bool IsTransport(Card card)
+        {
+            return card.DoKeywordsContain("transport");
+        }
+
+        #endregion
+
+        [Test()]
+        public void TestCatchwaterWorks()
+        {
+            SetupGameController("BaronBlade", "Ra", "Legacy", "Haka", "Cauldron.CatchwaterHarbor");
+            StartGame();
+            Assert.AreEqual(5, this.GameController.TurnTakerControllers.Count());
+
+        }
+
+    }
+}
