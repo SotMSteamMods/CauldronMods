fb115021617b134e0b37a51b133eb9359cf29ec5
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-01-10 10:46:49 -0600

    Added remaining Djinn Controllers

--- CauldronMods/Controller/Environments/TheChasmOfAThousandNights/Cards/GulCardController.cs
+++ CauldronMods/Controller/Environments/TheChasmOfAThousandNights/Cards/GulCardController.cs
@@ -12,6 +12,29 @@ namespace Cauldron.TheChasmOfAThousandNights
     {
         public GulCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
+            SpecialStringMaker.ShowNonEnvironmentTargetWithLowestHP();
+        }
+
+        public override void AddTriggers()
+        {
+            //At the end of the environment turn, this card deals the non-environment target with the lowest HP 3 infernal damage.
+            AddDealDamageAtEndOfTurnTrigger(TurnTaker, Card, (Card c) => c.IsNonEnvironmentTarget && GameController.IsCardVisibleToCardSource(c, GetCardSource()), TargetType.LowestHP, 3, DamageType.Infernal);
+            //Whenever this card destroys a target, it deals each target other than itself 1 cold damage.
+            AddTrigger((DestroyCardAction dca) => dca.WasCardDestroyed && dca.CardToDestroy != null && dca.CardToDestroy.Card != null && dca.CardToDestroy.Card.IsTarget && dca.CardSource != null && dca.CardSource.Card == Card, DealDamageResponse, TriggerType.DealDamage, TriggerTiming.After);
+        }
+
+        private IEnumerator DealDamageResponse(DestroyCardAction arg)
+        {
+            IEnumerator coroutine = DealDamage(Card, (Card c) => c != Card && GameController.IsCardVisibleToCardSource(c, GetCardSource()), 1, DamageType.Cold);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
         }
     }
 }
--- CauldronMods/Controller/Environments/TheChasmOfAThousandNights/Cards/TevaelCardController.cs
+++ CauldronMods/Controller/Environments/TheChasmOfAThousandNights/Cards/TevaelCardController.cs
@@ -12,6 +12,20 @@ namespace Cauldron.TheChasmOfAThousandNights
     {
         public TevaelCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
+            AddThisCardControllerToList(CardControllerListType.MakesIndestructible);
+            SpecialStringMaker.ShowNonEnvironmentTargetWithHighestHP(ranking: 3);
+        }
+
+        public override bool AskIfCardIsIndestructible(Card card)
+        {
+            //This card is indestructible while it has more than 0 HP.
+            return card == Card && card.HitPoints > 0;
+        }
+
+        public override void AddTriggers()
+        {
+            //At the end of the environment turn, this card deals the non-environment target with the third highest HP 4 projectile damage.
+            AddDealDamageAtEndOfTurnTrigger(TurnTaker, Card, (Card c) => c.IsNonEnvironmentTarget && GameController.IsCardVisibleToCardSource(c, GetCardSource()), TargetType.HighestHP, 4, DamageType.Projectile, highestLowestRanking: 3);
         }
     }
 }
