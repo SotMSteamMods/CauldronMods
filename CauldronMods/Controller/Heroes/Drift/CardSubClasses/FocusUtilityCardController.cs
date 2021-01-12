257f5b6e283e0837f68985d319c44c3f7f4a6d74
Jake <mgirman520@gmail.com>
2021-01-11 23:34:44 -0500

    Update FutureFocusCardController

---
+++ CauldronMods/Controller/Heroes/Drift/CardSubClasses/FocusUtilityCardController.cs
@@ -0,0 +1,30 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+
+namespace Cauldron.Drift
+{
+    public class FocusUtilityCardController : DriftUtilityCardController
+    {
+        public FocusUtilityCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+
+        public override IEnumerator Play()
+        {
+            //When this card enters play, return all other focus cards to your hand.
+            IEnumerator coroutine = base.GameController.MoveCards(base.TurnTakerController, base.FindCardsWhere(new LinqCardCriteria((Card c) => base.IsFocus(c) && c.IsInPlayAndHasGameText && c != this.Card)), (Card c) => new MoveCardDestination(base.HeroTurnTaker.Hand), cardSource: base.GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+    }
+}
--- CauldronMods/Controller/Heroes/Drift/Cards/FutureFocusCardController.cs
+++ CauldronMods/Controller/Heroes/Drift/Cards/FutureFocusCardController.cs
@@ -8,10 +8,52 @@ using Handelabra.Sentinels.Engine.Model;
 
 namespace Cauldron.Drift
 {
-    public class FutureFocusCardController : DriftUtilityCardController
+    public class FutureFocusCardController : FocusUtilityCardController
     {
         public FutureFocusCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
+
+        }
+
+        public override void AddTriggers()
+        {
+            //When {Drift} is dealt damage, if you have not shifted this turn, you may shift {DriftRRR}. If you shifted {DriftRRR} this way, {Drift} deals 1 target 3 radiant damage.
+            base.AddTrigger<DealDamageAction>((DealDamageAction action) => action.Target == base.GetActiveCharacterCard() && action.Amount > 0 && !base.Journal.CardPropertiesEntriesThisRound((CardPropertiesJournalEntry entry) => entry.Key == HasShifted).Any(), this.ShiftResponse, TriggerType.ModifyTokens, TriggerTiming.After);
+            ;
+        }
+
+        private IEnumerator ShiftResponse(DealDamageAction action)
+        {
+            bool canShift = base.CurrentShiftPosition() == 1;
+            List<YesNoCardDecision> decision = new List<YesNoCardDecision>();
+            //...you may shift {DriftRRR}. 
+            IEnumerator coroutine = base.GameController.MakeYesNoCardDecision(base.HeroTurnTakerController, SelectionType.MakeDecision, this.Card, action, decision, new Card[] { base.GetShiftTrack() }, base.GetCardSource());
+            if (decision.FirstOrDefault().Answer ?? false)
+            {
+                coroutine = base.ShiftRRR();
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+                //If you shifted {DriftRRR} this way, {Drift} deals 1 target 3 radiant damage.
+                if (canShift)
+                {
+                    coroutine = base.GameController.SelectTargetsAndDealDamage(base.HeroTurnTakerController, new DamageSource(base.GameController, base.GetActiveCharacterCard()), 3, DamageType.Radiant, 1, false, 1, cardSource: base.GetCardSource());
+                    if (base.UseUnityCoroutines)
+                    {
+                        yield return base.GameController.StartCoroutine(coroutine);
+                    }
+                    else
+                    {
+                        base.GameController.ExhaustCoroutine(coroutine);
+                    }
+                }
+            }
+            yield break;
         }
     }
 }
