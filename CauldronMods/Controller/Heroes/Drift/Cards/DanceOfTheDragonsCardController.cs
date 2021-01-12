7355e42c778aede012dbbaa54cc6ead4b1d26aa3
Jake <mgirman520@gmail.com>
2021-01-11 19:46:49 -0500

    Update DanceOfTheDragonsCardController

--- CauldronMods/Controller/Heroes/Drift/Cards/DanceOfTheDragonsCardController.cs
+++ CauldronMods/Controller/Heroes/Drift/Cards/DanceOfTheDragonsCardController.cs
@@ -12,6 +12,72 @@ namespace Cauldron.Drift
     {
         public DanceOfTheDragonsCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
+
+        }
+
+        public override IEnumerator Play()
+        {
+            //{DriftFuture}
+            if (base.IsTimeMatching(base.Future))
+            {
+                //{Drift} deals up to 3 targets 2 radiant damage each.
+                //Increase damage dealt this way by 1 to targets that entered play since the end of your last turn.
+                IEnumerator coroutine = base.GameController.SelectTargetsAndDealDamage(base.HeroTurnTakerController, new DamageSource(base.GameController, this.Card), (Card target) => 2 + this.IncreaseDamageIfTargetEnteredPlaySinceLastTurn(target), DamageType.Radiant, () => 3, true, 0, cardSource: base.GetCardSource());
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+
+                //Shift {DriftL}.
+
+            }
+
+            //{DriftPast}
+            if (base.IsTimeMatching(base.Future))
+            {
+                //Draw a card. 
+                IEnumerator coroutine = base.DrawCard();
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+
+                //{Drift} regains 1 HP. 
+                coroutine = base.GameController.GainHP(base.CharacterCard, 1);
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+
+                //Shift {DriftRR}
+
+            }
+            yield break;
+        }
+
+        private int IncreaseDamageIfTargetEnteredPlaySinceLastTurn(Card target)
+        {
+            IEnumerable<CardEntersPlayJournalEntry> cardEntries = base.Journal.QueryJournalEntries<CardEntersPlayJournalEntry>((CardEntersPlayJournalEntry e) => e.Card.IsTarget).Where(base.Game.Journal.SinceLastTurn<CardEntersPlayJournalEntry>(base.TurnTaker));
+            if (cardEntries.Where(entry => entry.Card == target).Any())
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
