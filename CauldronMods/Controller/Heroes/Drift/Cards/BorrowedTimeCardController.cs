e0b97b7d9e059895f6ac468ae343ed62bc0e822f
Jake <mgirman520@gmail.com>
2021-01-11 19:04:10 -0500

    Update BorrowedTimeCardController.cs

--- CauldronMods/Controller/Heroes/Drift/Cards/BorrowedTimeCardController.cs
+++ CauldronMods/Controller/Heroes/Drift/Cards/BorrowedTimeCardController.cs
@@ -12,6 +12,14 @@ namespace Cauldron.Drift
     {
         public BorrowedTimeCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
+
+        }
+
+        public override IEnumerator Play()
+        {
+            //Select {DriftL} or {DriftR}. Shift that direction up to 3 times. X is the number of times you shifted this way.
+            //If you shifted at least {DriftL} this way, X targets regain 2 HP each. If you shifted {DriftR} this way, {Drift} deals X targets 3 radiant damage each.
+            yield break;
         }
     }
 }
