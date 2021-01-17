63aedabb75a6177b2ff678875db6a90c0c3012a4
Jake <mgirman520@gmail.com>
2021-01-17 11:32:38 -0500

    Shift Tracks are needed for Breach and Dual

---
+++ CauldronMods/Controller/Heroes/Drift/CharacterCards/Shift Track/ThroughTheBreach/ThroughTheBreachShiftTrack3CardController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+
+namespace Cauldron.Drift
+{
+    public class ThroughTheBreachShiftTrack3CardController : ShiftTrackUtilityCardController
+    {
+        public ThroughTheBreachShiftTrack3CardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+
+        }
+    }
+}
