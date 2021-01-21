4f578fc6467e764da82903f98806bfd052b63434
LNBertolini <bertolini.lucas.n@gmail.com>
2021-01-20 19:04:56 -0600

    Renegade Gyrosaur controller + power test

--- Testing/Heroes/GyrosaurVariantTests.cs
+++ Testing/Heroes/GyrosaurVariantTests.cs
@@ -16,9 +16,8 @@ namespace CauldronTests
         #region GyrosaurHelperFunctions
         #endregion
         [Test]
-        public void TestGyrosaurLoads()
+        public void TestSpeedDemonGyrosaurLoads()
         {
-            Assert.Ignore("Still in notPromoCards");
             SetupGameController("BaronBlade", "Cauldron.Gyrosaur/SpeedDemonGyrosaurCharacter", "Megalopolis");
 
             Assert.AreEqual(3, this.GameController.TurnTakerControllers.Count());
@@ -27,6 +26,102 @@ namespace CauldronTests
             Assert.IsInstanceOf(typeof(SpeedDemonGyrosaurCharacterCardController), gyrosaur.CharacterCardController);
 
             Assert.AreEqual(28, gyrosaur.CharacterCard.HitPoints);
+        }
+        [Test]
+        public void TestRenegadeGyrosaurLoads()
+        {
+            SetupGameController("BaronBlade", "Cauldron.Gyrosaur/RenegadeGyrosaurCharacter", "Megalopolis");
+
+            Assert.AreEqual(3, this.GameController.TurnTakerControllers.Count());
+
+            Assert.IsNotNull(gyrosaur);
+            Assert.IsInstanceOf(typeof(RenegadeGyrosaurCharacterCardController), gyrosaur.CharacterCardController);
+
+            Assert.AreEqual(29, gyrosaur.CharacterCard.HitPoints);
+        }
+        [Test]
+        public void TestRenegadePowerDiscardCrash()
+        {
+            SetupGameController("BaronBlade", "Cauldron.Gyrosaur/RenegadeGyrosaurCharacter", "Legacy", "Ra", "Megalopolis");
+            StartGame();
+            DestroyNonCharacterVillainCards();
+
+            MoveAllCardsFromHandToDeck(gyrosaur);
+
+            var ttcs = new TurnTakerController[] { baron, gyrosaur, legacy, ra };
+            foreach(TurnTakerController ttc in ttcs)
+            {
+                SetHitPoints(ttc, 10);
+            }
+            QuickHPStorage(ttcs);
+
+            Card wipeout = PutInHand("Wipeout");
+            Card chase = PutOnDeck("AMerryChase");
+
+            UsePower(gyrosaur);
+            AssertInTrash(wipeout);
+            AssertInHand(chase);
+            QuickHPCheck(0, 2, 0, 0);
+        }
+        [Test]
+        public void TestRenegadePowerDiscardNonCrash()
+        {
+            SetupGameController("BaronBlade", "Cauldron.Gyrosaur/RenegadeGyrosaurCharacter", "Legacy", "Ra", "Megalopolis");
+            StartGame();
+            DestroyNonCharacterVillainCards();
+
+            MoveAllCardsFromHandToDeck(gyrosaur);
+
+            var ttcs = new TurnTakerController[] { baron, gyrosaur, legacy, ra };
+            foreach (TurnTakerController ttc in ttcs)
+            {
+                SetHitPoints(ttc, 10);
+            }
+            QuickHPStorage(ttcs);
+
+            Card wipeout = PutOnDeck("Wipeout");
+            Card chase = PutInHand("AMerryChase");
+
+            UsePower(gyrosaur);
+            AssertInTrash(chase);
+            AssertInHand(wipeout);
+            QuickHPCheck(-2, 0, 0, 0);
+        }
+        [Test]
+        public void TestRenegadePowerDiscardImpossible()
+        {
+            SetupGameController("BaronBlade", "Cauldron.Gyrosaur/RenegadeGyrosaurCharacter", "Legacy", "Ra", "Megalopolis");
+            StartGame();
+            DestroyNonCharacterVillainCards();
+
+            MoveAllCardsFromHandToDeck(gyrosaur);
+
+            var ttcs = new TurnTakerController[] { baron, gyrosaur, legacy, ra };
+            foreach (TurnTakerController ttc in ttcs)
+            {
+                SetHitPoints(ttc, 10);
+            }
+            QuickHPStorage(ttcs);
+
+            Card wipeout = PutOnDeck("Wipeout");
+            Card chase = PutOnDeck("AMerryChase");
+
+            UsePower(gyrosaur);
+            AssertOnTopOfDeck(wipeout);
+            AssertInHand(chase);
+            QuickHPCheck(-2, 0, 0, 0);
+        }
+        [Test]
+        public void TestCaptainGyrosaurLoads()
+        {
+            SetupGameController("BaronBlade", "Cauldron.Gyrosaur/CaptainGyrosaurCharacter", "Megalopolis");
+
+            Assert.AreEqual(3, this.GameController.TurnTakerControllers.Count());
+
+            Assert.IsNotNull(gyrosaur);
+            Assert.IsInstanceOf(typeof(CaptainGyrosaurCharacterCardController), gyrosaur.CharacterCardController);
+
+            Assert.AreEqual(29, gyrosaur.CharacterCard.HitPoints);
         }
     }
 }
