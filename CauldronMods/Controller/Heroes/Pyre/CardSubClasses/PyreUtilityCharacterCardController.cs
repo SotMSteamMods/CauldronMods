f728f5da65000412218f659f1313c57ac8d74c96
LNBertolini <bertolini.lucas.n@gmail.com>
2021-01-23 17:51:01 -0600

    In-progress Pyre stuff

---
+++ CauldronMods/Controller/Heroes/Pyre/CardSubClasses/PyreUtilityCharacterCardController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+
+namespace Cauldron.Pyre
+{
+    public abstract class PyreUtilityCharacterCardController : HeroCharacterCardController
+    {
+        private PyreTurnTakerController PyreTTC
+        {
+            get
+            {
+                if (TurnTakerControllerWithoutReplacements is PyreTurnTakerController pyreTTC)
+                {
+                    return pyreTTC;
+                }
+                return null;
+            }
+        }
+        protected PyreUtilityCharacterCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+        }
+
+        public override void AddStartOfGameTriggers()
+        {
+            if(TurnTakerController is PyreTurnTakerController ttc)
+            {
+                ttc.MoveMarkersToSide();
+            }
+        }
+
+        protected bool IsIrradiated(Card c)
+        {
+            if(c != null && c.IsInHand)
+            {
+                return c.NextToLocation.Cards.Any((Card nextTo) => nextTo.Identifier == "IrradiatedMarker");
+            }
+            return false;
+        }
+
+        protected IEnumerator IrradiateCard(Card cardToIrradiate)
+        {
+            var marker = TurnTaker.GetAllCards(realCardsOnly: false).Where((Card c) => !c.IsRealCard && c.Location.IsOffToTheSide).FirstOrDefault();
+            if(marker != null && cardToIrradiate.Location.IsHand)
+            {
+                IEnumerator coroutine = GameController.MoveCard(DecisionMaker, marker, cardToIrradiate.NextToLocation, doesNotEnterPlay: true, cardSource: GetCardSource());
+                if (UseUnityCoroutines)
+                {
+                    yield return GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    GameController.ExhaustCoroutine(coroutine);
+                }
+
+                var irradiateEffect = new OnPhaseChangeStatusEffect(CardWithoutReplacements, nameof(DoNothing), $"{cardToIrradiate.Title} is irradiated.", new TriggerType[] { TriggerType.Hidden }, Card);
+                irradiateEffect.CardMovedExpiryCriteria.Card = cardToIrradiate;
+
+                coroutine = AddStatusEffect(irradiateEffect, false);
+                if (UseUnityCoroutines)
+                {
+                    yield return GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    GameController.ExhaustCoroutine(coroutine);
+                }
+
+                if(PyreTTC != null)
+                {
+                    PyreTTC.AddIrradiatedSpecialString(cardToIrradiate);
+                }
+            }
+            yield break;
+        }
+        protected IEnumerator ClearIrradiation(Card c)
+        {
+            yield break;
+        }
+    }
+}
--- CauldronMods/Controller/Heroes/Pyre/CharacterCards/PyreCharacterCardController.cs
+++ CauldronMods/Controller/Heroes/Pyre/CharacterCards/PyreCharacterCardController.cs
@@ -8,13 +8,57 @@ using Handelabra.Sentinels.Engine.Model;
 
 namespace Cauldron.Pyre
 {
-    public class PyreCharacterCardController : HeroCharacterCardController
+    public class PyreCharacterCardController : PyreUtilityCharacterCardController
     {
         public PyreCharacterCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
         }
         public override IEnumerator UsePower(int index = 0)
         {
+            int numPlayers = GetPowerNumeral(0, 1);
+            //"1 player draws a card. {PyreIrradiate} that card until it leaves their hand. 
+            var storedDraw = new List<DrawCardAction>();
+            var selectHero = new SelectTurnTakersDecision(GameController, DecisionMaker, new LinqTurnTakerCriteria(tt => tt.IsHero && !tt.IsIncapacitatedOrOutOfGame && GameController.CanDrawCards(FindHeroTurnTakerController(tt.ToHero()), GetCardSource())), SelectionType.DrawCard, numberOfTurnTakers: numPlayers, numberOfCards: 1, cardSource: GetCardSource());
+            IEnumerator coroutine = GameController.SelectTurnTakersAndDoAction(selectHero, DrawAndIrradiateDrawnCard, cardSource: GetCardSource());
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+
+            //Shuffle a cascade card from your trash into your deck."
+
+            yield break;
+        }
+
+        private IEnumerator DrawAndIrradiateDrawnCard(TurnTaker tt)
+        {
+            var drawStorage = new List<DrawCardAction>();
+            IEnumerator coroutine = DrawCard(tt.ToHero(), cardsDrawn: drawStorage);
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+
+            if(DidDrawCards(drawStorage))
+            {
+                coroutine = IrradiateCard(drawStorage.FirstOrDefault().DrawnCard);
+                if (UseUnityCoroutines)
+                {
+                    yield return GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    GameController.ExhaustCoroutine(coroutine);
+                }
+            }
             yield break;
         }
 
