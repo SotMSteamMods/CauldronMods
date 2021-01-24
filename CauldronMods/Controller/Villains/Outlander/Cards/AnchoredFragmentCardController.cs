c021d84f5ed7e483d772ad895c019cbff59b57a0
Jake <mgirman520@gmail.com>
2021-01-24 18:06:38 -0500

    Update AnchoredFragmentCardController.cs

--- CauldronMods/Controller/Villains/Outlander/Cards/AnchoredFragmentCardController.cs
+++ CauldronMods/Controller/Villains/Outlander/Cards/AnchoredFragmentCardController.cs
@@ -15,7 +15,44 @@ namespace Cauldron.Outlander
 
         }
 
-        //When this card enters play, {Outlander} deals the hero target with the highest HP 1 melee damage.
-        //At the start of the villain turn, if {Outlander} was not dealt at least {H} times 2 damage in the last round, destroy {H} hero ongoing and/or equipment cards.
+        public override IEnumerator Play()
+        {
+            //When this card enters play, {Outlander} deals the hero target with the highest HP 1 melee damage.
+            IEnumerator coroutine = base.DealDamageToHighestHP(base.CharacterCard, 1, (Card c) => c.IsHero, (Card c) => 1, DamageType.Melee);
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+
+        public override void AddTriggers()
+        {
+            //At the start of the villain turn, if {Outlander} was not dealt at least {H} times 2 damage in the last round, destroy {H} hero ongoing and/or equipment cards.
+            base.AddStartOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, this.MaybeDestroyCardsResponse, TriggerType.DestroyCard);
+        }
+
+        private IEnumerator MaybeDestroyCardsResponse(PhaseChangeAction action)
+        {
+            //...if {Outlander} was not dealt at least {H} times 2 damage in the last round, 
+            if (base.Journal.DealDamageEntriesThisTurn().Where((DealDamageJournalEntry entry) => entry.TargetCard == base.CharacterCard && entry.Amount > 0).Sum((DealDamageJournalEntry entry) => entry.Amount) < base.Game.H * 2)
+            {
+                //...destroy {H} hero ongoing and/or equipment cards.
+                IEnumerator coroutine = base.GameController.SelectAndDestroyCards(base.DecisionMaker, new LinqCardCriteria((Card c) => c.IsHero && (c.IsOngoing || base.IsEquipment(c))), base.Game.H, cardSource: base.GetCardSource());
+                if (UseUnityCoroutines)
+                {
+                    yield return GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    GameController.ExhaustCoroutine(coroutine);
+                }
+            }
+            yield break;
+        }
     }
 }
