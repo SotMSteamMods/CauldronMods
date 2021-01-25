cf0ad9d04508e9efe6f9af508f5500b277ed7e95
Jake <mgirman520@gmail.com>
2021-01-24 20:20:35 -0500

    Update ArchangelCardController.cs

--- CauldronMods/Controller/Villains/Outlander/Cards/ArchangelCardController.cs
+++ CauldronMods/Controller/Villains/Outlander/Cards/ArchangelCardController.cs
@@ -15,7 +15,30 @@ namespace Cauldron.Outlander
 
         }
 
-        //The first time {Outlander} is dealt 4 or more damage from a single source each turn, play the top card of the villain deck.
-        //At the end of the villain turn, {Outlander} deals each non-villain target irreducible 1 projectile damage.
+        protected const string OncePerTurn = "OncePerTurn";
+
+        public override void AddTriggers()
+        {
+            //The first time {Outlander} is dealt 4 or more damage from a single source each turn, play the top card of the villain deck.
+            base.AddTrigger<DealDamageAction>((DealDamageAction action) => action.Amount >= 4 && action.Target == base.CharacterCard, base.PlayTheTopCardOfTheVillainDeckResponse, TriggerType.PlayCard, TriggerTiming.After);
+
+            //At the end of the villain turn, {Outlander} deals each non-villain target irreducible 1 projectile damage.
+            base.AddEndOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, this.DealDamageResponse, TriggerType.DealDamage);
+        }
+
+        private IEnumerator DealDamageResponse(PhaseChangeAction action)
+        {
+            //...{Outlander} deals each non-villain target irreducible 1 projectile damage.
+            IEnumerator coroutine = base.DealDamage(base.CharacterCard, (Card c) => !base.IsVillain(c) && c.IsTarget, 1, DamageType.Projectile);
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
     }
 }
