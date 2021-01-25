a09e966a532cdbc77bc799c5a9fbf4fb731fec45
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-01-25 05:19:17 -0600

    Added base class

--- CauldronMods/Controller/Environments/DungeonsOfTerror/CardSubClasses/DungeonsOfTerrorUtilityCardController.cs
+++ CauldronMods/Controller/Environments/DungeonsOfTerror/CardSubClasses/DungeonsOfTerrorUtilityCardController.cs
@@ -13,5 +13,28 @@ namespace Cauldron.DungeonsOfTerror
         protected DungeonsOfTerrorUtilityCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
         }
+
+        public static readonly string FateKeyword = "fate";
+
+        protected bool IsFate(Card card)
+        {
+            return card.DoKeywordsContain(FateKeyword);
+        }
+
+        protected IEnumerator CheckForNumberOfFates(IEnumerable<Card> cardsToCheck, List<int> storedResults)
+        {
+            //eventually, maybe add in reveal logic of some kind?
+            int numFates = 0;
+            foreach(Card card in cardsToCheck)
+            {
+                if(IsFate(card))
+                {
+                    numFates++;
+                }
+            }
+
+            storedResults.Add(numFates);
+            yield break;
+        }
     }
 }
