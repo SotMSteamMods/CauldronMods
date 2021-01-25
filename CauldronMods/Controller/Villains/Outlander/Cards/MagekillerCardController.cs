0e3cc167e4ba67c1537e787cb77bc06fb2a16998
Jake <mgirman520@gmail.com>
2021-01-24 21:17:57 -0500

    Update MagekillerCardController.cs

--- CauldronMods/Controller/Villains/Outlander/Cards/MagekillerCardController.cs
+++ CauldronMods/Controller/Villains/Outlander/Cards/MagekillerCardController.cs
@@ -15,7 +15,43 @@ namespace Cauldron.Outlander
 
         }
 
-        //The first time a hero one-shot enters play each turn, {Outlander} deals the hero target with the highest HP 1 irreducible lightning damage.
-        //At the end of the villain turn, {Outlander} deals the hero target with the highest HP 3 melee damage.
+        public override void AddTriggers()
+        {
+            //The first time a hero one-shot enters play each turn, {Outlander} deals the hero target with the highest HP 1 irreducible lightning damage.
+            base.AddTrigger<CardEntersPlayAction>((CardEntersPlayAction action) => action.CardEnteringPlay.IsHero && action.CardEnteringPlay.IsOneShot, this.HeroOneShotResponse, TriggerType.DealDamage, TriggerTiming.After);
+
+            //At the end of the villain turn, {Outlander} deals the hero target with the highest HP 3 melee damage.
+            base.AddEndOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, this.DealDamageResponse, TriggerType.DealDamage);
+        }
+
+        private IEnumerator HeroOneShotResponse(CardEntersPlayAction action)
+        {
+            //...{Outlander} deals the hero target with the highest HP 1 irreducible lightning damage.
+            IEnumerator coroutine = base.DealDamageToHighestHP(base.CharacterCard, 1, (Card c) => c.IsHero && c.IsTarget, (Card c) => 1, DamageType.Lightning, true);
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+
+        private IEnumerator DealDamageResponse(PhaseChangeAction action)
+        {
+            //...{Outlander} deals the hero target with the highest HP 3 melee damage.
+            IEnumerator coroutine = base.DealDamage(base.CharacterCard, (Card c) => !c.IsHero && c.IsTarget, 3, DamageType.Melee);
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
     }
 }
