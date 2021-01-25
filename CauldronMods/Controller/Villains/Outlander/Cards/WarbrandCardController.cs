6b5d60558e8fdc374d527aabfa80239a126baf0f
Jake <mgirman520@gmail.com>
2021-01-24 21:57:58 -0500

    Update WarbrandCardController.cs

--- CauldronMods/Controller/Villains/Outlander/Cards/WarbrandCardController.cs
+++ CauldronMods/Controller/Villains/Outlander/Cards/WarbrandCardController.cs
@@ -12,10 +12,49 @@ namespace Cauldron.Outlander
     {
         public WarbrandCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
-
+            base.SpecialStringMaker.ShowIfElseSpecialString(() => base.HasBeenSetToTrueThisTurn(OncePerTurn), () => "Outlander has dealt damage this turn.", () => "Outlander has not dealt damage this turn.");
         }
 
-        //The first time {Outlander} deals damage each turn, he then deals the hero target with the highest HP 2 projectile damage.
-        //At the end of the villain turn, {Outlander} deals the 2 hero targets with the lowest HP 1 projectile damage each.
+        protected const string OncePerTurn = "OncePerTurn";
+
+        public override void AddTriggers()
+        {
+            //The first time {Outlander} deals damage each turn, he then deals the hero target with the highest HP 2 projectile damage.
+            base.AddTrigger<DealDamageAction>((DealDamageAction action) => !base.HasBeenSetToTrueThisTurn(OncePerTurn) && action.DamageSource.IsSameCard(base.CharacterCard) && action.DidDealDamage, this.OncePerTurnResponse, TriggerType.DealDamage, TriggerTiming.After);
+
+            //At the end of the villain turn, {Outlander} deals the 2 hero targets with the lowest HP 1 projectile damage each.
+            base.AddEndOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, this.DealDamageResponse, TriggerType.DealDamage);
+        }
+
+        private IEnumerator OncePerTurnResponse(DealDamageAction action)
+        {
+            base.SetCardPropertyToTrueIfRealAction(OncePerTurn);
+            //...he then deals the hero target with the highest HP 2 projectile damage.
+            IEnumerator coroutine = base.DealDamageToHighestHP(base.CharacterCard, 1, (Card c) => c.IsHero && c.IsTarget, (Card c) => 2, DamageType.Projectile);
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+
+        private IEnumerator DealDamageResponse(PhaseChangeAction action)
+        {
+            //...{Outlander} deals the 2 hero targets with the lowest HP 1 projectile damage each.
+            IEnumerator coroutine = base.DealDamageToLowestHP(base.CharacterCard, 1, (Card c) => c.IsHero && c.IsTarget, (Card c) => 1, DamageType.Projectile, numberOfTargets: 2);
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
     }
 }
