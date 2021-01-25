bd7358c820df75b44aa0e8546e404a1fc3df9883
Jake <mgirman520@gmail.com>
2021-01-24 21:13:04 -0500

    Update KnightsHatredCardController.cs

--- CauldronMods/Controller/Villains/Outlander/Cards/KnightsHatredCardController.cs
+++ CauldronMods/Controller/Villains/Outlander/Cards/KnightsHatredCardController.cs
@@ -15,8 +15,16 @@ namespace Cauldron.Outlander
 
         }
 
-        //Increase damage dealt by {Outlander} by 1.
-        //Reduce damage dealt to {Outlander} by 1.
-        //At the start of the villain turn, destroy this card.
+        public override void AddTriggers()
+        {
+            //Increase damage dealt by {Outlander} by 1.
+            base.AddIncreaseDamageTrigger((DealDamageAction action) => action.DamageSource.IsSameCard(base.CharacterCard), 1);
+
+            //Reduce damage dealt to {Outlander} by 1.
+            base.AddReduceDamageTrigger((Card c) => c == base.CharacterCard, 1);
+
+            //At the start of the villain turn, destroy this card.
+            base.AddStartOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, base.DestroyThisCardResponse, TriggerType.DestroySelf);
+        }
     }
 }
