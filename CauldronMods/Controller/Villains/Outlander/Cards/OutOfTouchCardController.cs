9da0026d276cae08cf3d94a42eeec3d95a5d0701
Jake <mgirman520@gmail.com>
2021-01-24 21:39:25 -0500

    Update OutOfTouchCardController

--- CauldronMods/Controller/Villains/Outlander/Cards/OutOfTouchCardController.cs
+++ CauldronMods/Controller/Villains/Outlander/Cards/OutOfTouchCardController.cs
@@ -15,7 +15,28 @@ namespace Cauldron.Outlander
 
         }
 
-        //When this card enters play, {Outlander} deals the non-villain target with the highest HP X+3 melee damage, where X is the number of Trace cards in play.
-        //Reduce all HP recovery by 1. At the start of the villain turn, destroy this card.
+        public override IEnumerator Play()
+        {
+            //When this card enters play, {Outlander} deals the non-villain target with the highest HP X+3 melee damage, where X is the number of Trace cards in play.
+            IEnumerator coroutine = base.DealDamageToHighestHP(base.CharacterCard, 1, (Card c) => !base.IsVillain(c), (Card c) => base.FindCardsWhere(new LinqCardCriteria((Card card) => base.IsTrace(card) && c.IsInPlayAndHasGameText)).Count() + 3, DamageType.Melee);
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+
+        public override void AddTriggers()
+        {
+            //Reduce all HP recovery by 1.
+            base.AddTrigger<GainHPAction>((GainHPAction action) => action.CardSource != null, (GainHPAction action) => base.GameController.IncreaseHPGain(action, 1, cardSource: base.GetCardSource()), TriggerType.IncreaseHPGain, TriggerTiming.Before);
+
+            //At the start of the villain turn, destroy this card.
+            base.AddStartOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, base.DestroyThisCardResponse, TriggerType.DestroySelf);
+        }
     }
 }
