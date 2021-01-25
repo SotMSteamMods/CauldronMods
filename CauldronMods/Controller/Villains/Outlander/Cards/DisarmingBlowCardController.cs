a90611efcfd8cf1d4fc715d4522f92f33e216823
Jake <mgirman520@gmail.com>
2021-01-24 21:01:03 -0500

    Update DisarmingBlowCardController.cs

--- CauldronMods/Controller/Villains/Outlander/Cards/DisarmingBlowCardController.cs
+++ CauldronMods/Controller/Villains/Outlander/Cards/DisarmingBlowCardController.cs
@@ -15,7 +15,26 @@ namespace Cauldron.Outlander
 
         }
 
-        //{Outlander} deals the 2 non-villain targets with the highest HP 3 melee damage each.
-        //Any hero damaged this way discards 1 card.
+        public override IEnumerator Play()
+        {
+            //{Outlander} deals the 2 non-villain targets with the highest HP 3 melee damage each.
+            List<DealDamageAction> damageActions = new List<DealDamageAction>();
+            IEnumerator coroutine = base.DealDamageToHighestHP(base.CharacterCard, 1, (Card c) => !base.IsVillain(c) && c.IsTarget, (Card c) => 3, DamageType.Melee, storedResults: damageActions);
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+
+        public override void AddTriggers()
+        {
+            //Any hero damaged this way discards 1 card.
+            base.AddTrigger<DealDamageAction>((DealDamageAction action) => action.CardSource == base.GetCardSource() && action.Target.IsHero && action.Target.IsCharacter && action.DidDealDamage, (DealDamageAction action) => base.GameController.SelectAndDiscardCard(base.FindHeroTurnTakerController(action.Target.Owner.ToHero())), TriggerType.DiscardCard, TriggerTiming.After);
+        }
     }
 }
