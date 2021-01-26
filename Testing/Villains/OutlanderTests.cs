0b8dd5b38b0d66f52955ebddb4d246229c7549c0
Jake <mgirman520@gmail.com>
2021-01-25 23:46:01 -0500

    Create OutlanderTests.cs

---
+++ Testing/Villains/OutlanderTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using Cauldron.Outlander;
+using Handelabra.Sentinels.UnitTest;
+using Handelabra.Sentinels.Engine.Model;
+using Handelabra.Sentinels.Engine.Controller;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CauldronTests
+{
+    [TestFixture()]
+    class OutlanderTests : CauldronBaseTest
+    {
+        [Test()]
+        public void TestOutlanderLoad()
+        {
+            SetupGameController("Cauldron.Outlander", "Haka", "Bunker", "TheScholar", "Megalopolis");
+
+            Assert.AreEqual(5, this.GameController.TurnTakerControllers.Count());
+
+            Assert.IsNotNull(outlander);
+            Assert.IsInstanceOf(typeof(OutlanderCharacterCardController), outlander.CharacterCardController);
+
+            foreach (Card card in outlander.TurnTaker.GetAllCards())
+            {
+                CardController cc = GetCardController(card);
+                Assert.IsTrue(cc.GetType() != typeof(CardController), $"{card.Identifier} is does not have a CardController");
+            }
+
+            Assert.AreEqual(20, outlander.CharacterCard.HitPoints);
+        }
+
+        [Test()]
+        public void TestOutlanderDecklist()
+        {
+            SetupGameController("Cauldron.Outlander", "Haka", "Bunker", "TheScholar", "Megalopolis");
+
+            AssertHasKeyword("trace", new string[] { "Archangel", "Crusader", "Dragonborn", "Magekiller", "Warbrand" });
+
+            AssertHasKeyword("one-shot", new string[] { "DisarmingBlow", "RiftbladeStrikes", "TransdimensionalOnslaught" });
+
+            AssertHasKeyword("ongoing", new string[] { "AnchoredFragment", "DimensionalInsinuation", "KnightsHatred", "OutOfTouch" });
+        }
+    }
+}
