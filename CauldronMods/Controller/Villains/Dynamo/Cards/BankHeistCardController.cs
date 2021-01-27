a81106da04235f880f0af06e12544598aec4ecf5
Jake <mgirman520@gmail.com>
2021-01-27 13:30:37 -0500

    Update BankHeistCardController.cs

--- CauldronMods/Controller/Villains/Dynamo/Cards/BankHeistCardController.cs
+++ CauldronMods/Controller/Villains/Dynamo/Cards/BankHeistCardController.cs
@@ -15,6 +15,42 @@ namespace Cauldron.Dynamo
 
         }
 
-        //At the end of the villain turn, each player may discard 1 card. If fewer than {H - 1} cards were discarded this way, discard the top card of the villain deck.
+        public override void AddTriggers()
+        {
+            //At the end of the villain turn, each player may discard 1 card. If fewer than {H - 1} cards were discarded this way, discard the top card of the villain deck.
+            base.AddEndOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, this.DiscardCardsResponse, TriggerType.DiscardCard);
+        }
+
+        private IEnumerator DiscardCardsResponse(PhaseChangeAction action)
+        {
+            //...each player may discard 1 card. 
+            List<DiscardCardAction> storedResultsDiscard = new List<DiscardCardAction>();
+            IEnumerator coroutine = base.GameController.EachPlayerDiscardsCards(0, 1, storedResultsDiscard, cardSource: base.GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+
+            //If fewer than {H - 1} cards were discarded this way...
+            if (base.GetNumberOfCardsDiscarded(storedResultsDiscard) < base.Game.H - 1)
+            {
+                //...discard the top card of the villain deck.
+                List<MoveCardAction> storedResult = new List<MoveCardAction>();
+                coroutine = base.GameController.DiscardTopCard(base.TurnTaker.Deck, storedResult, cardSource: base.GetCardSource());
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+            }
+            yield break;
+        }
     }
 }
