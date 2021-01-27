cc36d9a0f66bf4eb173fe29fe8d09c834cb364ee
Jake <mgirman520@gmail.com>
2021-01-27 14:36:00 -0500

    Update HeresThePlanCardController.cs

--- CauldronMods/Controller/Villains/Dynamo/Cards/HeresThePlanCardController.cs
+++ CauldronMods/Controller/Villains/Dynamo/Cards/HeresThePlanCardController.cs
@@ -15,7 +15,48 @@ namespace Cauldron.Dynamo
 
         }
 
-        //Reveal cards from the top of the villain deck until a Plot is revealed. Put it into play and shuffle the other revealed cards back into the villain deck.
-        //The villain target with the highest HP deals each hero target {H - 1} melee damage.
+        public override void Play()
+        {
+            //Reveal cards from the top of the villain deck until a Plot is revealed. Put it into play and shuffle the other revealed cards back into the villain deck.
+            IEnumerator coroutine = base.RevealCards_MoveMatching_ReturnNonMatchingCards(base.TurnTakerController, base.TurnTaker.Deck, false, true, false, new LinqCardCriteria((Card c) => base.IsPlot(c)), 1);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+
+            //The villain target with the highest HP...
+            IEnumerable<Card> highestVillains = base.GameController.FindAllTargetsWithHighestHitPoints(1, (Card c) => base.IsVillainTarget(c), base.GetCardSource());
+            Card highestVillain = highestVillains.FirstOrDefault();
+            if (highestVillains.Count() > 1)
+            {
+                List<SelectCardDecision> storedResults = new List<SelectCardDecision>();
+                coroutine = base.GameController.SelectCardAndStoreResults(base.DecisionMaker, SelectionType.CardToDealDamage, new LinqCardCriteria((Card c) => highestVillains.Contains(c)), storedResults, false, cardSource: base.GetCardSource());
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+                highestVillain = storedResults.FirstOrDefault().SelectedCard;
+            }
+
+            //...deals each hero target {H - 1} melee damage.
+            coroutine = base.DealDamage(highestVillain, (Card c) => c.IsHero && c.IsTarget, base.Game.H - 1, DamageType.Melee);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
     }
 }
