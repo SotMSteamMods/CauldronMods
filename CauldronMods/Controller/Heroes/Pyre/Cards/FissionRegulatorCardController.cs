b6a0fa76f79e48f1e2d5723cdd7c4407198e148b
LNBertolini <bertolini.lucas.n@gmail.com>
2021-01-28 08:44:40 -0600

    Fission Regulator controller

--- CauldronMods/Controller/Heroes/Pyre/Cards/FissionRegulatorCardController.cs
+++ CauldronMods/Controller/Heroes/Pyre/Cards/FissionRegulatorCardController.cs
@@ -17,10 +17,112 @@ namespace Cauldron.Pyre
         public override void AddTriggers()
         {
             //"When Rogue Fission Cascade would enter play, instead discard it and draw a card. Then put a Cascade card from your trash on top of your deck and destroy this card.",
+            AddTrigger((CardEntersPlayAction cep) => cep.CardEnteringPlay != null && cep.CardEnteringPlay.Identifier == "RogueFissionCascade", PreventCascadeResponse, TriggerType.CancelAction, TriggerTiming.Before);
+        }
+        private IEnumerator PreventCascadeResponse(CardEntersPlayAction cep)
+        {
+            //Instead,
+            IEnumerator coroutine = CancelAction(cep);
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+
+            //discard it...
+            coroutine = GameController.MoveCard(DecisionMaker, cep.CardEnteringPlay, cep.CardEnteringPlay.NativeTrash, isDiscard: true, cardSource: GetCardSource());
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+
+            //and draw a card.
+            coroutine = DrawCard();
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+
+            //Then put a Cascade card from your trash on top of your deck...
+            var cascadeInTrash = FindCardsWhere((Card c) => c.Location == TurnTaker.Trash && IsCascade(c)).FirstOrDefault();
+            if(cascadeInTrash != null)
+            {
+                coroutine = GameController.MoveCard(DecisionMaker, cascadeInTrash, DecisionMaker.TurnTaker.Deck, cardSource: GetCardSource());
+                if (UseUnityCoroutines)
+                {
+                    yield return GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    GameController.ExhaustCoroutine(coroutine);
+                }
+            }
+
+            //and destroy this card.
+            coroutine = DestroyThisCardResponse(cep);
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
         }
         public override IEnumerator UsePower(int index = 0)
         {
             //"Each player selects 1 non-{PyreIrradiate} card in their hand. {PyreIrradiate} those cards until they leave their hands."
+            IEnumerator coroutine = GameController.SelectTurnTakersAndDoAction(DecisionMaker, new LinqTurnTakerCriteria(tt => tt.IsHero && !tt.IsIncapacitatedOrOutOfGame && GameController.IsTurnTakerVisibleToCardSource(tt, GetCardSource())), SelectionType.CardFromHand, SelectAndIrradiateCardInHand, allowAutoDecide: true, cardSource: GetCardSource());
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+            yield break;
+        }
+
+        private IEnumerator SelectAndIrradiateCardInHand(TurnTaker tt)
+        {
+            var heroTTC = FindHeroTurnTakerController(tt.ToHero());
+            var storedCard = new List<SelectCardDecision>();
+            IEnumerator coroutine = GameController.SelectCardAndStoreResults(heroTTC, SelectionType.CardFromHand, new LinqCardCriteria((Card c) => c.Location == heroTTC.HeroTurnTaker.Hand && !IsIrradiated(c), "non-irradiated"), storedCard, false, cardSource: GetCardSource());
+            if (UseUnityCoroutines)
+            {
+                yield return GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                GameController.ExhaustCoroutine(coroutine);
+            }
+
+            if(DidSelectCard(storedCard))
+            {
+                var toIrradiate = GetSelectedCard(storedCard);
+                coroutine = IrradiateCard(toIrradiate);
+                if (UseUnityCoroutines)
+                {
+                    yield return GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    GameController.ExhaustCoroutine(coroutine);
+                }
+            }
             yield break;
         }
     }
