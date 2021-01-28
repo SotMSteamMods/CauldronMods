4b30a67933e07ad59151548a733a8fef96059a6b
Jake <mgirman520@gmail.com>
2021-01-27 19:21:24 -0500

    Update StrangleholdCardController.cs

--- CauldronMods/Controller/Villains/Dynamo/Cards/StrangleholdCardController.cs
+++ CauldronMods/Controller/Villains/Dynamo/Cards/StrangleholdCardController.cs
@@ -15,7 +15,54 @@ namespace Cauldron.Dynamo
 
         }
 
-        //If Python is in play, destroy {H} hero ongoing and/or equipment cards.
-        //Otherwise, search the villain deck and trash for Python and put him into play. If you searched the villain deck, shuffle it.
+        public override IEnumerator Play()
+        {
+            IEnumerator coroutine;
+            //If Python is in play...
+            if (base.FindPython().IsInPlayAndHasGameText)
+            {
+                //...destroy {H} hero ongoing and/or equipment cards.
+                coroutine = base.GameController.SelectAndDestroyCards(base.DecisionMaker, new LinqCardCriteria((Card c) => c.IsHero && (base.IsEquipment(c) || c.IsOngoing)), base.Game.H, cardSource: base.GetCardSource());
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+            }
+            else
+            {
+                //Otherwise, search the villain deck and trash for Python and put him into play.
+                Location pythonLoc = base.FindPython().Location;
+                if (pythonLoc.IsVillain && (pythonLoc.IsDeck || pythonLoc.IsTrash))
+                {
+                    coroutine = base.GameController.PlayCard(base.TurnTakerController, base.FindPython(), true, cardSource: base.GetCardSource());
+                    if (base.UseUnityCoroutines)
+                    {
+                        yield return base.GameController.StartCoroutine(coroutine);
+                    }
+                    else
+                    {
+                        base.GameController.ExhaustCoroutine(coroutine);
+                    }
+
+                    //If you searched the villain deck, shuffle it.
+                    if (pythonLoc.IsDeck)
+                    {
+                        coroutine = base.ShuffleDeck(base.DecisionMaker, base.TurnTaker.Deck);
+                        if (base.UseUnityCoroutines)
+                        {
+                            yield return base.GameController.StartCoroutine(coroutine);
+                        }
+                        else
+                        {
+                            base.GameController.ExhaustCoroutine(coroutine);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
