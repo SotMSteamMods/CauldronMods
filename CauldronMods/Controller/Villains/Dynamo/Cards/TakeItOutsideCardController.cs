da9d2d031013e583ac40ad3ca16f09ae4c9a101d
Jake <mgirman520@gmail.com>
2021-01-27 19:47:24 -0500

    Update TakeItOutsideCardController.cs

--- CauldronMods/Controller/Villains/Dynamo/Cards/TakeItOutsideCardController.cs
+++ CauldronMods/Controller/Villains/Dynamo/Cards/TakeItOutsideCardController.cs
@@ -15,7 +15,63 @@ namespace Cauldron.Dynamo
 
         }
 
-        //{Dynamo} deals the hero target with the highest HP 5 energy damage. If a hero target takes damage this way, destroy 1 environment card.
-        //{Dynamo} deals each other hero target 1 sonic damage.
+        public override IEnumerator Play()
+        {
+            //{Dynamo} deals the hero target with the highest HP 5 energy damage.
+            List<DealDamageAction> dealDamageActions = new List<DealDamageAction>();
+            IEnumerator coroutine = base.DealDamageToHighestHP(base.CharacterCard, 1, (Card c) => c.IsHero && c.IsTarget, (Card c) => 5, DamageType.Energy, storedResults: dealDamageActions);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+
+            if (dealDamageActions.Any())
+            {
+                DealDamageAction damageAction = dealDamageActions.FirstOrDefault();
+                //If a hero target takes damage this way...
+                if (damageAction.Target.IsHero && damageAction.Amount > 0)
+                {
+                    //...destroy 1 environment card.
+                    coroutine = base.GameController.SelectAndDestroyCard(base.DecisionMaker, new LinqCardCriteria((Card c) => c.IsEnvironment), false);
+                    if (base.UseUnityCoroutines)
+                    {
+                        yield return base.GameController.StartCoroutine(coroutine);
+                    }
+                    else
+                    {
+                        base.GameController.ExhaustCoroutine(coroutine);
+                    }
+                }
+
+                //{Dynamo} deals each other hero target 1 sonic damage.
+                coroutine = base.DealDamage(base.CharacterCard, (Card c) => c.IsHero && c.IsTarget && c != damageAction.Target, (Card c) => 1, DamageType.Sonic);
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+            }
+            else
+            {
+                //If somehow no targets were hit, go after all hero targets
+                coroutine = base.DealDamage(base.CharacterCard, (Card c) => c.IsHero && c.IsTarget, (Card c) => 1, DamageType.Sonic);
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(coroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(coroutine);
+                }
+            }
+            yield break;
+        }
     }
 }
