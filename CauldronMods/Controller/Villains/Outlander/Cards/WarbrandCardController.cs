42b17c8985ef016f6f1d2fd54e70df3f165abc8f
MSpekkio <onlytelford@yahoo.com>
2021-01-31 12:50:06 -0500

    updated once per turn key to be unique

--- CauldronMods/Controller/Villains/Outlander/Cards/WarbrandCardController.cs
+++ CauldronMods/Controller/Villains/Outlander/Cards/WarbrandCardController.cs
@@ -8,30 +8,30 @@ using Handelabra.Sentinels.Engine.Model;
 
 namespace Cauldron.Outlander
 {
-    public class WarbrandCardController : OutlanderUtilityCardController
+    public class WarbrandCardController : OutlanderTraceCardController
     {
         public WarbrandCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
-            base.SpecialStringMaker.ShowIfElseSpecialString(() => base.HasBeenSetToTrueThisTurn(OncePerTurn), () => "Outlander has dealt damage this turn.", () => "Outlander has not dealt damage this turn.");
-            base.SpecialStringMaker.ShowHeroTargetWithLowestHP();
+            SpecialStringMaker.ShowIfElseSpecialString(() => HasBeenSetToTrueThisTurn(OncePerTurn), () => "Outlander has dealt damage this turn.", () => "Outlander has not dealt damage this turn.");
+            SpecialStringMaker.ShowHeroTargetWithLowestHP();
         }
 
-        protected const string OncePerTurn = "OncePerTurn";
+        protected const string OncePerTurn = "OutlanderWarbrandOncePerTurn";
 
         public override void AddTriggers()
         {
             //The first time {Outlander} deals damage each turn, he then deals the hero target with the highest HP 2 projectile damage.
-            base.AddTrigger<DealDamageAction>((DealDamageAction action) => !base.HasBeenSetToTrueThisTurn(OncePerTurn) && action.DamageSource.IsSameCard(base.CharacterCard) && action.DidDealDamage, this.OncePerTurnResponse, TriggerType.DealDamage, TriggerTiming.After);
+            AddTrigger<DealDamageAction>((DealDamageAction action) => !HasBeenSetToTrueThisTurn(OncePerTurn) && action.DamageSource.IsSameCard(CharacterCard) && action.DidDealDamage, OncePerTurnResponse, TriggerType.DealDamage, TriggerTiming.After);
 
             //At the end of the villain turn, {Outlander} deals the 2 hero targets with the lowest HP 1 projectile damage each.
-            base.AddEndOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, this.DealDamageResponse, TriggerType.DealDamage);
+            AddEndOfTurnTrigger((TurnTaker tt) => tt == TurnTaker, DealDamageResponse, TriggerType.DealDamage);
         }
 
         private IEnumerator OncePerTurnResponse(DealDamageAction action)
         {
-            base.SetCardPropertyToTrueIfRealAction(OncePerTurn);
+            SetCardPropertyToTrueIfRealAction(OncePerTurn);
             //...he then deals the hero target with the highest HP 2 projectile damage.
-            IEnumerator coroutine = base.DealDamageToHighestHP(base.CharacterCard, 1, (Card c) => c.IsHero && c.IsTarget, (Card c) => 2, DamageType.Projectile);
+            IEnumerator coroutine = base.DealDamageToHighestHP(CharacterCard, 1, (Card c) => c.IsHero && c.IsTarget, (Card c) => 2, DamageType.Projectile);
             if (UseUnityCoroutines)
             {
                 yield return GameController.StartCoroutine(coroutine);
@@ -40,13 +40,12 @@ namespace Cauldron.Outlander
             {
                 GameController.ExhaustCoroutine(coroutine);
             }
-            yield break;
         }
 
         private IEnumerator DealDamageResponse(PhaseChangeAction action)
         {
             //...{Outlander} deals the 2 hero targets with the lowest HP 1 projectile damage each.
-            IEnumerator coroutine = base.DealDamageToLowestHP(base.CharacterCard, 1, (Card c) => c.IsHero && c.IsTarget, (Card c) => 1, DamageType.Projectile, numberOfTargets: 2);
+            IEnumerator coroutine = DealDamageToLowestHP(CharacterCard, 1, (Card c) => c.IsHero && c.IsTarget, (Card c) => 1, DamageType.Projectile, numberOfTargets: 2);
             if (UseUnityCoroutines)
             {
                 yield return GameController.StartCoroutine(coroutine);
@@ -55,7 +54,6 @@ namespace Cauldron.Outlander
             {
                 GameController.ExhaustCoroutine(coroutine);
             }
-            yield break;
         }
     }
 }
