05b45203f1521b4b9fc7da272ccd3191d0b142aa
MSpekkio <onlytelford@yahoo.com>
2021-01-31 12:39:39 -0500

    added trace base class with special string

---
+++ CauldronMods/Controller/Villains/Outlander/CardSubClasses/OutlanderTraceCardController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+
+namespace Cauldron.Outlander
+{
+    public abstract class OutlanderTraceCardController : OutlanderUtilityCardController
+    {
+        protected OutlanderTraceCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+            SpecialStringMaker.ShowSpecialString(() => "This card is indestructible.");
+        }
+    }
+}
