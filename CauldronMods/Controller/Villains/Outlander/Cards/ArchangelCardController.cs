23e91b5ac8c9969f214e95ca235d3a50569bb391
MSpekkio <onlytelford@yahoo.com>
2021-01-31 12:59:18 -0500

    cleanup

--- CauldronMods/Controller/Villains/Outlander/Cards/ArchangelCardController.cs
+++ CauldronMods/Controller/Villains/Outlander/Cards/ArchangelCardController.cs
@@ -8,29 +8,29 @@ using Handelabra.Sentinels.Engine.Model;
 
 namespace Cauldron.Outlander
 {
-    public class ArchangelCardController : OutlanderUtilityCardController
+    public class ArchangelCardController : OutlanderTraceCardController
     {
         public ArchangelCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
-            base.SpecialStringMaker.ShowIfElseSpecialString(() => base.HasBeenSetToTrueThisTurn(OncePerTurn), () => "Outlander has been dealt 4 or more damage from a single source this turn.", () => "Outlander has not been dealt 4 or more damage from a single source this turn.");
+            SpecialStringMaker.ShowIfElseSpecialString(() => HasBeenSetToTrueThisTurn(OncePerTurn), () => "Outlander has been dealt 4 or more damage from a single source this turn.", () => "Outlander has not been dealt 4 or more damage from a single source this turn.");
         }
 
-        protected const string OncePerTurn = "OncePerTurn";
+        protected const string OncePerTurn = "OutlanderArchangelOncePerTurn";
 
         public override void AddTriggers()
         {
             //The first time {Outlander} is dealt 4 or more damage from a single source each turn, play the top card of the villain deck.
-            base.AddTrigger<DealDamageAction>((DealDamageAction action) => !base.HasBeenSetToTrueThisTurn(OncePerTurn) && action.Amount >= 4 && action.Target == base.CharacterCard, this.OncePerTurnResponse, TriggerType.PlayCard, TriggerTiming.After);
+            AddTrigger<DealDamageAction>((DealDamageAction action) => !HasBeenSetToTrueThisTurn(OncePerTurn) && action.Amount >= 4 && action.Target == CharacterCard, OncePerTurnResponse, TriggerType.PlayCard, TriggerTiming.After);
 
             //At the end of the villain turn, {Outlander} deals each non-villain target irreducible 1 projectile damage.
-            base.AddEndOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, this.DealDamageResponse, TriggerType.DealDamage);
+            AddEndOfTurnTrigger((TurnTaker tt) => tt == TurnTaker, DealDamageResponse, TriggerType.DealDamage);
         }
 
         private IEnumerator OncePerTurnResponse(DealDamageAction action)
         {
-            base.SetCardPropertyToTrueIfRealAction(OncePerTurn);
+            SetCardPropertyToTrueIfRealAction(OncePerTurn);
             //...play the top card of the villain deck.
-            IEnumerator coroutine = base.PlayTheTopCardOfTheVillainDeckResponse(action);
+            IEnumerator coroutine = PlayTheTopCardOfTheVillainDeckWithMessageResponse(action);
             if (UseUnityCoroutines)
             {
                 yield return GameController.StartCoroutine(coroutine);
@@ -39,13 +39,12 @@ namespace Cauldron.Outlander
             {
                 GameController.ExhaustCoroutine(coroutine);
             }
-            yield break;
         }
 
         private IEnumerator DealDamageResponse(PhaseChangeAction action)
         {
             //...{Outlander} deals each non-villain target irreducible 1 projectile damage.
-            IEnumerator coroutine = base.DealDamage(base.CharacterCard, (Card c) => !base.IsVillain(c) && c.IsTarget, 1, DamageType.Projectile, true);
+            IEnumerator coroutine = DealDamage(CharacterCard, (Card c) => !IsVillain(c) && c.IsTarget, 1, DamageType.Projectile, isIrreducible: true);
             if (UseUnityCoroutines)
             {
                 yield return GameController.StartCoroutine(coroutine);
@@ -54,7 +53,6 @@ namespace Cauldron.Outlander
             {
                 GameController.ExhaustCoroutine(coroutine);
             }
-            yield break;
         }
     }
 }
