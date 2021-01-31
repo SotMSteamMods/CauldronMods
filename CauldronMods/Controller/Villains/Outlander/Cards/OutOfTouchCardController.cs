5142854ccbca8de7073363aef506d4ed492ab46d
MSpekkio <onlytelford@yahoo.com>
2021-01-31 13:55:09 -0500

    added special strings

--- CauldronMods/Controller/Villains/Outlander/Cards/OutOfTouchCardController.cs
+++ CauldronMods/Controller/Villains/Outlander/Cards/OutOfTouchCardController.cs
@@ -12,13 +12,14 @@ namespace Cauldron.Outlander
     {
         public OutOfTouchCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
-            base.SpecialStringMaker.ShowNonVillainTargetWithHighestHP();
+            SpecialStringMaker.ShowNonVillainTargetWithHighestHP();
+            SpecialStringMaker.ShowNumberOfCardsInPlay(new LinqCardCriteria(c => IsTrace(c), "trace"));
         }
 
         public override IEnumerator Play()
         {
             //When this card enters play, {Outlander} deals the non-villain target with the highest HP X+3 melee damage, where X is the number of Trace cards in play.
-            IEnumerator coroutine = base.DealDamageToHighestHP(base.CharacterCard, 1, (Card c) => !base.IsVillain(c), (Card c) => base.FindCardsWhere(new LinqCardCriteria((Card card) => base.IsTrace(card) && card.IsInPlayAndHasGameText)).Count() + 3, DamageType.Melee);
+            IEnumerator coroutine = DealDamageToHighestHP(CharacterCard, 1, (Card c) => !IsVillain(c), (Card c) => FindCardsWhere(new LinqCardCriteria((Card card) => IsTrace(card) && card.IsInPlayAndHasGameText)).Count() + 3, DamageType.Melee);
             if (UseUnityCoroutines)
             {
                 yield return GameController.StartCoroutine(coroutine);
@@ -27,16 +28,15 @@ namespace Cauldron.Outlander
             {
                 GameController.ExhaustCoroutine(coroutine);
             }
-            yield break;
         }
 
         public override void AddTriggers()
         {
             //Reduce all HP recovery by 1.
-            base.AddTrigger<GainHPAction>((GainHPAction action) => action.CardSource != null, (GainHPAction action) => base.GameController.ReduceHPGain(action, 1, cardSource: base.GetCardSource()), TriggerType.IncreaseHPGain, TriggerTiming.Before);
+            AddTrigger<GainHPAction>((GainHPAction action) => action.CardSource != null, (GainHPAction action) => GameController.ReduceHPGain(action, 1, cardSource: GetCardSource()), TriggerType.IncreaseHPGain, TriggerTiming.Before);
 
             //At the start of the villain turn, destroy this card.
-            base.AddStartOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, base.DestroyThisCardResponse, TriggerType.DestroySelf);
+            AddStartOfTurnTrigger((TurnTaker tt) => tt == TurnTaker, DestroyThisCardResponse, TriggerType.DestroySelf);
         }
     }
 }
