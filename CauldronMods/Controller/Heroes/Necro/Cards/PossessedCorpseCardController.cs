6b71d556961d70acb652e770f1d69a651de4427e
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-02-06 00:38:34 -0600

    Added Possessed Corpse SpecialString

--- CauldronMods/Controller/Heroes/Necro/Cards/PossessedCorpseCardController.cs
+++ CauldronMods/Controller/Heroes/Necro/Cards/PossessedCorpseCardController.cs
@@ -9,11 +9,13 @@ namespace Cauldron.Necro
     {
         public PossessedCorpseCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController, 2)
         {
+            SpecialStringMaker.ShowSpecialString(() => BuildUndeadSpecialString(highestOrLowest: false, cardCriteria: new LinqCardCriteria(c => !this.IsUndead(c) && IsHeroConsidering1929(c), $"non-undead {HeroStringConsidering1929} target", useCardsSuffix: false)));
+
         }
 
         public override void AddTriggers()
         {
-            //At the end of your turn, this card deals the non-undead target with the second lowest HP 2 toxic damage.
+            //At the end of your turn, this card deals the non-undead target with the lowest HP 2 toxic damage.
             base.AddEndOfTurnTrigger(tt => tt == TurnTaker, p => base.DealDamageToLowestHP(Card, 1, c => !this.IsUndead(c) && IsHeroConsidering1929(c), c => 2, DamageType.Infernal), TriggerType.DealDamage);
         }
     }
