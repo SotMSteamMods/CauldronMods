b2c8e74de09dbc0d100a63a22687f4d0e548bcae
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-03-02 21:57:19 -0600

    echonavigation cleanup

--- CauldronMods/Controller/Heroes/Cricket/Cards/EchonavigationCardController.cs
+++ CauldronMods/Controller/Heroes/Cricket/Cards/EchonavigationCardController.cs
@@ -16,7 +16,6 @@ namespace Cauldron.Cricket
         {
             //Reveal the top card of each deck. You may replace or discard each card.",
             IEnumerator coroutine = GameController.SelectLocationsAndDoAction(DecisionMaker, SelectionType.RevealTopCardOfDeck, l => l.IsDeck && !l.OwnerTurnTaker.IsIncapacitatedOrOutOfGame && l.IsRealDeck, (Location loc) => this.RevealCard_DiscardItOrPutItOnDeck(base.HeroTurnTakerController, base.FindTurnTakerController(loc.OwnerTurnTaker), loc, false), cardSource: GetCardSource());
-            //IEnumerator coroutine = base.GameController.SelectTurnTakersAndDoAction(base.HeroTurnTakerController, new LinqTurnTakerCriteria((TurnTaker tt) => true), SelectionType.RevealTopCardOfDeck, (TurnTaker tt) => this.RevealCard_DiscardItOrPutItOnDeck(base.HeroTurnTakerController, base.FindTurnTakerController(tt), tt.Deck, false), allowAutoDecide: true, cardSource: base.GetCardSource());
             if (base.UseUnityCoroutines)
             {
                 yield return base.GameController.StartCoroutine(coroutine);
