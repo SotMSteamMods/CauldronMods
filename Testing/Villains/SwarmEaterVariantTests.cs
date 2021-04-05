ec611ed501291938f7e035f2c32177c5b7871036
LNBertolini <bertolini.lucas.n@gmail.com>
2021-04-04 20:04:40 -0500

    Variant challenge test

--- Testing/Villains/SwarmEaterVariantTests.cs
+++ Testing/Villains/SwarmEaterVariantTests.cs
@@ -291,5 +291,54 @@ namespace CauldronTests
             DealDamage(jumper, legacy, 1, DamageType.Melee);
             QuickHPCheck(-2);
         }
+        [Test()]
+        public void TestChallengeMode()
+        {
+            SetupGameController(new string[] { "Cauldron.SwarmEater/DistributedHivemindSwarmEaterCharacter", "Legacy", "Haka", "Ra", "Megalopolis" }, challenge: true);
+            StartGame();
+            DestroyNonCharacterVillainCards();
+
+            Card subterran = PutOnDeck("SubterranAug");
+            Card jumper = PlayCard("JumperAug");
+            AssertUnderCard(jumper, subterran);
+
+            //Check that Subterran Aug's first time each turn is once-per-target-per-turn
+            QuickHPStorage(swarm.CharacterCard, jumper);
+            DealDamage(legacy, swarm, 2, DamageType.Melee);
+            QuickHPCheck(-1, 0);
+            DealDamage(legacy, swarm, 2, DamageType.Melee);
+            QuickHPCheck(-2, 0);
+
+            DealDamage(legacy, swarm, 2, DamageType.Melee);
+            QuickHPCheck(0, 0);
+            DealDamage(legacy, swarm, 2, DamageType.Melee);
+            QuickHPCheck(0, -1);
+
+            DestroyCard(jumper);
+
+            Card speed = PutOnDeck("SpeedAug");
+            Card lasher = PlayCard("LasherAug");
+
+            Card fire = PutOnDeck("FireAug");
+            Card blade = PlayCard("BladeAug");
+
+            DecisionSelectTurnTaker = legacy.TurnTaker;
+            QuickHPStorage(legacy);
+            QuickHandStorage(legacy);
+            SetHitPoints(legacy, 30);
+            SetHitPoints(haka, 10);
+            SetHitPoints(unity, 10);
+
+            //Lasher should deal 3 + 1 damage at EOT, Blade should deal 2
+            //Fire Aug's absorb should go off twice, causing Legacy to discard two cards
+
+            GoToEndOfTurn();
+            QuickHPCheck(-6);
+            QuickHandCheck(-2);
+
+            //Also Swarm Eater's damage should be boosted by the Speed Aug absorb
+            DealDamage(swarm, legacy, 1, DamageType.Melee);
+            QuickHPCheck(-2);
+        }
     }
 }
