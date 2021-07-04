cc55e1858608928c8c293b0bf6ea0e0039d1e4e2
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-07-04 12:05:49 -0500

    added fix based on n202015226ac's suggestion

--- CauldronMods/Controller/Heroes/Starlight/Cards/NightloreArmorCardController.cs
+++ CauldronMods/Controller/Heroes/Starlight/Cards/NightloreArmorCardController.cs
@@ -14,6 +14,10 @@ namespace Cauldron.Starlight
             SpecialStringMaker.ShowNumberOfCardsInPlay(new LinqCardCriteria((Card c) => IsConstellation(c), "constellation"));
         }
 
+        public readonly string HasConstellationBeenDestroyed = "HasConstellationBeenDestroyed";
+        public readonly string HasSaidNoToNightloreArmor = "HasSaidNoToNightloreArmor";
+        public readonly string CurrentActionGUID = "CurrentActionGUID";
+
         public override void AddTriggers()
         {
             //"Whenever damage would be dealt to another hero target, you may destroy a constellation card in play to prevent that damage."
@@ -25,51 +29,84 @@ namespace Cauldron.Starlight
                         TriggerType.WouldBeDealtDamage,
                         TriggerType.CancelAction
                     },
-                isActionOptional: true,
                 timing: TriggerTiming.Before);
         }
 
         private IEnumerator DestroyConstellationToPreventDamage(DealDamageAction dd)
         {
-            var constellationsInPlay = FindCardsWhere(IsConstellationInPlay);
-            if (constellationsInPlay.Count() == 0)
+            if (IsPropertyTrue(HasSaidNoToNightloreArmor) && dd.InstanceIdentifier.GetHashCode() + dd.Target.GetHashCode() == GetCardPropertyJournalEntryInteger(CurrentActionGUID).Value)
             {
-                //don't bother player with trigger they can't do anything about
+                if(!dd.IsPretend)
+                {
+                    SetCardProperty(HasSaidNoToNightloreArmor, false);
+                    SetCardProperty(CurrentActionGUID, -1);
+                }
                 yield break;
             }
+            if (!IsPropertyTrue(HasConstellationBeenDestroyed))
+            {
+                var constellationsInPlay = FindCardsWhere(IsConstellationInPlay);
+                if (constellationsInPlay.Count() == 0)
+                {
+                    //don't bother player with trigger they can't do anything about
+                    yield break;
+                }
 
-            //"...you may..."
-            List<YesNoCardDecision> yesNoDecision = new List<YesNoCardDecision> { };
+                //"...you may..."
+                List<YesNoCardDecision> yesNoDecision = new List<YesNoCardDecision> { };
 
-            //What does the associatedCards argument actually do here? Should/should not be passing in the list of constellations?
-            //Looks like it puts a constellation on the other side of the decision. Probably a good idea.
-            IEnumerator askPrevent = GameController.MakeYesNoCardDecision(HeroTurnTakerController, SelectionType.PreventDamage, Card, dd, yesNoDecision, constellationsInPlay, GetCardSource());
-            if (UseUnityCoroutines)
-            {
-                yield return GameController.StartCoroutine(askPrevent);
-            }
-            else
-            {
-                GameController.ExhaustCoroutine(askPrevent);
-            }
-            if (!DidPlayerAnswerYes(yesNoDecision))
-            {
-                yield break;
-            }
+                //What does the associatedCards argument actually do here? Should/should not be passing in the list of constellations?
+                //Looks like it puts a constellation on the other side of the decision. Probably a good idea.
+                IEnumerator askPrevent = GameController.MakeYesNoCardDecision(HeroTurnTakerController, SelectionType.PreventDamage, Card, dd, yesNoDecision, constellationsInPlay, GetCardSource());
+                if (UseUnityCoroutines)
+                {
+                    yield return GameController.StartCoroutine(askPrevent);
+                }
+                else
+                {
+                    GameController.ExhaustCoroutine(askPrevent);
+                }
+                if (!DidPlayerAnswerYes(yesNoDecision))
+                {
+                    SetCardProperty(HasSaidNoToNightloreArmor, true);
+                    SetCardProperty(CurrentActionGUID, dd.InstanceIdentifier.GetHashCode() + dd.Target.GetHashCode());
+                    yield break;
+                }
 
-            //"...destroy a constellation in play..."
-            IEnumerator destroyConstellation = GameController.SelectAndDestroyCard(HeroTurnTakerController, new LinqCardCriteria(IsConstellationInPlay, "constellation"), optional: false, cardSource: GetCardSource());
-            //"...to prevent that damage."
-            IEnumerator preventDamage = CancelAction(dd, isPreventEffect: true);
-            if (UseUnityCoroutines)
-            {
-                yield return GameController.StartCoroutine(destroyConstellation);
-                yield return GameController.StartCoroutine(preventDamage);
+                //"...destroy a constellation in play..."
+                IEnumerator destroyConstellation = GameController.SelectAndDestroyCard(HeroTurnTakerController, new LinqCardCriteria(IsConstellationInPlay, "constellation"), optional: false, cardSource: GetCardSource());
+                if (UseUnityCoroutines)
+                {
+                    yield return GameController.StartCoroutine(destroyConstellation);
+                }
+                else
+                {
+                    GameController.ExhaustCoroutine(destroyConstellation);
+                }
+
+                SetCardProperty(HasConstellationBeenDestroyed, true);
             }
-            else
+            if (IsPropertyTrue(HasConstellationBeenDestroyed))
             {
-                GameController.ExhaustCoroutine(destroyConstellation);
-                GameController.ExhaustCoroutine(preventDamage);
+               
+                //"...to prevent that damage."
+                IEnumerator preventDamage = CancelAction(dd, isPreventEffect: true);
+                if (UseUnityCoroutines)
+                {
+                    yield return GameController.StartCoroutine(preventDamage);
+                }
+                else
+                {
+                    GameController.ExhaustCoroutine(preventDamage);
+                }
+
+                if (!dd.IsPretend)
+                {
+                    SetCardProperty(HasConstellationBeenDestroyed, false);
+                    SetCardProperty(CurrentActionGUID, -1);
+                    SetCardProperty(HasSaidNoToNightloreArmor, false);
+                }
+
             }
             yield break;
         }
