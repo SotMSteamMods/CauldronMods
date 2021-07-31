2d01588b499b7e862e4431b070aaac724e38afef
LNBertolini <bertolini.lucas.n@gmail.com>
2021-07-31 16:55:03 -0500

    Add test to check Necro 1929 with hero mission targets

--- Testing/Heroes/NecroVariantTests.cs
+++ Testing/Heroes/NecroVariantTests.cs
@@ -151,6 +151,8 @@ namespace CauldronTests
 
         }
 
+
+
         [Test()]
         public void TestPastNecroInnatePowerCorpseExplosion()
         {
@@ -175,6 +177,31 @@ namespace CauldronTests
             QuickHPUpdate();
             DealDamage(necro, zombie, 50, DamageType.Infernal);
             QuickHPCheck(-2, -2, 0, 0, 0);
+        }
+
+        [Test()]
+        public void TestPastNecroInnatePowerCorpseExplosion_RedMenaceFlipped()
+        {
+            SetupGameController(new string[] { "OblivAeon", "Cauldron.Necro/PastNecroCharacter", "Legacy", "Haka", "Tachyon", "Luminary", "Cauldron.WindmillCity", "MobileDefensePlatform", "InsulaPrimalis", "Cauldron.VaultFive", "Cauldron.Northspar" }, shieldIdentifier: "PrimaryObjective");
+            StartGame();
+
+            Card red = MoveCard(oblivaeon, "TheRedMenace", oblivaeon.TurnTaker.FindSubDeck("MissionDeck"));
+            GoToBeforeStartOfTurn(necro);
+            RunActiveTurnPhase();
+
+            GoToPlayCardPhase(necro);
+            SetHitPoints(red, 5);
+            Card ghoul = PlayCard("Ghoul");
+            Card explosion = PlayCard("CorpseExplosion");
+
+            DestroyCard(red);
+            SetHitPoints(red, 5);
+
+            GoToUsePowerPhase(necro);
+            UsePower(necro.CharacterCard);
+            QuickHPStorage(red);
+            DestroyCard(ghoul, necro.CharacterCard);
+            QuickHPCheck(-2);
         }
 
         [Test()]
