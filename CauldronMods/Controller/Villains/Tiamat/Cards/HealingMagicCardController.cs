926dd54dffd5e221ddca47113e60e79cc71e0076
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-11-22 21:47:29 -0500

    Adjustments to use DidSelectCard and GetSelectedCard

--- CauldronMods/Controller/Villains/Tiamat/Cards/HealingMagicCardController.cs
+++ CauldronMods/Controller/Villains/Tiamat/Cards/HealingMagicCardController.cs
@@ -19,11 +19,9 @@ namespace Cauldron.Tiamat
 
         public override IEnumerator Play()
         {
-            IEnumerator coroutine;
-
             List<SelectCardDecision> storedResults = new List<SelectCardDecision>();
             LinqCardCriteria criteria = new LinqCardCriteria((Card c) => base.CanCardBeConsideredLowestHitPoints(c, (Card card) => card.DoKeywordsContain("head") && card.IsTarget));
-            coroutine = base.GameController.SelectCardAndStoreResults(this.DecisionMaker, SelectionType.GainHP, criteria, storedResults, false, cardSource: base.GetCardSource());
+            IEnumerator coroutine = base.GameController.SelectCardAndStoreResults(this.DecisionMaker, SelectionType.GainHP, criteria, storedResults, false, cardSource: base.GetCardSource());
             if (base.UseUnityCoroutines)
             {
                 yield return base.GameController.StartCoroutine(coroutine);
@@ -32,18 +30,16 @@ namespace Cauldron.Tiamat
             {
                 base.GameController.ExhaustCoroutine(coroutine);
             }
-            Card lowestHPHead = storedResults.FirstOrDefault()?.SelectedCard;
 
-            //The Head with the lowest HP regains {H} + X HP, where X is the number of Healing Magic cards in the villain trash.
-            Func<int> X = () => PlusNumberOfThisCardInTrash(base.H);
-
-            if (lowestHPHead == null)
+            if(DidSelectCard(storedResults))
+            {
+                //The Head with the lowest HP regains {H} + X HP, where X is the number of Healing Magic cards in the villain trash.
+                Card lowestHPHead = GetSelectedCard(storedResults);
+                Func<int> X = () => PlusNumberOfThisCardInTrash(base.H);
+                coroutine = base.GameController.GainHP(lowestHPHead, PlusNumberOfThisCardInTrash(base.H), X, cardSource: GetCardSource());
+            } else
             {
                 coroutine = base.GameController.SendMessageAction("There are no active heads to regain HP.", Priority.Medium, base.GetCardSource(), showCardSource: true);
-            }
-            else
-            {
-                coroutine = base.GameController.GainHP(lowestHPHead, PlusNumberOfThisCardInTrash(base.H), X, cardSource: GetCardSource());
             }
 
             //Play the top card of the villain deck.
