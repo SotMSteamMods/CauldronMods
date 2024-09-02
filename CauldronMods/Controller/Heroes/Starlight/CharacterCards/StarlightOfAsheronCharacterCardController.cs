0d8c0649b45fb26517377ae9731cbe297cb4ede4
origamiswami <76876227+origamiswami@users.noreply.github.com>
2024-09-02 10:50:14 -0400

    Fix softlock with Asheron as Representative of Earth
    Fix softlock when Starlight of Asheron is Representative of Earth

--- CauldronMods/Controller/Heroes/Starlight/CharacterCards/StarlightOfAsheronCharacterCardController.cs
+++ CauldronMods/Controller/Heroes/Starlight/CharacterCards/StarlightOfAsheronCharacterCardController.cs
@@ -11,7 +11,10 @@ namespace Cauldron.Starlight
         public StarlightOfAsheronCharacterCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
         {
             IsCoreCharacterCard = false;
-            SpecialStringMaker.ShowNumberOfCardsAtLocation(HeroTurnTaker.Hand, new LinqCardCriteria((Card c) => IsConstellation(c), "constellation"));
+            if (base.TurnTaker.IsPlayer)
+            {
+                SpecialStringMaker.ShowNumberOfCardsAtLocation(HeroTurnTaker.Hand, new LinqCardCriteria((Card c) => IsConstellation(c), "constellation"));
+            }
         }
 
         public override IEnumerator UsePower(int index = 0)
