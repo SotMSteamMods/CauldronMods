d75a9ddabdcb97d44d50332d4ac50bedc7d4f95e
origamiswami <76876227+origamiswami@users.noreply.github.com>
2024-10-06 18:13:11 -0400

    Update PallidAcademicCardController.cs
    Pallid Academic now respects card/target distinction

--- CauldronMods/Controller/Villains/Mythos/Cards/PallidAcademicCardController.cs
+++ CauldronMods/Controller/Villains/Mythos/Cards/PallidAcademicCardController.cs
@@ -34,7 +34,7 @@ namespace Cauldron.Mythos
             //When a hero target is dealt damage by another hero target:
             //--{MythosDanger} Play the top card of the villain deck.
             //--{MythosMadness} Move the bottom card of a deck to the top of that deck.
-            base.AddTrigger<DealDamageAction>((DealDamageAction action) => action.DidDealDamage && action.DamageSource.IsHero && action.DamageSource.IsTarget && IsHero(action.Target) && action.DamageSource != null && action.DamageSource.Card != null && action.Target != action.DamageSource.Card, this.DangerMadnessResponse, new TriggerType[] { TriggerType.PlayCard, TriggerType.MoveCard }, TriggerTiming.After);
+            base.AddTrigger<DealDamageAction>((DealDamageAction action) => action.DidDealDamage && action.DamageSource.IsHeroTarget && IsHeroTarget(action.Target) && action.DamageSource != null && action.DamageSource.Card != null && action.Target != action.DamageSource.Card, this.DangerMadnessResponse, new TriggerType[] { TriggerType.PlayCard, TriggerType.MoveCard }, TriggerTiming.After);
         }
 
         private IEnumerator DangerMadnessResponse(DealDamageAction action)
